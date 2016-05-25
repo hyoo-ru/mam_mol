ca1523bcdaa007d6a437e2d0b84007e8d67d0a30
nin-jin <nin-jin@ya.ru>
2016-05-25 18:58:40 +0300

    Testing without dependencies.

--- test/test.ts
+++ test/test.ts
@@ -1,3 +1,56 @@
-function $mol_test( script : ( test : $jin2_test ) => void ) {
-	new $jin2_test( script )
+function $mol_test( code : string | { ( test : $mol_test_case ) : void } ) {
+	$mol_test_all.push( new $mol_test_case( code ) )
+}
+
+var $mol_test_all = []
+
+var $mol_test_run = () => {
+	for( var test of $mol_test_all ) {
+		test.run()
+	}
+} 
+
+class $mol_test_case {
+	
+	code : { ( test : $mol_test_case ) : void }
+	
+	constructor( code : string | { ( test : $mol_test_case ) : void } ) {
+		if( typeof code === 'string' ) {
+			this.code = <any> new Function( 'test' , code )
+		} else {
+			this.code = code
+		}
+	}
+	
+	run() {
+		this.code.call( null , this )
+	}
+	
+	done() {
+	}
+	
+	ok( value ) {
+		if( value ) return
+		throw new Error( `Not true (${value})` )
+	}
+	
+	not( value ) {
+		if( !value ) return
+		throw new Error( `Not false (${value})` )
+	}
+	
+	fail( message ) {
+		throw new Error( message )
+	}
+	
+	equal( a , b ) {
+		if( a === b ) return 
+		throw new Error( `Not equal (${a},${b})` )
+	}
+	
+	unique( a , b ) {
+		if( a !== b ) return
+		throw new Error( `Not unique (${a},${b})` )
+	}
+	
 }
