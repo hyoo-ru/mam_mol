6bf279af2ce29144246de408955af3e977627b60
nin-jin <nin-jin@ya.ru>
2016-06-06 14:37:06 +0300

    Better design of components. Using constants in css.

--- demo/form/form.view.ts
+++ demo/form/form.view.ts
@@ -6,14 +6,26 @@ module $.$mol {
 			return this.local( `nameFirst()` , ...diff ) || ''
 		}
 		
+		nameFirstErrors() {
+			return this.nameFirst() ? [] : [ 'Input required' ]
+		}
+		
 		@ $mol_prop()
 		nameNick( ...diff : string[] ) {
 			return this.local( `nameNick()` , ...diff ) || ''
 		}
 		
+		nameNickErrors() {
+			return this.nameNick() ? [] : [ 'Input required' ]
+		}
+		
 		@ $mol_prop()
 		nameSecond( ...diff : string[] ) {
 			return this.local( `nameSecond()` , ...diff ) || ''
+		}
+		
+		nameSecondErrors() {
+			return this.nameSecond() ? [] : [ 'Input required' ]
 		}
 		
 		@ $mol_prop()
