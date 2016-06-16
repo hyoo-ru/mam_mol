a5598eb1be3748161b4d680c712affda45c42a01
nin-jin <nin-jin@ya.ru>
2016-06-16 10:18:08 +0300

    Ranges support contatenation.

--- range/range.stage=test.ts
+++ range/range.stage=test.ts
@@ -17,3 +17,24 @@ $mol_test( test => {
 	test.equal( list.join() , '3,4,5' )
 
 } )
+
+/// Slice of lazy
+$mol_test( test => {
+
+	var list1 = $mol_range({ value : id => id * 2 , count : () => 3 })
+	var list2 = $mol_range({ value : id => id * 3 , count : () => 3 })
+	var list3 = $mol_range([ 11 , 22 , 33 ])
+
+	test.equal( list1.concat( list2 , list3 ).join() , '0,2,4,0,3,6,11,22,33' )
+
+} )
+
+/// Every
+$mol_test( test => {
+
+	var list = $mol_range({ value : id => id * 2 , count : () => 3 })
+
+	test.equal( list.every( v => v >= 0 ) , true )
+	test.equal( list.every( v => v > 0 ) , false )
+
+} )
--- range/range.ts
+++ range/range.ts
@@ -2,19 +2,50 @@ class $mol_range_base< Value > {
 	
 	value( id : number ) : Value { return void 0 }
 	count() { return Number.POSITIVE_INFINITY }
-	
+
 	slice( start = 0 , end? : number ) {
 		return new $mol_range_lazy({
 			value: id => this.value( id + start ),
 			count: () => Math.min( end , this.count() ) - start
 		})
 	}
-	
+
+	concat( ...args ) {
+		var ranges = [ this ].concat( args ).map( $mol_range )
+		return new $mol_range_lazy({
+			value: id => {
+				var subId = id
+				for( var range of ranges ) {
+					var nextId = subId - range.count()
+					if( nextId < 0 ) return range.value( subId )
+					subId = nextId
+				}
+				return
+			} ,
+			count: () => {
+				var count = 0
+				ranges.forEach( range => {
+					count += range.count()
+				} )
+				return count
+			} ,
+		})
+	}
+    
 	forEach( handle : ( value : Value , id : number ) => void ) {
 		var count = this.count() 
 		for( var i = 0 ; i < count ; ++i ) {
 			handle( this.value( i ) , i )
 		}
+	}
+	
+	every( check : ( value : Value , id : number ) => boolean ) {
+		var res = true
+		this.forEach( ( val , id ) => {
+			if( !res ) return
+			res = check( val , id )
+		} )
+		return res
 	}
 	
 	join( delim = ',' ) {
@@ -60,13 +91,12 @@ class $mol_range_lazy< Value > extends $mol_range_base< Value > {
 	
 }
 
-interface $mol_range_initializer< Value > {
+type $mol_range_initializer< Value > = Value | Value[] | {
 	value? : ( id : number )=> Value
 	count? : ()=> number
 }
 
-function $mol_range< Value >( arg : Value[] | $mol_range_initializer< Value > ) : $mol_range_base< Value > {
-	if( typeof arg['value'] === 'function' ) return new $mol_range_lazy( arg )
-	if( 'length' in arg ) return new $mol_range_array( arg as Value[] )
-	throw new Error( 'Unsupported range initializer' )
+function $mol_range< Value >( arg : $mol_range_initializer< Value > ) : $mol_range_base< Value > {
+	if( typeof arg['value'] === 'function' && typeof arg['count'] === 'function' ) return new $mol_range_lazy( arg )
+	return new $mol_range_array( [].concat( arg ) )
 }
