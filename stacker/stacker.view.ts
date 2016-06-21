9f0c1a6b1599e09454a7493304110f4c332c8e45
nin-jin <nin-jin@ya.ru>
2016-06-21 18:38:28 +0300

    Better stacker logic.

---
+++ stacker/stacker.view.ts
@@ -0,0 +1,25 @@
+module $.$mol {
+	export class $mol_stacker extends $.$mol_stacker {
+
+		@ $mol_prop()
+		addonerFocused( ...diff : boolean[] ) {
+			this.argument().link({})
+			return diff[0] || false
+		}
+
+		mainerFocused( ...diff : boolean[] ) {
+			return !this.addonerFocused( ...diff.map( v => !v ) )
+		}
+
+	}
+}
+
+module $.$mol {
+	export class $mol_stacker_panel extends $.$mol_stacker_panel {
+
+		focuses( ...diff : Event[] ) {
+			this.focused( true )
+		}
+
+	}
+}
