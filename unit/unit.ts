3adec0667b5f6d883bb0b52ec287c7946263b157
nin-jin <nin-jin@ya.ru>
2016-08-14 15:52:27 +0300

    Added units support.

--- unit/unit.ts
+++ unit/unit.ts
@@ -30,4 +30,15 @@ class $mol_unit extends $mol_object {
 		return this.prefix() + this.valueView() + this.postfix()
 	}
 	
+	static summ( a : $mol_unit , b : $mol_unit ) {
+		var Class = a.Class() 
+		if( Class !== b.Class() ) throw new Error( `Not same measure: ${Class} , ${b.Class()}` )
+		return new Class( a.valueOf() + b.valueOf() )
+	}
+	
+	mult( m : number ) {
+		var Class = this.Class()
+		return new Class( this.valueOf() * m )
+	}
+	
 }
