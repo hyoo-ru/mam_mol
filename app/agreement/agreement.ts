5c9a05ac38ad74047e77284551eea7a742241e94
nin-jin <nin-jin@ya.ru>
2016-08-17 16:06:38 +0300

    More info in agreemenet details.

--- app/agreement/agreement.ts
+++ app/agreement/agreement.ts
@@ -5,7 +5,7 @@ class $mol_app_agreement_provider extends $mol_model {
 }
 
 /// Группа закупок
-class $mol_app_agreement_supplyGroup extends $mol_model {
+class $mol_app_agreement_supply_group extends $mol_model {
 	id() : string { return void 0 }
 	name() : string { return void 0 }
 	manager() : $mol_app_agreement_person { return void 0 }
@@ -17,14 +17,26 @@ class $mol_app_agreement_supply_division extends $mol_model {
 	name() : string { return void 0 }
 }
 
-/// Продукт
-class $mol_app_agreement_product extends $mol_model {
+/// Способ оплаты
+class $mol_app_agreement_payMethod extends $mol_model {
 	id() : string { return void 0 }
 	name() : string { return void 0 }
-	dateSupply() : $jin.time.moment_class { return void 0 }
-	division() : $mol_app_agreement_supply_division { return void 0 }
-	price() : $mol_unit_money { return void 0 }
-	quantity() : number { return void 0 }
+}
+
+/// Дебитор
+class $mol_app_agreement_debitor extends $mol_model {
+	id() : string { return void 0 }
+	name() : string { return void 0 }
+}
+
+/// Продукт
+interface $mol_app_agreement_supply_position {
+	name : string
+	dateSupply : $jin.time.moment_class
+	stock : $mol_app_agreement_stock
+	price : $mol_unit_money
+	quantity : number
+	cost : $mol_unit_money
 }
 
 /// Работник
@@ -61,17 +73,15 @@ class $mol_app_agreement_supply extends $mol_model {
 	id() : string { return void 0 }
 	provider() : $mol_app_agreement_provider { return void 0 }
 	consumer() : $mol_app_agreement_consumer { return void 0 }
-	stock() : $mol_app_agreement_stock { return void 0 }
+	group() : $mol_app_agreement_supply_group { return void 0 }
 	status() : $mol_app_agreement_supply_status { return void 0 }
-	positions() : { product : $mol_app_agreement_product , quantity : $mol_unit }[] { return void 0 }
-	
-	cost() {
-		var cost = 0
-		this.positions().forEach( pos => {
-			cost += pos.product.price().valueOf() * pos.quantity.valueOf()
-		} )
-		return new $mol_unit_money( cost )
-	}
+	ballanceUnit() : $mol_app_agreement_ballanceUnit { return void 0 }
+	manager() : $mol_app_agreement_person { return void 0 }
+	contract() : $mol_app_agreement_contract { return void 0 }
+	payMethod() : $mol_app_agreement_payMethod { return void 0 }
+	debitor() : $mol_app_agreement_debitor { return void 0 }
+	positions() : $mol_app_agreement_supply_position[] { return void 0 }
+	cost() : $mol_unit_money { return void 0 }
 }
 
 /// Статус заявки на закупку
@@ -91,18 +101,18 @@ class $mol_app_agreement_domain_mock extends $mol_model {
 	
 	@ $mol_prop()
 	supply( id : string ) {
-		var cost = new $mol_unit_money_rur( Math.round( Math.random() * 1000000 ) )
-		var status = [ 'pending' , 'approved' , 'rejected' ][ Math.floor( Math.random() * 3 ) ]
-		var provider = [ 'Ros Neft' , 'Gaz Prom' , 'Ural Ugol' ][ Math.floor( Math.random() * 3 ) ]
-		var stock = [ 'Pulkovo 10' , 'Domodedovo 1' , 'Sheremetievo 5' ][ Math.floor( Math.random() * 3 ) ]
-		var consumer = [ 'ACME' , 'ACME Mobility' , 'ACME Communications' ][ Math.floor( Math.random() * 3 ) ]
 		return new $mol_app_agreement_supply().setup( obj => {
 			obj.id = ()=> id
-			obj.cost = ()=> cost
-			obj.status = ()=> $mol_app_agreement_supply_status[ status ]
-			obj.provider = ()=> this.provider( provider )
-			obj.consumer = ()=> this.consumer( consumer )
-			obj.stock = ()=> this.stock( stock )
+			obj.cost = ()=> new $mol_unit_money_rur( Math.round( Math.random() * 1000000 ) )
+			obj.status = ()=> $mol_app_agreement_supply_status[ [ 'pending' , 'approved' , 'rejected' ][ Math.floor( Math.random() * 3 ) ] ]
+			obj.provider = ()=> this.provider( Math.random().toString( 16 ).substring( 2 ) )
+			obj.consumer = ()=> this.consumer( Math.random().toString( 16 ).substring( 2 ) )
+			obj.group = ()=> this.supplyGroup( Math.random().toString( 16 ).substring( 2 ) )
+			obj.contract = ()=> this.contract( Math.random().toString( 16 ).substring( 2 ) )
+			obj.manager = ()=> this.person( Math.random().toString( 16 ).substring( 2 ) )
+			obj.ballanceUnit = ()=> this.ballanceUnit( Math.random().toString( 16 ).substring( 2 ) )
+			obj.payMethod = ()=> this.payMethod( Math.random().toString( 16 ).substring( 2 ) )
+			obj.debitor = ()=> this.debitor( Math.random().toString( 16 ).substring( 2 ) )
 		} )
 	}
 	
@@ -110,24 +120,79 @@ class $mol_app_agreement_domain_mock extends $mol_model {
 	provider( id : string ) {
 		return new $mol_app_agreement_provider().setup( obj => {
 			obj.id = ()=> id
-			obj.name = ()=> id
+			obj.name = ()=> [ 'ACME Human Resources' , 'ACME' , 'ACME Mobility' ][ Math.floor( Math.random() * 3 ) ]
 		} )
 	}
-	
+
 	@ $mol_prop()
 	consumer( id : string ) {
 		return new $mol_app_agreement_consumer().setup( obj => {
 			obj.id = ()=> id
-			obj.name = ()=> id
+			obj.name = ()=> [ 'ACME Product Placement' , 'ACME Direct' , 'ACME Learning' ][ Math.floor( Math.random() * 3 ) ]
 		} )
 	}
-	
+
+	@ $mol_prop()
+	ballanceUnit( id : string ) {
+		return new $mol_app_agreement_ballanceUnit().setup( obj => {
+			obj.id = ()=> id
+			obj.name = ()=> [ 'ACME Enterprise' , 'ACME Customer' , 'ACME Inside' ][ Math.floor( Math.random() * 3 ) ]
+		} )
+	}
+
+	@ $mol_prop()
+	division( id : string ) {
+		return new $mol_app_agreement_supply_division().setup( obj => {
+			obj.id = ()=> id
+			obj.name = ()=> [ 'Food' , 'Tech' , 'Humans' ][ Math.floor( Math.random() * 3 ) ]
+		} )
+	}
+
+	@ $mol_prop()
+	supplyGroup( id : string ) {
+		return new $mol_app_agreement_supply_group().setup( obj => {
+			obj.id = ()=> id
+			obj.name = ()=> [ 'Ivanov Group' , 'Petrov Group' , 'Sidorov Group' ][ Math.floor( Math.random() * 3 ) ]
+		} )
+	}
+
 	@ $mol_prop()
 	stock( id : string ) {
 		return new $mol_app_agreement_stock().setup( obj => {
 			obj.id = ()=> id
-			obj.name = ()=> id
+			obj.name = ()=> [ 'Riga#10' , 'Moscow#123' , 'Saint-Petersburg#42' ][ Math.floor( Math.random() * 3 ) ]
+		} )
+	}
+
+	@ $mol_prop()
+	person( id : string ) {
+		return new $mol_app_agreement_person().setup( obj => {
+			obj.id = ()=> id
+			obj.name = ()=> [ 'Ivanov AB' , 'Petrov BC' , 'Sidorov CD' ][ Math.floor( Math.random() * 3 ) ]
 		} )
 	}
 	
+	@ $mol_prop()
+	contract( id : string ) {
+		return new $mol_app_agreement_person().setup( obj => {
+			obj.id = ()=> id
+		} )
+	}
+
+	@ $mol_prop()
+	payMethod( id : string ) {
+		return new $mol_app_agreement_payMethod().setup( obj => {
+			obj.id = ()=> id
+			obj.name = ()=> [ 'Accounting' , 'Cash' ][ Math.floor( Math.random() * 2 ) ]
+		} )
+	}
+
+	@ $mol_prop()
+	debitor( id : string ) {
+		return new $mol_app_agreement_payMethod().setup( obj => {
+			obj.id = ()=> id
+			obj.name = ()=> [ 'ACME Finance' , 'ACME Credit Systems' , 'ACME $' ][ Math.floor( Math.random() * 3 ) ]
+		} )
+	}
+
 }
--- app/agreement/detailer/detailer.view.ts
+++ app/agreement/detailer/detailer.view.ts
@@ -12,11 +12,35 @@ module $.$mol {
 		providerName() {
 			return this.supply().provider().name()
 		}
-		
+
 		consumerName() {
 			return this.supply().consumer().name()
 		}
-		
+
+		ballanceUnitName() {
+			return this.supply().ballanceUnit().name()
+		}
+
+		supplyGroupName() {
+			return this.supply().group().name()
+		}
+
+		managerName() {
+			return this.supply().manager().name()
+		}
+
+		payMethodName() {
+			return this.supply().payMethod().name()
+		}
+
+		debitorName() {
+			return this.supply().debitor().name()
+		}
+
+		contractId() {
+			return this.supply().contract().id()
+		}
+
 		cost() {
 			return this.supply().cost()
 		}
