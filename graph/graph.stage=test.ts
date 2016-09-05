b2bf75d5a73e2d60f04768b62e1d00c853e3b54f
nin-jin <nin-jin@ya.ru>
2016-09-05 18:00:40 +0300

    Fixed graph test.

--- graph/graph.stage=test.ts
+++ graph/graph.stage=test.ts
@@ -1,12 +1,12 @@
 /// Cut cycle in right point
 $mol_test( test => {
 	
-	var graph = new $mol_graph
+	var graph = new $mol_graph< void , { priority : number } >()
 	graph.link( 'A' , 'B' , { priority : 0 } )
 	graph.link( 'B' , 'C' , { priority : -2 } )
 	graph.link( 'C' , 'D' , { priority : 0 } )
 	graph.link( 'D' , 'A' , { priority : -1 } )
 
-	test.equal( graph.sorted().join(''), 'CDAB' )
+	test.equal( graph.sorted( edge => edge.priority ).join(''), 'CDAB' )
 	
 } )
