0df354f31f4c7e985c47d7c76a7ebff38b564d3e
nin-jin <nin-jin@ya.ru>
2016-09-08 11:23:31 +0300

    Run tests for tests instantly.

--- test/test.stage=test.ts
+++
@@ -1,8 +0,0 @@
-/// Positive tests
-$mol_test( test => {
-	test.ok( 1 )
-	test.not( 0 )
-	test.equal( 2 , 2 )
-	test.unique( [ 3 ] , [ 3 ] )
-	test.done()
-})
--- test/test.ts
+++ test/test.ts
@@ -54,3 +54,12 @@ class $mol_test_case {
 	}
 	
 }
+
+/// Positive tests
+$mol_test( test => {
+	test.ok( 1 )
+	test.not( 0 )
+	test.equal( 2 , 2 )
+	test.unique( [ 3 ] , [ 3 ] )
+	test.done()
+})
