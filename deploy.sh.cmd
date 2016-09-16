38e93edc722202e17d8afc162a9cff83d914d183
nin-jin <nin-jin@ya.ru>
2016-09-16 12:50:29 +0300

    Commit and optional push then switch to master.

--- deploy.sh.cmd
+++ deploy.sh.cmd
@@ -1,6 +1,4 @@
 git checkout gh-pages
 git merge master
 npm start mol mol/app/supplies mol/perf/render mol/perf/uibench
-git commit -a -m "Update"
-git push
-git checkout master
+git commit -a -m "Update" && git push || git checkout master
