658f64dd7fa14d302b5bfd3e3b61a211db45650c
JGluhov <evgeniy.glukhov@saprun.com>
2016-09-23 16:48:18 +0300

    merging with master.

--- viewer/selection/selection.web.ts
+++ viewer/selection/selection.web.ts
@@ -3,9 +3,19 @@ document.addEventListener( 'selectionchange' , event => {
 } )
 
 document.addEventListener( 'focusin' , event => {
-	$mol_viewer_selection.focused( event.srcElement )
+	let parents : Element[] = [],
+		element : Element
+	
+	element = event.srcElement
+	
+	while(element) {
+		parents.push(element)
+		element = element.parentElement ? element.parentElement : null
+	}
+	
+	$mol_viewer_selection.focused( parents )
 } )
 
 document.addEventListener( 'focusout' , event => {
-	$mol_viewer_selection.focused( null )
+	$mol_viewer_selection.focused( [] )
 } )
