6fa3bf85a53b4bc0a300182d16f72c8a051538e3
nin-jin <nin-jin@ya.ru>
2016-09-25 00:20:43 +0300

    IE11 compatibility.

--- merge/dict/dict.ts
+++ merge/dict/dict.ts
@@ -1,3 +1,6 @@
 function $mol_merge_dict< Target , Source >( target : Target , source : Source ) : Target & Source {
-	return (<any>Object).assign( {} , target , source )
+	let result = <any> {}
+	for( let key in target ) result[ key ] = ( <any> target )[ key ]
+	for( let key in source ) result[ key ] = ( <any> source )[ key ]
+	return result
 }
