9f0fcffc8c6b076729ee798371c1d1ad20c01653
JGluhov <evgeniy.glukhov@saprun.com>
2016-10-17 15:55:14 +0300

    floater complete.

---
+++ floater/floater.view.ts
@@ -0,0 +1,9 @@
+module $.$mol {
+	export class $mol_floater extends $.$mol_floater {
+		shiftStyle() {
+			const offset = this.context().$mol_scroller_scrollTop()
+			return `${ offset }px`
+		}
+	}
+}
+
