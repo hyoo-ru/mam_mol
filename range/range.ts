b6753b94c8a4797d42276425d083a562ea0d459a
nin-jin <nin-jin@ya.ru>
2016-10-17 14:10:18 +0300

    $mol_range_lazy uses item() instead of get()

--- range/range.ts
+++ range/range.ts
@@ -2,7 +2,7 @@ module $ {
 	
 	export class $mol_range_common< Value > extends Array< Value > {
 		
-		get( id : number ) : Value {
+		item( id : number ) : Value {
 			return
 		}
 		
@@ -13,46 +13,44 @@ module $ {
 		}
 		
 		forEach( handle : ( value : Value , id : number ) => void ) {
-			var length = this.length
-			for( var i = 0 ; i < length ; ++i ) {
-				handle( this.get( i ) , i )
+			const length = this.length
+			for( let i = 0 ; i < length ; ++i ) {
+				handle( this.item( i ) , i )
 			}
 		}
 		
 		valueOf() : Value[] {
-			var list : Value[] = []
+			const list : Value[] = []
 			this.forEach( val => list.push( val ) )
 			return list
 		}
 		
 		concat( ...args : any[] ) : Value[] {
-			var ranges = args.map( range => range.valueOf() )
+			const ranges = args.map( range => range.valueOf() )
 			return this.valueOf().concat( ...ranges )
 			//return new $mol_range_list( [ this ].concat( args ) )
 		}
 		
 		slice( start = 0 , end? : number ) {
-			var source = this
-			return new $mol_range_lazy<Value>(
-				{
-					get( id ){
-						return source.get( id + start )
-					} ,
-					get length() {
-						return Math.min( end , source.length ) - start
-					}
+			const source = this
+			return new $mol_range_lazy<Value>( {
+				item( id ){
+					return source.item( id + start )
+				} ,
+				get length() {
+					return Math.min( end , source.length ) - start
 				}
-			)
+			} )
 		}
 		
 		join( delim = ',' ) {
-			var list : Value[] = []
+			const list : Value[] = []
 			this.forEach( val => list.push( val ) )
 			return list.join( delim )
 		}
 		
 		every( check : ( value : Value , id : number ) => boolean ) {
-			var res = true
+			let res = true
 			this.forEach(
 				( val , id ) => {
 					if( !res ) return
@@ -63,7 +61,7 @@ module $ {
 		}
 		
 		some( check : ( value : Value , id : number ) => boolean ) {
-			var res = false
+			let res = false
 			this.forEach(
 				( val , id ) => {
 					if( res ) return
@@ -79,15 +77,15 @@ module $ {
 		
 		constructor(
 			private source = {
-				get( id : number ) : Value { return } ,
+				item( id : number ) { return < Value > void 0 } ,
 				length : 0
 			}
 		) {
 			super()
 		}
 		
-		get( id : number ) : Value {
-			return this.source.get( id )
+		item( id : number ) : Value {
+			return this.source.item( id )
 		}
 		
 		get length() {
