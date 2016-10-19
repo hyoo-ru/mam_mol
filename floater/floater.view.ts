a3c9fb9ceedb98c23103735357074e88adf11d29
nin-jin <nin-jin@ya.ru>
2016-10-19 14:00:14 +0300

    Some floater fixes.

--- floater/floater.view.ts
+++ floater/floater.view.ts
@@ -1,9 +1,11 @@
 module $.$mol {
 	export class $mol_floater extends $.$mol_floater {
+		
 		shiftStyle() {
 			const offset = this.context().$mol_scroller_scrollTop()
 			return `${ offset }px`
 		}
+		
 	}
 }
 
