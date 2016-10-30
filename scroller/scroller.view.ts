4ed2e539a9c99e793654441ac7e5722b552f43b9
nin-jin <nin-jin@ya.ru>
2016-10-30 16:46:16 +0300

    Scroller render shadow on overflow.

--- scroller/scroller.view.ts
+++ scroller/scroller.view.ts
@@ -18,17 +18,29 @@ module $.$mol {
 			if( next ) this.moving( true )
 			return $mol_state_session.value( this.objectPath() + '.scrollTop()' , next ) || 0
 		}
-
+		
 		scrollLeft( next? : number ) {
 			return $mol_state_session.value( this.objectPath() + '.scrollLeft()' , next ) || 0
 		}
-
+		
+		@ $mol_mem()
+		scrollBottom( next? : number ) {
+			return next || 0
+		}
+		
+		@ $mol_mem()
+		scrollRight( next? : number ) {
+			return next || 0
+		}
+		
 		eventScroll( next? : Event ) {
 			this.moving( true )
 			new $mol_defer( ()=> {
-				const el = this.DOMNode()
+				const el = this.DOMNode() as HTMLElement
 				this.scrollTop( el.scrollTop )
 				this.scrollLeft( el.scrollLeft )
+				this.scrollBottom( Math.max( 0 , el.scrollHeight - el.scrollTop - el.offsetHeight ) )
+				this.scrollRight( Math.max( 0 , el.scrollWidth - el.scrollLeft - el.offsetWidth ) )
 			} )
 		}
 		
@@ -49,6 +61,16 @@ module $.$mol {
 			subContext.$mol_scroller_scrollTop = ()=> this.scrollTop()
 			subContext.$mol_scroller_moving = ()=> this.moving()
 			return subContext
+		}
+		
+		@ $mol_mem()
+		shadowStyle() {
+			let shadows : string[] = []
+			if( this.scrollTop() > 0 ) shadows.push( 'inset 0 6px 6px -6px rgba( 0 , 0 , 0 , .25 )' )
+			if( this.scrollLeft() > 0 ) shadows.push( 'inset 6px 0 6px -6px rgba( 0 , 0 , 0 , .25 )' )
+			if( this.scrollBottom() > 0 ) shadows.push( 'inset 0 -6px 6px -6px rgba( 0 , 0 , 0 , .25 )' )
+			if( this.scrollRight() > 0 ) shadows.push( 'inset -6px 0 6px -6px rgba( 0 , 0 , 0 , .25 )' )
+			return shadows.join( ' , ' )
 		}
 		
 	}
