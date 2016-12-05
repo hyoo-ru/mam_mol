33abf5fc7dff9bccba7a59fc5e5970ad27cf4ad5
nin-jin <nin-jin@ya.ru>
2016-12-05 15:50:03 +0300

    $mol_app_bench uses $mol_bencher now.

--- app/bench/bench.view.ts
+++ app/bench/bench.view.ts
@@ -91,23 +91,8 @@ namespace $.$mol {
 		}
 		
 		@ $mol_mem()
-		resultsSortCol( next? : string ) {
+		resultsColSort( next? : string ) {
 			return $mol_state_arg.value( this.stateKey( 'sort' ) , next )
-		}
-		
-		@ $mol_mem()
-		resultsSorted() {
-			const prev = this.results()
-			const col = this.resultsSortCol()
-			if( !col ) return prev
-			
-			const next : { [ sample : string ] : { [ step : string ] : any } } = {}
-			
-			const keys = Object.keys( prev )
-			keys.sort( ( a , b )=> this.resultNumber({ row : a , col }) - this.resultNumber({ row : b , col }) )
-			keys.forEach( row => next[ row ] = prev[ row ] )
-			
-			return next
 		}
 		
 		menuOptions() {
@@ -128,43 +113,6 @@ namespace $.$mol {
 			return next
 		}
 		
-		griderCeller( id : { row : string , col : string } ) {
-			if( id.col === 'sample' ) return this.resulter().cellerText( id )
-			return this.resulterCellerNumber( id )
-		}
-		
-		resultValue( id : { row : string , col : string } ) {
-			return this.results()[ id.row ][ id.col ]
-		}
-		
-		resultNumber( id : { row : string , col : string } ) {
-			return parseInt( this.resultValue( id ) , 10 )
-		}
-		
-		@ $mol_mem_key()
-		resultMaxValue( col : string ) {
-			let max = 0
-			
-			const results = this.results()
-			for( let sample in results ) {
-				const numb = this.resultNumber({ row : sample , col })
-				if( numb > max ) max = numb
-			}
-			
-			return max
-		}
-		
-		resultPortion( id : { row : string , col : string } ) {
-			return this.resultNumber( id ) / this.resultMaxValue( id.col )
-		}
-		
-		resulterHeaderTitle( col : string ) {
-			return col
-		}
-		
-		eventResulterSortToggle( col : string , next? : Event ) {
-			this.resultsSortCol( col )
-		}
 	}
 	
 }
