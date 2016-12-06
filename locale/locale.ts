8eb4a4b4029001aa2bcee801417ee1faf260e8c2
nin-jin <nin-jin@ya.ru>
2016-12-06 19:32:09 +0300

    New localization scheme.

--- locale/locale.ts
+++ locale/locale.ts
@@ -11,14 +11,24 @@ namespace $ {
 			return $mol_state_local.value( 'locale' , next ) || $mol_state_arg.value( 'locale' ) || 'en'
 		}
 		
+		@ $mol_mem()
 		static texts() {
 			const uri = `-/web.locale=${ this.lang() }.json`
 			const resource = $mol_http_resource_json.item< $mol_locale_dict >( uri ) 
 			return resource.json()
 		}
 		
-		static text( context : string , key : string ) {
-			return this.texts()[ `${ context }_${ key }` ]
+		static text( contexts : string[] , key : string ) {
+			const texts = this.texts()
+			
+			for( let i = 0 ; i < contexts.length ; ++i ) {
+				const text = texts[ `${ contexts[i] }_${ key }` ]
+				if( text ) return text
+			}
+			
+			console.warn( 'Locale tet not found: ' , contexts , key )
+			
+			return `<${ key }>`
 		}
 		
 	}
