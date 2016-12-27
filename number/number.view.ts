5100c364518c77e57b6c713bf96738d57dd0c7fd
Arthur Lavrischev <ar2r13lawrence@gmail.com>
2016-12-27 17:34:18 +0300

    scroll on

--- number/number.view.ts
+++ number/number.view.ts
@@ -1,24 +1,24 @@
 namespace $.$mol {
 	export class $mol_number extends $.$mol_number {
-		
+
 		eventDec( next? : Event ) {
 			this.value( this.value() - this.precisionChange() )
 		}
-		
+
 		eventInc( next? : Event ) {
 			this.value( Number( this.value() ) + this.precisionChange() )
 		}
-		
+
 		valueString( next? : string ) {
 			if( next !== void 0 ) {
 				this.value( next === '' ? null : Number( next ) )
 			}
-			
+
 			var precisionView = this.precisionView()
 			var value = next ? Number( next ) : this.value()
-			
+
 			if( value === null ) return ''
-			
+
 			if( precisionView >= 1 ) {
 				return ( value / precisionView ).toFixed()
 			} else {
@@ -26,22 +26,23 @@ namespace $.$mol {
 				return value.toFixed( fixedNumber )
 			}
 		}
-		
+
 		eventWheel( next? : MouseWheelEvent ) {
+			next.preventDefault();
 			if( next.wheelDelta > 0 ) {
 				this.eventInc( next )
 			} else {
 				this.eventDec( next )
 			}
 		}
-		
+
 		incrementer() {
 			return this.enabledInc() ? super.incrementer() : null
 		}
-		
+
 		decrementer() {
 			return this.enabledDec() ? super.decrementer() : null
 		}
-		
+
 	}
 }
