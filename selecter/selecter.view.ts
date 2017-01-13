289ff5223f7d670d89dc278af851b7f6fb63b424
Arthur Lavrischev <ar2r13lawrence@gmail.com>
2017-01-13 23:03:14 +0300

    WIP 3

--- selecter/selecter.view.ts
+++ selecter/selecter.view.ts
@@ -1,12 +1,32 @@
 namespace $.$mol {
 
 	export class $mol_selecter extends $.$mol_selecter {
-		optionLabel( id: string ) {
+		optionLabel ( id: string ) {
 			return id;
 		}
 		
-		optioners() {
-			return this.options().map( ( option: string ) => this.optioner( option ) )
+		optionsFiltered () {
+			let filter = this.prefix();
+			return this.options().filter( id => {
+				if( id !== this.value() )
+					return id.toLowerCase().match( filter ) 
+			})
+		}
+		
+		optioners () {
+			return this.optionsFiltered().map( ( option: string ) => this.optioner( option ) )
+		}
+		 
+		selectedOption () {
+			return (this.value())
+				? this.optionerContent(this.value())
+				: (this.options().length > this.minOptionsToSearch())
+					? [ this.stringer() ]
+					: [ this.emptyOptioner() ]
+		}
+		
+		eventSelect ( id: string, event?: MouseEvent ) {
+			this.value( id )
 		}
 	}
 }
