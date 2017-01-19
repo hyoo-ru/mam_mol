ab56f39da1a745a53be7955159436687922349c3
nin-jin <nin-jin@ya.ru>
2017-01-19 18:59:10 +0300

    Do not fail on nodejs.

--- atom/atom.ie.web.js
+++ atom/atom.ie.web.js
@@ -1,21 +1,23 @@
-namespace $ {
-	
-	let nativeAlert = window.alert
-	export function alert( message : string ) {
+if( typeof alert === 'function' ) {
+	let nativeAlert = alert
+	window.alert = function alert( message ) {
 		console.warn( 'Alerts causes atom synchronization problems in IE. Use custom notificator instead.' )
 		return nativeAlert( message )
 	}
+}
 
-	let nativeConfirm = window.confirm
-	export function confirm( question : string ) {
+if( typeof confirm === 'function' ) {
+	let nativeConfirm = confirm
+	window.confirm = function confirm( question ) {
 		console.warn( 'Confirms causes atom synchronization problems in IE. Use custom dialog instead.' )
 		return nativeConfirm( question )
 	}
-	
-	let nativePrompt = window.prompt
-	export function prompt( question : string , def = '' ) {
+}
+
+if( typeof confirm === 'function' ) {
+	let nativePrompt = prompt
+	window.prompt = function prompt( question , def ) {
 		console.warn( 'Prompts causes atom synchronization problems in IE. Use custom dialog instead.' )
 		return nativePrompt( question , def )
 	}
-
 }
