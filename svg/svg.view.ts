42d5d3642402c44a44ea4f0469536f88fad2faeb
nin-jin <nin-jin@ya.ru>
2017-02-15 17:28:10 +0300

    Added $mol_svg_line

--- svg/svg.view.ts
+++ svg/svg.view.ts
@@ -1,5 +1,33 @@
 namespace $.$mol {
 	
+	export class $mol_svg_line extends $.$mol_svg_line {
+		
+		from() {
+			return this.pos()[ 0 ]
+		}
+		
+		from_x() {
+			return this.from()[0]
+		}
+		
+		from_y() {
+			return this.from()[1]
+		}
+		
+		to() {
+			return this.pos()[ 1 ]
+		}
+		
+		to_x() {
+			return this.to()[0]
+		}
+		
+		to_y() {
+			return this.to()[1]
+		}
+		
+	}
+	
 	export class $mol_svg_circle extends $.$mol_svg_circle {
 		
 		pos_x() {
