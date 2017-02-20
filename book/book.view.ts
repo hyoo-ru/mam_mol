8c8f8efab9eb4f9dfec293e0b9556eedd08eab73
nin-jin <nin-jin@ya.ru>
2017-02-20 17:06:32 +0300

    Added $mol_ghost for wrappers. $mol_book uses $mol_ghost to prevent scrolling position resetting.

--- book/book.view.ts
+++ book/book.view.ts
@@ -1,21 +1,31 @@
 namespace $.$mol {
 	export class $mol_book extends $.$mol_book {
 		
-		sub() {
-			const pages = this.pages().filter( page => page ).reverse()
+		@ $mol_mem()
+		break_point() {
+			const pages = this.pages().filter( page => page )
 			const limit = this.width()
 
 			let width = 0
-			for( var visible = 0 ; visible < pages.length ; ++ visible ) {
-				width += pages[ visible ].minimal_width()
+			for( var break_point = pages.length ; break_point > 0 ; -- break_point ) {
+				width += pages[ break_point - 1 ].minimal_width()
 				if( width > limit ) break
 			}
+			if( break_point === pages.length ) -- break_point
 			
-			if( visible == 0 ) visible = 1
-			
-			if( visible === pages.length ) return pages
-			
-			return [ ... pages.slice( 0 , visible ) , this.Break() , ... pages.slice( visible ) ]
+			return break_point
+		}
+		
+		page( index : number ) {
+			return this.pages()[ index ]
+		}
+		
+		page_visible( index : number ) {
+			return index >= this.break_point()
+		}
+		
+		pages_extended() {
+			return this.pages().map( ( page , index )=> this.Page( index ) )
 		}
 		
 		title() {
