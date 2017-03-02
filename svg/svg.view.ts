7e426bd47687096d6b6e26e9286a6df5c16b6514
nin-jin <nin-jin@ya.ru>
2017-03-02 12:09:42 +0300

    Added svg text support.

--- svg/svg.view.ts
+++ svg/svg.view.ts
@@ -40,4 +40,16 @@ namespace $.$mol {
 		
 	}
 	
+	export class $mol_svg_text extends $.$mol_svg_text {
+		
+		pos_x() {
+			return this.pos()[ 0 ]
+		}
+		
+		pos_y() {
+			return this.pos()[ 1 ]
+		}
+		
+	}
+	
 }
