aa1ef1c14672549855bacbb073082a99f07a8ffe
nin-jin <nin-jin@ya.ru>
2017-04-13 15:26:50 +0300

    Block and element editing.

--- app/studio/studio.view.ts
+++ app/studio/studio.view.ts
@@ -1,13 +1,54 @@
 namespace $.$mol {
+	
+	export interface $mol_app_studio_prop {
+		type : 'boolean' | 'number' | 'string' | '$mol_view' | 'List' | 'Dict' | 'any'
+		keyed : boolean
+		mutable : boolean
+	}
+	
+	export interface $mol_app_studio_props {
+		[ name : string ] : $mol_app_studio_prop
+	}
 
 	export class $mol_app_studio extends $.$mol_app_studio {
 		
+		@ $mol_mem_key()
+		props( name : string , next? : $mol_app_studio_props ) {
+			return this.registry()[ name ]
+		}
+		
+		@ $mol_mem_key()
+		props_all( name : string ) : $mol_app_studio_props {
+			let View = this.view_class( name )
+			
+			let props_all : $mol_app_studio_props = {}
+			
+			while( true ) {
+				const props = this.props( View.toString() )
+				if( !props ) break
+				
+				props_all = { ... props , ... props_all }
+				View = Object.getPrototypeOf( View.prototype ).constructor
+			}
+			
+			return props_all
+		}
+		
+		props_self() {
+			return this.props_all( this.Element().constructor.toString() )
+		}
+		
+		view_class( name : string ) {
+			if( !$[ name ] ) throw new Error( `View class not found: ${name}` )
+			return $[ name ]
+		}
+		
 		fields() {
-			return Object.keys( this.registry()[ this.component() ] ).map( name => this.Field( name ) )
+			return Object.keys( this.props_self() ).map( name => this.Field( name ) )
 		}
 		
 		Field( name : string ) {
-			switch( this.registry()[ this.component() ][ name ].type ) {
+			switch( this.props_self()[ name ].type ) {
 				case 'string' : return this.String_field( name )
 				case '$mol_view' : return this.View_field( name )
 				case 'List' : return this.List_field( name )
@@ -20,7 +61,7 @@ namespace $.$mol {
 		}
 		
 		value( name : string ) {
-			switch( this.registry()[ this.component() ][ name ].type ) {
+			switch( this.props_self()[ name ].type ) {
 				case 'string' : return this.string_value( name ) || void null
 				case '$mol_view' : return this.View( name )
 				case 'List' : return this.View( name )
@@ -28,12 +69,36 @@ namespace $.$mol {
 			}
 		}
 		
+		string_hint( name : string ) {
+			return this.Block()[ name ]()
+		}
+		
+		view_hint( name : string ) {
+			return this.Block()[ name ]().constructor.toString()
+		}
+		
 		@ $mol_mem_key()
 		View( name : string ) {
-			const Class = this.string_value( name )
-			if( !Class ) return void null
+			const view_name = this.string_value( name )
+			if( !view_name ) return void null
 			
-			return new $[ Class ]
+			const View = this.view_class( view_name )
+			return new View
+		}
+		
+		parent() {
+			return $mol_state_arg.value( this.state_key( 'block' ) ) || super.parent()
+		}
+		
+		element() {
+			return $mol_state_arg.value( this.state_key( 'element' ) )
+		}
+		
+		Element() {
+			const element = this.element()
+			if( !element ) return this.Block()
+			
+			return this.Block()[ element ]()
 		}
 		
 		@ $mol_mem()
@@ -51,22 +116,31 @@ namespace $.$mol {
 		}
 		
 		@ $mol_mem()
-		Preview() {
-			const props = this.registry()[ this.component() ]
+		Block() {
+			const Class = this.view_class( this.parent() )
+			return new Class
+		}
+		
+		@ $mol_mem()
+		Block_wrapped() {
+			const props = this.props_self()
 			
-			const Class = $[ this.component() ]
-			const obj = new Class
+			const obj = this.Block()
 			
 			for( let name in props ) {
 				const value = obj[ name ] 
 				obj[ name ] = ()=> {
 					const val = this.value( name )
 					if( val !== void 0 ) return val
-					return value.call( obj )
+					return value && value.call( obj )
 				}
 			}
 			
 			return obj
+		}
+		
+		editor_title() {
+			return this.element() || this.parent()
 		}
 		
 	}
