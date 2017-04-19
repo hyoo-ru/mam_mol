3217ef8ea220fbf18b6fa9dbddb272dc85b7ff9f
muleronko <myl-roma1@yandex.ru>
2017-04-19 15:24:06 +0300

    Final change source button

--- list/demo/large/large.view.ts
+++ list/demo/large/large.view.ts
@@ -1,6 +1,7 @@
+
 namespace $.$mol {
 	export class $mol_list_demo_large extends $.$mol_list_demo_large {
-		
+
 		rows() {
 			var next : $mol_view[] = []
 			for( var id = 0 ; id < 1000 ; ++id ) {
@@ -8,7 +9,7 @@ namespace $.$mol {
 			}
 			return next
 		}
-		
+
 		row_text( id : number ) {
 			return `Row #${ id + 1 }`
 		}
