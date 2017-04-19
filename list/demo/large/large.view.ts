a82cd59d158612ac09bd33e7f7fe46ed377a25cd
muleronko <myl-roma1@yandex.ru>
2017-04-19 19:18:09 +0300

    del tab

--- list/demo/large/large.view.ts
+++ list/demo/large/large.view.ts
@@ -1,6 +1,6 @@
 namespace $.$mol {
 	export class $mol_list_demo_large extends $.$mol_list_demo_large {
-			
+		
 		rows() {
 			var next : $mol_view[] = []
 			for( var id = 0 ; id < 1000 ; ++id ) {
