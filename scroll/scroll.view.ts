eab015d75d1031a05a9f6244bbfefb169ff04bf2
nin-jin <nin-jin@ya.ru>
2017-06-09 00:46:27 +0800

    Disable scroll offset restoring.

--- scroll/scroll.view.ts
+++ scroll/scroll.view.ts
@@ -14,13 +14,13 @@ namespace $.$mol {
 	
 	export class $mol_scroll extends $.$mol_scroll {
 
-		scroll_top( next? : number ) {
-			return $mol_state_session.value( `${ this }.scroll_top()` , next ) || 0
-		}
-		
-		scroll_left( next? : number ) {
-			return $mol_state_session.value( `${ this }.scroll_left()` , next ) || 0
-		}
+		// scroll_top( next? : number ) {
+		// 	return $mol_state_session.value( `${ this }.scroll_top()` , next ) || 0
+		// }
+		// 
+		// scroll_left( next? : number ) {
+		// 	return $mol_state_session.value( `${ this }.scroll_left()` , next ) || 0
+		// }
 		
 		@ $mol_mem()
 		scroll_bottom( next? : number ) {
