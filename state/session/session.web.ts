c3813ab80c279bb573398b3740b626b1f1b3f771
nin-jin <nin-jin@ya.ru>
2017-07-11 13:19:16 +0300

    Defended sessionStorage like localStorage.

--- state/session/session.web.ts
+++ state/session/session.web.ts
@@ -2,12 +2,39 @@ namespace $ {
 	
 	export class $mol_state_session< Value > extends $mol_object {
 		
+		static 'native()' : Pick< Storage , 'getItem'|'setItem'|'removeItem' >
+		static native() {
+			if( this['native()'] ) return this['native()']
+
+			try {
+				const native = $mol_dom_context.sessionStorage
+				native.setItem( '' , '' )
+				native.removeItem( '' )
+				return this['native()'] = native
+			} catch( error ) {
+				console.warn( error )
+			}
+
+			return this['native()'] = {
+				getItem( key : string ) {
+					return this[ ':' + key ]
+				} ,
+				setItem( key : string , value : string ) {
+					this[ ':' + key ] = value
+				} ,
+				removeItem( key : string ) {
+					this[ ':' + key ] = void 0
+				}
+			}
+
+		}
+
 		@ $mol_mem_key()
 		static value< Value >( key : string , next? : Value ) : Value {
-			if( next === void 0 ) return JSON.parse( sessionStorage.getItem( key ) || 'null' )
+			if( next === void 0 ) return JSON.parse( this.native().getItem( key ) || 'null' )
 			
-			if( next === null ) sessionStorage.removeItem( key )
-			else sessionStorage.setItem( key , JSON.stringify( next ) )
+			if( next === null ) this.native().removeItem( key )
+			else this.native().setItem( key , JSON.stringify( next ) )
 			
 			return next
 		}
