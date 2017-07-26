3e27b425379518a4d2fa9b55d931d7912dacbd18
nin-jin <nin-jin@ya.ru>
2017-07-26 13:09:47 +0300

    Fixed $mol_ghost caching.

--- ghost/ghost.view.ts
+++ ghost/ghost.view.ts
@@ -2,17 +2,15 @@ namespace $.$mol {
 
 	export class $mol_ghost extends $.$mol_ghost {
 		
-		'dom_node()' : Element
+		@ $mol_mem()
 		dom_node() {
-			if( this['dom_node()'] ) return this['dom_node()']
-			
 			const node = this.Sub().dom_node()
 
 			$mol_dom_render_attributes( node , this.attr_static() )
 			$mol_dom_render_events( node , this.event() )
 			$mol_dom_render_events_async( node , this.event_async() )
 
-			return this['dom_node()'] = node
+			return node
 		}
 		
 		render() {
