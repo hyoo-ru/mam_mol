db95bfa5b386ea3833dab8f63f537f7fd7bceb66
nin-jin <nin-jin@ya.ru>
2017-09-11 11:35:57 +0300

    Dynamic pop bubble position depends on anchor position.

--- pop/pop.view.ts
+++ pop/pop.view.ts
@@ -9,7 +9,16 @@ namespace $.$$ {
 		}
 		
 		height_max() {
-			return this.$.$mol_view_visible_height() * 0.3;
+			return this.$.$mol_window.size().height * 0.33;
+		}
+
+		align() {
+			const viewport = this.$.$mol_window.size()
+			
+			const vert = this.top() > ( viewport.height - this.bottom() ) ? 'top' : 'bottom'
+			const hor = this.left() > ( viewport.width - this.right() ) ? 'left' : 'right'
+			
+			return `${ vert }_${ hor }`
 		}
 		
 	}
