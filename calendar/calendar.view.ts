54d919f647fb82b9eb115d4989a57c4d07c8dd8e
muleronko <myl-roma1@yandex.ru>
2017-09-19 13:40:44 +0300

    Calendar fix bugs

--- calendar/calendar.view.ts
+++ calendar/calendar.view.ts
@@ -11,7 +11,7 @@ namespace $.$mol {
 			
 			return start_draw_day
 		}
-/* start - week day in header */
+
 		head(){
 			const next : $mol_view[] = []
 			for( let id = 0 ; id < 7 ; ++id ) {
@@ -23,8 +23,7 @@ namespace $.$mol {
 		head_day( id : number ){
 			return this.start_draw_day().shift( { day : id } ).toString('WD')
 		}
-/* end */
-/* Нарисовал строки */
+
 		rows(){
 			const week : $mol_view[] = []
 			for( let row = 0; row < 6; ++row ){
@@ -32,7 +31,7 @@ namespace $.$mol {
 			}
 			return week
 		}
-/* Нарисовал ячейки */
+
 		cells(row : number){
 			const day : $mol_view[] = []
 			for( let col = 0; col < 7; ++col ){
@@ -40,26 +39,23 @@ namespace $.$mol {
 			}
 			return day
 		}
-/* end */
 
-/* заполнение датами */
+		cell(id: {row : number, col : number }){
+			return this.start_draw_day().shift({day : (id.col + 7 * id.row)}).toString("D")
+		}
 
-		// cell(id: {row : number, col : number }){
-		// 	return this.start_draw_day().shift({day : (id.col + 7 * id.row)}).toString("D")
-		// }
-
-		// other_month(id: {row : number, col : number }){
-		// 	let month = this.start_draw_day().shift({day : (id.col + 7 * id.row)}).month
-		// 	let this_month = this.first_day().month
+		other(id: {row : number, col : number }){
+			let month = this.start_draw_day().shift({day : (id.col + 7 * id.row)}).month
+			let this_month = this.first_day().month
 			
-		// 	return this_month != month ? true : false
-		// }
+			return this_month != month ? true : false
+		}
 
-		// weekend(id: {row : number, col : number }){
-		// 	let day = this.start_draw_day().shift({day : (id.col + 7 * id.row)}).weekday
+		weekend(id: {row : number, col : number }){
+			let day = this.start_draw_day().shift({day : (id.col + 7 * id.row)}).weekday
 
-		// 	return  day == 0 ? true : day == 6 ? true : false
-		// }
+			return  day == 0 ? true : day == 6 ? true : false
+		}
 
 	 }
 
