b5acac6b7ef8ed9b32c18054d583b339795d0157
nin-jin <nin-jin@ya.ru>
2017-09-22 16:10:19 +0300

    $mol_func_name: always cache function name for performance reason.

--- func/func.ts
+++ func/func.ts
@@ -7,12 +7,10 @@ namespace $ {
 	export var $mol_func_name_dict = new WeakMap< Function , string >()
 
 	export function $mol_func_name( func : Function ) : string {
-		if( func.name ) return func.name
-		
 		let name = $mol_func_name_dict.get( func )
 		if( name != null ) return name
 		
-		name = Function.prototype.toString.call( func ).match( /^function ([a-z0-9_$]*)/ )[ 1 ]
+		name = func.name || Function.prototype.toString.call( func ).match( /^function ([a-z0-9_$]*)/ )[ 1 ]
 		$mol_func_name_dict.set( func , name )
 		
 		return name
