636f51d4f0e7b48797250cb893e76abfbcbb49b5
nin-jin <nin-jin@ya.ru>
2017-09-27 22:08:20 +0300

    Fixed bundling.

--- build/build.node.ts
+++ build/build.node.ts
@@ -2,6 +2,26 @@ declare var process : any
 
 namespace $ {
 	
+	export function $mol_build_start( paths : string[] ) {
+		var build = $mol_build.relative( '.' )
+		if( paths.length > 0 ) {
+			process.argv.slice( 2 ).forEach(
+				( path : string )=> {
+					path = build.root().resolve( path ).path()
+					build.bundle( { path } ).valueOf()
+				}
+			)
+		} else {
+			build.server().express()
+		}
+	}
+	
+	setImmediate(
+		()=> {
+			$mol_build_start( process.argv.slice( 2 ) )
+		}
+	)
+	
 	export class $mol_build extends $mol_object {
 		
 		@ $mol_mem_key
