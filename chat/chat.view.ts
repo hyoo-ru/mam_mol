4e69cad175f6d968791f1846a1e1014b87ee9286
nin-jin <nin-jin@ya.ru>
2017-10-12 23:52:14 +0300

    Added $mol_chat - GitHub based chat. Comments adding working on mol.js.org only now.

---
+++ chat/chat.view.ts
@@ -0,0 +1,55 @@
+namespace $.$$ {
+
+	export class $mol_chat extends $.$mol_chat {
+
+		@ $mol_mem
+		posts_data() {
+			return this.issue().comments().items()
+		}
+
+		posts() {
+			return this.posts_data().map( ( _ , index )=> this.Post( index ) )
+		}
+
+		post_user_ava( index: number ) {
+			return this.posts_data()[ index ].user().avatar()
+		}
+
+		post_user_name( index: number ) {
+			return this.posts_data()[ index ].user().name()
+		}
+
+		post_user_link( index: number ) {
+			return this.posts_data()[ index ].user().link()
+		}
+
+		post_body( index: number ) {
+			return this.posts_data()[ index ].text()
+		}
+
+		post_updated( index: number ) {
+			return this.posts_data()[ index ].moment_updated()
+		}
+
+		add_submit_enabled() {
+			return this.add_body().trim().length > 0
+		}
+
+		@ $mol_mem
+		adding( post : $mol_github_comment ) {
+			if( !post ) return
+
+			post = this.issue().comments().add( post ).valueOf() as $mol_github_comment
+			
+			this.add_body( '' )
+			
+			return post
+		}
+
+		add() {
+			this.adding( $mol_github_comment.make({ text : ()=> this.add_body() }) )
+		}
+
+	}
+
+}
