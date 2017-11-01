a2f481c913bace6005c50150cdf7a1fc94dc8b12
nin-jin <nin-jin@ya.ru>
2017-11-01 18:48:29 +0300

    $mol_pref_uibench: refactoring

--- perf/uibench/uibench.view.ts
+++ perf/uibench/uibench.view.ts
@@ -8,171 +8,32 @@ namespace $.$$ {
 		}
 
 		@ $mol_mem
-		stateTable() {
+		table_state() {
 			return this.state().table
 		}
 
 		@ $mol_mem
-		stateAnim() {
+		anim_state() {
 			return this.state().anim
 		}
 
 		@ $mol_mem
-		stateTree() {
+		tree_state() {
 			return this.state().tree
 		}
 
 		@ $mol_mem
-		page() : $mol_view {
-			switch( this.state().location ) {
-				case 'table' : return this.table()
-				case 'anim' : return this.anim()
-				case 'tree' : return this.tree()
+		location() {
+			return this.state().location
+		}
+
+		@ $mol_mem
+		sub() {
+			switch( this.location() ) {
+				case 'table' : return [ this.Table() ]
+				case 'anim' : return [ this.Anim() ]
+				case 'tree' : return [ this.Tree() ]
 			}
-			return null
-		}
-
-	}
-
-	export class $mol_perf_uibench_table extends $.$mol_perf_uibench_table {
-
-		state() {
-			return { items : [] as any[] }
-		}
-		
-		rows() {
-			return this.state().items.map( ( v : any , i : number )=> this.rower( i ) )
-		}
-
-		@ $mol_mem_key
-		rower( id : number ) {
-			return $mol_perf_uibench_table_row.make({
-				state : ()=> this.state().items[ id ] || [] ,
-			})
-		}
-
-	}
-
-	export class $mol_perf_uibench_table_row extends $.$mol_perf_uibench_table_row {
-
-		state() {
-			return { props : [] as any[] , active : false , id : 0 }
-		}
-		
-		headerText() {
-			return '#' + this.id()
-		}
-
-		id() {
-			return this.state().id
-		}
-
-		className() {
-			return super.className() + ( this.state().active ? ' active' : '' )
-		}
-		
-		cells() {
-			return ( this.state().props || [] ).map( ( v : any , j : number )=> this.cell( j ) )
-		}
-
-		@ $mol_mem_key
-		cell( id : number ) {
-			return $mol_perf_uibench_table_cell.make({
-				text : ()=> this.state().props[ id ] ,
-			})
-		}
-		
-	}
-
-	export class $mol_perf_uibench_table_cell extends $.$mol_perf_uibench_table_cell {
-
-		event_click( next? : Event ) {
-			console.log( 'Click' , this.text() )
-			next.preventDefault()
-			next.stopPropagation()
-		}
-		
-	}
-
-	export class $mol_perf_uibench_anim extends $.$mol_perf_uibench_anim {
-		
-		state() {
-			return { items : [] as any[] }
-		}
-		
-		items() {
-			return this.state().items.map( ( v : any , i : number )=> this.item( i ) )
-		}
-		
-		@ $mol_mem_key
-		item( i : number ) {
-			return $mol_perf_uibench_anim_box.make({
-				state : ()=> this.state().items[ i ] || { id : '' , time : 0 } ,
-			})
-		}
-		
-	}
-
-	export class $mol_perf_uibench_anim_box extends $.$mol_perf_uibench_anim_box {
-
-		state() {
-			return { id : '' , time : 0 }
-		}
-
-		id() {
-			return this.state().id
-		}
-
-		time() {
-			return this.state().time
-		}
-
-		styleRadius() {
-			return `${this.time() % 10}px`
-		}
-
-		styleColor() {
-			return `rgba(0,0,0,${0.5 + ((this.time() % 10) / 10)})`
-		}
-
-	}
-
-	export class $mol_perf_uibench_tree extends $.$mol_perf_uibench_tree {
-
-		state() {
-			return { root : null as any }
-		}
-
-		stateRoot() {
-			return this.state().root || { children : [] }
-		}
-		
-	}
-
-	export class $mol_perf_uibench_tree_branch extends $.$mol_perf_uibench_tree_branch {
-
-		state() {
-			return { children : [] as any[] }
-		}
-
-		sub() : $mol_view[] {
-			return ( this.state().children || [] ).map( ( child : any , i : number )=> {
-				return child.container ? this.branch( i ) : this.leaf( i )
-			} )
-		}
-
-		@ $mol_mem_key
-		branch( i : number ) {
-			return $mol_perf_uibench_tree_branch.make({
-				state : ()=> this.state().children[ i ] || { children : [] }
-			})
-		}
-
-		@ $mol_mem_key
-		leaf( i : number ) {
-			return $mol_perf_uibench_tree_leaf.make({
-				text : ()=> ( ( this.state().children || [] )[ i ] || {} ).id
-			})
 		}
 
 	}
