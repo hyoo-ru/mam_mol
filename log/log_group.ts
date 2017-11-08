7665996d1f04a56eb83e0baa4cf920790620ba1c
nin-jin <nin-jin@ya.ru>
2017-11-08 19:18:17 +0300

    $mol_log_group: collapse only when filter is empty, optimization when logs disabled.

--- log/log_group.ts
+++ log/log_group.ts
@@ -3,13 +3,17 @@ namespace $ {
 	export function $mol_log_group< Task extends Function >( name : string , task : Task ) {
 		
 		return function $mol_log_group_wrapper( ... args : any [] ) {
+			const filter = $mol_log_filter()
+			if( filter == null ) return task.apply( this , args )
+
 			let started = false
 			let prev = $mol_log_context()
 
 			$mol_log_context( ()=> {
 				if( prev ) prev()
 				started = true
-				console.groupCollapsed( name )
+				if( filter ) console.group( name )
+				else console.groupCollapsed( name )
 				$mol_log_context( prev = null )
 			} )
 			
