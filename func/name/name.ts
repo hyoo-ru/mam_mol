0850f2868b347bb17597271adf9db04b3405a59e
nin-jin <nin-jin@ya.ru>
2017-11-24 10:57:58 +0300

    $mol_func_name: Node 6 compatibility (classes has not `name` like functions).

--- func/name/name.ts
+++ func/name/name.ts
@@ -5,8 +5,7 @@ namespace $ {
 	export function $mol_func_name( func : Function ) : string {
 		let name = $mol_func_name_dict.get( func )
 		if( name != null ) return name
-		
-		name = func.name || Function.prototype.toString.call( func ).match( /^function ([a-z0-9_$]*)/ )[ 1 ]
+		name = func.name || Function.prototype.toString.call( func ).match( /([a-z0-9_$]*) ?(\(|\{|extends)/ )[ 1 ]
 		$mol_func_name_dict.set( func , name )
 		
 		return name
