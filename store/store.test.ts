5a4508eb9627d8870fae9bc341980619131a101d
nin-jin <nin-jin@ya.ru>
2018-02-06 20:22:52 +0300

    $mol_store: reactive store with lens.

---
+++ store/store.test.ts
@@ -0,0 +1,101 @@
+namespace $ {
+
+	$mol_test({
+
+		'default data' () {
+
+			const store = new $mol_store({
+				foo : 1 ,
+				bar : 2 ,
+			})
+
+			$mol_assert_equal( store.data().foo , 1 )
+			$mol_assert_equal( store.data().bar , 2 )
+
+		} ,
+
+		'safe reference' () {
+
+			const foo = { foo : 1 }
+			const bar = { bar : 1 }
+
+			const store = new $mol_store({ foo , bar })
+
+			store.data({
+				foo : { foo : 1 } ,
+				bar : { bar : 3 } ,
+			})
+
+			$mol_assert_equal( store.data().foo , foo )
+			$mol_assert_unique( store.data().bar , bar )
+
+		} ,
+
+		'get and set by key' () {
+
+			const store = new $mol_store({
+				foo : 1 ,
+			})
+
+			$mol_assert_equal( store.value( 'foo' ) , 1 )
+
+			store.value( 'foo' , 2 )
+
+			$mol_assert_equal( store.value( 'foo' ) , 2 )
+
+		} ,
+
+		'get and set by lens' () {
+
+			const store = new $mol_store({
+				foo : 1 ,
+			})
+
+			const lens = store.sub( 'foo' )
+
+			$mol_assert_equal( lens.data() , 1 )
+
+			lens.data( 2 )
+
+			$mol_assert_equal( lens.data() , 2 )
+
+		} ,
+
+		'views and actions' () {
+
+			const Person = class extends $mol_store<{ name : { first : string , last : string } }> {
+				
+				get full_name() {
+					const name = this.value( 'name' )
+					return name.first + ' ' + name.last
+				}
+
+				swap_names() {
+					const name = this.value( 'name' )
+					this.value( 'name' , {
+						first : name.last ,
+						last : name.first ,
+					} )
+				}
+
+			}
+
+			const store = new Person({
+				name : {
+					first : 'Foo' ,
+					last : 'Bar' ,
+				} ,
+			})
+
+			$mol_assert_equal( store.full_name , 'Foo Bar' )
+
+			store.swap_names()
+
+			$mol_assert_equal( store.full_name , 'Bar Foo' )
+
+		} ,
+
+
+	})
+
+}
---
+++ store/store.ts
@@ -0,0 +1,40 @@
+namespace $ {
+
+	export class $mol_store< Data > extends $mol_object {
+
+		constructor( data? : Data ) {
+			super()
+			if( data !== undefined ) this.data( data )
+		}
+
+		@ $mol_mem
+		data( next? : Data ) {
+			return next
+		}
+
+		value< Key extends keyof Data >( key : Key , next? : Data[ Key ] ) {
+			return this.sub( key ).data( next )
+		}
+
+		sub< Key extends keyof Data >( key : Key ) {
+			return this.lens(
+				data => data[ key ] ,
+				( data , next )=> Object.assign( {} , data , { [ key ] : next } ) ,
+			)
+		}
+
+		lens< Value >(
+			get : ( data : Data )=> Value ,
+			set? : ( data : Data , next? : Value )=> Data ,
+		) {
+			const sub = new $mol_store< Value >()
+			sub.data = next => {
+				if( next == undefined ) return get( this.data() )
+				return get( this.data( set( this.data() , next ) ) )
+			}
+			return sub
+		}
+
+	}
+
+}
