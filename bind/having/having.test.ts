22f1b02261342e29978684d34ceb3ba0c26a029a
nin-jin <nin-jin@ya.ru>
2018-03-08 12:28:09 +0300

    $mol_bind_having: Test for unidirectional bind.

--- bind/having/having.test.ts
+++ bind/having/having.test.ts
@@ -26,7 +26,37 @@ namespace $ {
 			app.Input.value = 'Jack'
 			$mol_assert_equal( app.name , 'Jack' )
 			
-		}
+		} ,
+
+		'unidirectional bind'() {
+
+			class View {
+				text = ''
+			}
+
+			class App {
+
+				name = 'Jin'
+
+				get view_text() {
+					return this.name
+				}
+
+				@ $mol_bind_having({ text : 'view_text' })
+				View = new View
+
+			}
+
+			const app = new App
+			$mol_assert_equal( app.View.text , 'Jin' )
+
+			$mol_assert_fail( ()=> app.View.text = 'John' , TypeError )
+			$mol_assert_equal( app.View.text , 'Jin' )
+			
+			app.name = 'Jack'
+			$mol_assert_equal( app.View.text , 'Jack' )
+			
+		} ,
 		
 	})
 
