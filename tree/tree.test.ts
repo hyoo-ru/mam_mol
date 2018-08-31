c009b7ad1c0faaeae47287a06177905e95f4a93e
nin-jin <nin-jin@ya.ru>
2018-08-31 10:00:06 +0500

    $mol_tree: fixed json integration

--- tree/tree.test.ts
+++ tree/tree.test.ts
@@ -25,7 +25,23 @@ namespace $ {
 
 			$mol_assert_equal( $mol_tree.fromString( 'a b c d' ).insert( new $mol_tree , null , null , null ).toString() , 'a b \\\n' )
 			$mol_assert_equal( $mol_tree.fromString( 'a b' ).insert( new $mol_tree , null , null , null , null ).toString() , 'a b \\\n\t\\\n' )
-		}
+		} ,
+
+		'fromJSON'() {
+			$mol_assert_equal( $mol_tree.fromJSON([]).toString() , '/\n' )
+			$mol_assert_equal( $mol_tree.fromJSON([ false , true ]).toString() , '/\n\tfalse\n\ttrue\n' )
+			$mol_assert_equal( $mol_tree.fromJSON([ 0 , 1 , 2.3 ]).toString() , '/\n\t0\n\t1\n\t2.3\n' )
+			$mol_assert_equal( $mol_tree.fromJSON([ '' , 'foo' , 'bar\nbaz' ]).toString() , '/\n\t\\\n\t\\foo\n\t\\\n\t\t\\bar\n\t\t\\baz\n' )
+			$mol_assert_equal( $mol_tree.fromJSON({ 'foo' : false , 'bar\nbaz' : 'lol' }).toString() , '*\n\tfoo false\n\t\\\n\t\t\\bar\n\t\t\\baz\n\t\t\\lol\n' )
+		} ,
+		
+		'toJSON'() {
+			$mol_assert_equal( JSON.stringify( $mol_tree.fromString( '/\n' ).sub[0] ) , '[]' )
+			$mol_assert_equal( JSON.stringify( $mol_tree.fromString( '/\n\tfalse\n\ttrue\n' ).sub[0] ) , '[false,true]' )
+			$mol_assert_equal( JSON.stringify( $mol_tree.fromString( '/\n\t0\n\t1\n\t2.3\n' ).sub[0] ) , '[0,1,2.3]' )
+			$mol_assert_equal( JSON.stringify( $mol_tree.fromString( '/\n\t\\\n\t\\foo\n\t\\\n\t\t\\bar\n\t\t\\baz\n' ).sub[0] ) , '["","foo","bar\\nbaz"]' )
+			$mol_assert_equal( JSON.stringify( $mol_tree.fromString( '*\n\tfoo false\n\t\\\n\t\t\\bar\n\t\t\\baz\n\t\t\\lol\n' ).sub[0] ) , '{"foo":false,"bar\\nbaz":"lol"}' )
+		} ,
 		
 	} )	
 }
