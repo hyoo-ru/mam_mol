6c0f8a9d264edc1447d967c762fb0544675962e4
nin-jin <nin-jin@ya.ru>
2018-09-14 06:35:48 +0300

    $mol_func_name_from - set up name of anon func by name of another one

--- func/name/name.ts
+++ func/name/name.ts
@@ -5,10 +5,16 @@ namespace $ {
 	export function $mol_func_name( func : Function ) : string {
 		let name = $mol_func_name_dict.get( func )
 		if( name != null ) return name
+
 		name = func.name || Function.prototype.toString.call( func ).match( /([a-z0-9_$]*) ?(\(|\{|extends)/ )[ 1 ]
 		$mol_func_name_dict.set( func , name )
 		
 		return name
+	}
+
+	export function $mol_func_name_from< Target extends Function >( target : Target , source : Function ) {
+		$mol_func_name_dict.set( target , $mol_func_name( source ) )
+		return target
 	}
 
 }
