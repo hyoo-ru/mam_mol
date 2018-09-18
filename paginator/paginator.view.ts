ddd4713388c5d5218e170359a6dd8fe1f9371bd2
nin-jin <nin-jin@ya.ru>
2018-09-18 22:58:22 +0300

    $mol_paginator - pages switcher

---
+++ paginator/paginator.view.ts
@@ -0,0 +1,15 @@
+namespace $.$$ {
+
+	export class $mol_paginator extends $.$mol_paginator {
+		
+		backward() {
+			this.value( this.value() - 1 )
+		}
+
+		forward() {
+			this.value( this.value() + 1 )
+		}
+
+	}
+
+}
