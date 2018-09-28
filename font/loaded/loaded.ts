8f937a0d70efa691896f6a56696d843ab59cbea3
nin-jin <nin-jin@ya.ru>
2018-09-28 21:44:23 +0300

    $mol_font_wait: added timeout

--- font/loaded/loaded.ts
+++ font/loaded/loaded.ts
@@ -2,8 +2,9 @@ namespace $ {
 
 	export const $mol_font_loaded_cache = new Set< string >()
 
-	export function $mol_font_loaded( face : string ) {
+	export function $mol_font_loaded( face : string , next? : boolean ) {
 		
+		if( next ) $mol_font_loaded_cache.add( face )
 		if( $mol_font_loaded_cache.has( face ) ) return true
 
 		const mono = $mol_font_measure( 16 , face + ', monospace' , 'W' )
--- font/wait/wait.ts
+++ font/wait/wait.ts
@@ -1,8 +1,16 @@
 namespace $ {
 
-	export function $mol_font_wait( face : string ) {
-		if( $mol_font_loaded( face ) ) return
-		$mol_state_time.now()
+	export const $mol_font_wait_started = new Map< string , number >()
+
+	export function $mol_font_wait( face : string , timeout = 5000 ) {
+		if( $mol_font_loaded( face ) ) return true
+
+		const time = $mol_state_time.now()
+		const started = $mol_font_wait_started.get( face ) || time
+		$mol_font_wait_started.set( face , started )
+
+		if( time - started > timeout ) return $mol_font_loaded( face , true )
+		
 		throw new $mol_atom_wait( `Wait font face: ${ face }` )
 	}
 
