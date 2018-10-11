cc69a65c647de17ab24f87f72ea557a9e049328e
nin-jin <nin-jin@ya.ru>
2018-10-11 21:17:44 +0300

    $mol_func_name: less crunches.

--- func/name/name.ts
+++ func/name/name.ts
@@ -1,19 +1,11 @@
 namespace $ {
 	
-	export var $mol_func_name_dict = new WeakMap< Function , string >()
-
 	export function $mol_func_name( func : Function ) : string {
-		let name = $mol_func_name_dict.get( func )
-		if( name != null ) return name
-
-		name = func.name || Function.prototype.toString.call( func ).match( /([a-z0-9_$]*) ?(\(|\{|extends)/ )[ 1 ]
-		$mol_func_name_dict.set( func , name )
-		
-		return name
+		return func.name
 	}
 
 	export function $mol_func_name_from< Target extends Function >( target : Target , source : Function ) {
-		$mol_func_name_dict.set( target , $mol_func_name( source ) )
+		Object.defineProperty( target , 'name' , { value : source.name } )
 		return target
 	}
 
