6ef019349db099d38606fbcd58de86bdbce4e80b
nin-jin <nin-jin@ya.ru>
2018-10-19 21:02:11 +0300

    $mol_plugin: independence of $mol_view

--- plugin/plugin.view.ts
+++ plugin/plugin.view.ts
@@ -4,13 +4,17 @@ namespace $.$$ {
 
 		@ $mol_mem
 		dom_node() {
-			const node = ( this.object_host() as $mol_view ).dom_node()
+			const node = this.object_host().dom_node()
 
 			$mol_dom_render_attributes( node , this.attr_static() )
 			$mol_dom_render_events( node , this.event() )
 			$mol_dom_render_events_async( node , this.event_async() )
 
 			return node
+		}
+
+		render() {
+			return this.dom_node()
 		}
 		
 	}
