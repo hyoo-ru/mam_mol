3197abbdec897f505663ab7326edc4d1272cd3b7
nin-jin <nin-jin@ya.ru>
2018-10-19 20:46:33 +0300

    $mol_object: functional initializer.

--- object2/object2.ts
+++ object2/object2.ts
@@ -1,15 +1,15 @@
 namespace $ {
 
 	export namespace $$ { let $ }
-	
+
 	export class $mol_object2 {
 		
 		static $ = $ as $mol_ambient_context
 		$ = this.constructor['$'] as typeof $mol_object2.$
 
-		public static make< Instance >( this : { new() : Instance } , config : Partial< Instance > ) : Instance {
+		public static make< Instance >( this : { new() : Instance } , init? : ( instance : Instance )=> void  ) : Instance {
 			const instance = new this
-			for( let key in config ) instance[ key ] = config[ key ]
+			if( init ) init( instance )
 			return instance
 		}
 		
