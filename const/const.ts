1fe2679a9b0571f58bbe8ca2e1871abb464a90d1
nin-jin <nin-jin@ya.ru>
2018-11-14 22:07:02 +0300

    $mol_const: value as toStringTag

--- const/const.ts
+++ const/const.ts
@@ -2,6 +2,7 @@ namespace $ {
 	export function $mol_const< Value >( value : Value ) {
 		var getter = <{ () : Value , '()' : Value }> ( () => value )
 		getter['()'] = value
+		getter[ Symbol.toStringTag ] = value
 		return getter
 	}
 }
