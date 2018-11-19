ed70b4a71956626f5cea0a7c2dfdf21a180ef23d
nin-jin <nin-jin@ya.ru>
2018-11-19 22:09:15 +0300

    $mol_font_wait: don't depends on reactivity.

--- font/wait/wait.ts
+++ font/wait/wait.ts
@@ -2,24 +2,26 @@ namespace $ {
 
 	export class $mol_font_wait extends $mol_object2 {
 
-		@ $mol_mem_key
 		static face( config : { face : string , text : string } ) {
 
 			if( this.$.$mol_font_loaded( config ) ) return true
-
-			const atom = this.$.$mol_atom_current()
 			
-			const check = ()=> {
-				if( this.$.$mol_font_loaded( config ) ) atom.push( true )
-				else this.$.requestAnimationFrame( check )				
-			}
+			throw new Promise( ( done , fail )=> {
 
-			this.$.requestAnimationFrame( check )
-
-			this.$.setTimeout( ()=> $mol_font_loaded( config , true ) , 5000 )
+				const check = ()=> {
+					if( this.$.$mol_font_loaded( config ) ) done()
+					else this.$.requestAnimationFrame( check )				
+				}
+	
+				this.$.requestAnimationFrame( check )
+	
+				this.$.setTimeout( ()=> {
+					this.$.$mol_font_loaded( config , true )
+					fail( new Error( `Font loading timeout ${ config.face }` ) )
+				} , 5000 )
+				
+			} )
 			
-			throw new this.$.$mol_atom_wait( `Wait font face: ${ config.face }` )
-
 		}
 
 	}
