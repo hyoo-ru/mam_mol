91e7f4a0b604d419515f75c246c464a95cc8ade1
nin-jin <nin-jin@ya.ru>
2018-11-22 22:32:44 +0300

    $mol_atom2_poll: digest -> monitor

--- atom2/poll/poll.test.ts
+++ atom2/poll/poll.test.ts
@@ -6,26 +6,26 @@ namespace $ {
 
 			let state = 1
 
-			const digest = $.$mol_atom2_poll( ()=> state )
-			$mol_assert_equal( digest.get() , 1 )
+			const monitor = $.$mol_atom2_poll( ()=> state )
+			$mol_assert_equal( monitor.get() , 1 )
 			
 			state = 2
-			$mol_assert_equal( digest.get() , 1 )
+			$mol_assert_equal( monitor.get() , 1 )
 				
 			$.$mol_after_mock_warp()
-			$mol_assert_equal( digest.get() , 2 )
+			$mol_assert_equal( monitor.get() , 2 )
 			
 			state = 3
-			$mol_assert_equal( digest.get() , 2 )
+			$mol_assert_equal( monitor.get() , 2 )
 			
-			digest.destructor()
-			$mol_assert_equal( $mol_atom2_value( ()=> digest.get() ) , undefined )
+			monitor.destructor()
+			$mol_assert_equal( $mol_atom2_value( ()=> monitor.get() ) , undefined )
 			
 			$.$mol_after_mock_warp()
-			$mol_assert_equal( $mol_atom2_value( ()=> digest.get() ) , undefined )
+			$mol_assert_equal( $mol_atom2_value( ()=> monitor.get() ) , undefined )
 			
 			state = 4
-			$mol_assert_equal( digest.get() , 4 )
+			$mol_assert_equal( monitor.get() , 4 )
 
 		} ,
 
