0d1dfc26b9ea9cc7483af506f1d18eabf307c1d2
nin-jin <nin-jin@ya.ru>
2019-01-19 12:42:37 +0300

    $mol_fail_catched: removed because unused and depends on es6

--- fail/fail.ts
+++ fail/fail.ts
@@ -1,7 +1,5 @@
 namespace $ {
 
-	export let $mol_fail_catched = new WeakSet< any >()
-
 	export function $mol_fail_hidden( error : any ) : never {
 		throw error /// Use 'Never Pause Here' breakpoint in DevTools
 	}
