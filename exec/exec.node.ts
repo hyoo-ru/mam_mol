8443a5e89fb188e8f8481ea7d66545718642b6f3
nin-jin <nin-jin@ya.ru>
2019-01-20 09:40:09 +0300

    $mol_exec: hide command until error

--- exec/exec.node.ts
+++ exec/exec.node.ts
@@ -1,15 +1,13 @@
 namespace $ {
 	export function $mol_exec( dir : string , command : string , ...args : string[] ) {
 		
-		console.log( `${dir}> ${command} ${args.join( ' ' )}` )
-		
 		var res = $node.child_process.spawnSync(
 			command ,
 			args ,
 			{ cwd : dir , stdio : [ 'pipe' , process.stdout , 'pipe' ] }
 		)
 		
-		if( res.status ) throw new Error( res.stderr.toString() )
+		if( res.status ) throw new Error( `${dir}> ${command} ${args.join( ' ' )}\n ${ res.stderr }` )
 		
 		return res
 	}
