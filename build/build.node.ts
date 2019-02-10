cf4a02d6adcb8fb61602e5c096c853ab290054ee
nin-jin <nin-jin@ya.ru>
2019-02-10 22:30:29 +0300

    $mol_build +1

--- build/build.node.ts
+++ build/build.node.ts
@@ -517,7 +517,7 @@ namespace $ {
 			
 			var concater = new $node[ 'concat-with-sourcemaps' ]( true , target.name() , '\n;\n' )
 			if( bundle === 'node' ) {
-				concater.add( '' , 'require( "source-map-support" ).install(); var exports = void 0;\n' )
+				concater.add( '' , 'require'+'( "source-map-support" ).install(); var exports = void 0;\n' )
 			}
 			
 			sources.forEach(
@@ -574,7 +574,7 @@ namespace $ {
 			
 			var sources = this.sourcesJS( { path , exclude : exclude.filter( ex => ex !== 'test' && ex !== 'dev' ) } )
 			if( bundle === 'node' ) {
-				concater.add( '' , 'require( "source-map-support" ).install()\n' )
+				concater.add( '' , 'require'+'( "source-map-support" ).install()\n' )
 			} else {
 				var sourcesNoTest = this.sourcesJS( { path , exclude } )
 				sources = sources.filter( src => sourcesNoTest.indexOf( src ) === -1 )
