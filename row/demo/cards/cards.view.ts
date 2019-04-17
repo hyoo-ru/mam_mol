3a7109fa510078eec08fd1c2565a3d1600f79a9e
Stefan Zerkalica <zerkalica@gmail.com>
2019-04-17 20:17:12 +0300

    fix(row): renaming

--- row/demo/cards/cards.view.ts
+++
@@ -1,8 +0,0 @@
-namespace $.$$ {
-	export class $mol_row_demo_cards extends $.$mol_row_demo_cards {
-		cards() { return $mol_range2( id => this.Card( id ) , ()=> this.count() ) }
-		card_title(id: string) {
-			return $mol_stub_product_name()
-		}
-	}
-}
