737e57f09f8e2da24c47689cd73e6477a89e2846
nin-jin <nin-jin@ya.ru>
2019-06-02 20:56:17 +0300

    $mol_vector_matrix - 2d vectors

--- vector/vector.test.ts
+++ vector/vector.test.ts
@@ -1,7 +1,7 @@
 namespace $ {
 	$mol_test({
 
-		'Limit'() {
+		'Vector limiting'() {
 
 			let point = new $mol_vector_3d( 7 , 10 , 13 )
 			const res = point.limited( [ 1 , 5 ] , [ 15 , 20 ] , [ 5 , 10 ] )
@@ -12,7 +12,7 @@ namespace $ {
 
 		} ,
 
-		'Add'() {
+		'Vector adding'() {
 
 			let point = new $mol_vector_3d( 1 , 2 , 3 )
 			let res = point.added( 5 , 10 , 15 )
@@ -23,7 +23,7 @@ namespace $ {
 
 		} ,
 
-		'Mult'() {
+		'Vector multiplying'() {
 
 			let point = new $mol_vector_3d( 2 , 3 , 4 )
 			let res = point.multed( 5 , 2 , -2 )
@@ -31,6 +31,34 @@ namespace $ {
 			$mol_assert_equal( res.x , 10 )
 			$mol_assert_equal( res.y , 6 )
 			$mol_assert_equal( res.z , -8 )
+
+		} ,
+
+		'Matrix adding'() {
+
+			let matrix = new $mol_vector_matrix<2,3>( ...[ [ 1 , 2 ] , [ 3 , 4 ] , [ 5 , 6 ] ] as const )
+			let res = matrix.added2( ...[ [ 10 , 20 ] , [ 30 , 40 ] , [ 50 , 60 ] ] as const )
+
+			$mol_assert_equal( res[0][0] , 11 )
+			$mol_assert_equal( res[0][1] , 22 )
+			$mol_assert_equal( res[1][0] , 33 )
+			$mol_assert_equal( res[1][1] , 44 )
+			$mol_assert_equal( res[2][0] , 55 )
+			$mol_assert_equal( res[2][1] , 66 )
+
+		} ,
+
+		'Matrix multiplying'() {
+
+			let matrix = new $mol_vector_matrix<2,3>( ...[ [ 2 , 3 ] , [ 4 , 5 ] , [ 6 , 7 ] ] as const )
+			let res = matrix.multed2( ...[ [ 2 , 3 ] , [ 4 , 5 ] , [ 6 , 7 ] ] as const )
+
+			$mol_assert_equal( res[0][0] , 4 )
+			$mol_assert_equal( res[0][1] , 9 )
+			$mol_assert_equal( res[1][0] , 16 )
+			$mol_assert_equal( res[1][1] , 25 )
+			$mol_assert_equal( res[2][0] , 36 )
+			$mol_assert_equal( res[2][1] , 49 )
 
 		} ,
 
--- vector/vector.ts
+++ vector/vector.ts
@@ -6,30 +6,22 @@ namespace $ {
 
 		constructor( ... values : Value[] & { length : Length } ) { super( ... values ) }
 		
-		limited( this : $mol_vector< number , Length > , ... limit : [ number , number ][] & { length : Length } ) : this {
-			
-			return new ( this.constructor as any )( ... this.map( ( value , index )=> {
-				
-				const range = new $mol_vector_range( ... limit[ index ] )
-				
-				if( value < range.min ) return range.min
-				if( value > range.max ) return range.max
-				
-				return value
-			} ) )
-			
+		map : < Res >( convert : ( value : Value , index : number , array : this ) => Res , self? : any )=> $mol_vector< Res , Length >
+
+		merged< Patch >( patches : readonly Patch[] & { length : Length } , combine : ( value : Value , patch : Patch ) => Value ) : this {
+			return this.map( ( value , index )=> combine( value , patches[ index ] ) ) as any
 		}
 
-		added( this : $mol_vector< number , Length > , ... diff : number[] & { length : Length } ) : this {
-			
-			return new ( this.constructor as any )( ... this.map( ( value , index )=> value + diff[ index ] ) )
-			
+		limited( this : $mol_vector< number , Length > , ... limits : readonly [ number , number ][] & { length : Length } ) : this {
+			return this.merged( limits , ( value , [ min , max ] )=> ( value < min ) ? min : ( value > max ) ? max : value ) as any
 		}
 
-		multed( this : $mol_vector< number , Length > , ... diff : number[] & { length : Length } ) : this {
-			
-			return new ( this.constructor as any )( ... this.map( ( value , index )=> value * diff[ index ] ) )
-			
+		added( this : $mol_vector< number , Length > , ... diff : readonly number[] & { length : Length } ) : this {
+			return this.merged( diff , ( a , b )=> a + b ) as any
+		}
+
+		multed( this : $mol_vector< number , Length > , ... mults : readonly number[] & { length : Length } ) : this {
+			return this.merged( mults , ( a , b )=> a * b ) as any
 		}
 
 	}
@@ -49,7 +41,7 @@ namespace $ {
 	export class $mol_vector_3d< Value > extends $mol_vector< Value , 3 > {
 		[0]: Value
 		[1]: Value
-		[3]: Value
+		[2]: Value
 		get x() { return this[0] }
 		get y() { return this[1] }
 		get z() { return this[2] }
@@ -61,6 +53,25 @@ namespace $ {
 		[1]: Value
 		get min() { return this[0] }
 		get max() { return this[1] }
+	}
+
+	export class $mol_vector_matrix<
+		Width extends number ,
+		Height extends number ,
+	> extends $mol_vector< readonly number[] & { length : Width } , Height > {
+
+		added2(
+			... diff : readonly ( readonly number[] & { length : Width } )[] & { length : Height }
+		) : this {
+			return this.merged( diff , ( a , b )=> a.map( ( a2 , index ) => a2 + b[ index ] ) as any ) as any
+		}
+
+		multed2(
+			... diff : readonly ( readonly number[] & { length : Width } )[] & { length : Height }
+		) : this {
+			return this.merged( diff , ( a , b )=> a.map( ( a2 , index ) => a2 * b[ index ] ) as any ) as any
+		}
+
 	}
 
 }
