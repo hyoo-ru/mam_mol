7fd7aa64abe72b8a3a179a00b3d06e8204341192
nin-jin <nin-jin@ya.ru>
2019-06-02 15:15:23 +0300

    $mol_build: nested packs support

--- build/build.node.ts
+++ build/build.node.ts
@@ -348,43 +348,51 @@ namespace $ {
 		}
 		
 		@ $mol_mem_key
-		packEnsure( name : string ) {
-			var mapping = this.packMapping()
+		modEnsure( path : string ) {
+
+			var mod = $mol_file.absolute( path )
+			if( mod === this.root() ) return false
+
+			var parent = mod.parent()
+			this.modEnsure( parent.path() )
 			
-			var pack = this.root().resolve( name )
-			if( pack.exists() ) {
-				if( pack.resolve( '.git' ).exists() ) {
+			var mapping = this.packMapping( parent.path() )
+			
+			if( mod.exists() ) {
+				if( mod.resolve( '.git' ).exists() ) {
 					try {
 						//$mol_exec( pack.path() , 'git' , '--no-pager' , 'fetch' )
-						process.stdout.write( $mol_exec( pack.path() , 'git' , '--no-pager' , 'log' , '--oneline' , 'HEAD..origin/master' ).stdout )
+						process.stdout.write( $mol_exec( mod.path() , 'git' , '--no-pager' , 'log' , '--oneline' , 'HEAD..origin/master' ).stdout )
 					} catch( error ) {
 						console.error( error.message )
 					}
 				}
 				return false
 			}
-			
-			for( let repo of mapping.select( 'pack' , name , 'git' ).sub ) {
-				$mol_exec( this.root().path() , 'git' , 'clone' , repo.value , name )
-				pack.stat( undefined , $mol_atom_force_cache )
+
+			for( let repo of mapping.select( 'pack' , mod.name() , 'git' ).sub ) {
+				console.log( '> git clone' , repo.value , mod.path() )
+				$mol_exec( this.root().path() , 'git' , 'clone' , repo.value , mod.name() )
+				mod.stat( undefined , $mol_atom_force_cache )
 				return true
 			}
 			
-			throw new Error( `Package "${name}" not found` )
+			if( parent === this.root() ) {
+				throw new Error( `Root package "${ mod.relate( this.root() ) }" not found` )
+			}
+
+			return false
 		}
 		
-		modEnsure( path : string ) {
-			var file = $mol_file.absolute( path )
-			var sub = file.relate( this.root() )
-			var name = sub.replace( /\/.*$/ , '' )
+		@ $mol_mem_key
+		packMapping( path : string ) {
+
+			const pack = $mol_file.absolute( path )
+			const file = pack.resolve( './.meta.tree' )
 			
-			return this.packEnsure( name ).valueOf()
-		}
-		
-		@ $mol_mem
-		packMapping() {
-			const file = $mol_file.relative( '.pms.tree' )
+			if( !file.exists() ) return new $mol_tree
 			return $mol_tree.fromString( file.content().toString() , file.path() )
+
 		}
 		
 		@ $mol_mem_key
