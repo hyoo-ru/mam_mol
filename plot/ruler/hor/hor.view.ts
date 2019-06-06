cb31390b08353156e7c817c82474991fede59ff2
Stefan Zerkalica <zerkalica@gmail.com>
2019-06-06 12:43:28 +0300

    refactor: base ruler, vert

--- plot/ruler/hor/hor.view.ts
+++ plot/ruler/hor/hor.view.ts
@@ -1,18 +1,12 @@
 namespace $.$$ {
 	export class $mol_plot_ruler_hor extends $.$mol_plot_ruler_hor {
-		dimensions() {
-			return [
-				[ Number.POSITIVE_INFINITY , Number.POSITIVE_INFINITY ] ,
-				[ Number.NEGATIVE_INFINITY , Number.NEGATIVE_INFINITY ] ,
-			]
-		}
-
 		@ $mol_mem
 		step() {
 			const dims = this.dimensions_expanded()
 			const size = $mol_math_round_expand( ( dims[1][0] - dims[0][0] ) , -1 )
-			const min_width = this.step_width()
 			const [scale_x] = this.scale()
+
+			const min_width = this.step_width()
 			const count = Math.max( 1 , Math.pow( 10 , Math.floor( Math.log( size * scale_x / min_width ) / Math.log( 10 ) ) ) )
 			let step = size / count
 			const step_max = min_width * 2 / scale_x
@@ -41,31 +35,17 @@ namespace $.$$ {
 
 			return next
 		}
-		
+
 		curve() {
 			return this.points()[0].map( point => `M ${ point } 1000 V 0` ).join( ' ' ) || ''
 		}
-		
-		labels() {
-			return this.points()[0].map( ( point , index )=> this.Label( index ) )
-		}
-		
+
 		label_pos_x( index : number ) {
 			return this.points()[0][ index ] + 'px'
 		}
 
-		precision() {
-			const step = this.step()
-			return Math.max( 0 , Math.min( 15 , ( step - Math.floor( step ) ).toString().length - 2 ) )
+		label_pos_y( index : number ) {
+			return this.title_pos_y()
 		}
-
-		label_text( index : number ) {
-			return this.points()[1][ index ].toFixed( this.precision() )
-		}
-		
-		back() {
-			return [ this ]
-		}
-		
 	}
 }
