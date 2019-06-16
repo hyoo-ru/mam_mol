ae32ded8352fe80156967a1664eacd683e63b981
Stefan Zerkalica <zerkalica@gmail.com>
2019-06-16 22:35:08 +0300

    fix: mark hor

--- plot/mark/hor/hor.view.ts
+++ plot/mark/hor/hor.view.ts
@@ -1,47 +1,61 @@
 namespace $.$$ {
 	export class $mol_plot_mark_hor extends $.$mol_plot_mark_hor {
 		@ $mol_mem
-		series_x() {
+		series_x(): number[] {
 			return this.labels().map((val, index) => index)
 		}
-		
+
 		@ $mol_mem
-		points_record() {
-			const count = 10
-			const series_x = this.series_x()
-			const labels = this.labels()
+		labels(): string[] {
 			const precision = this.precision()
+			return this.series_x().map(val => val.toFixed(precision))
+		}
+
+		@ $mol_mem
+		step() {
+			const count = 10
 			const [[viewport_left,], [viewport_right,]] = this.viewport()
-			const step = (viewport_right - viewport_left) / count
+			return (viewport_right - viewport_left) / count
+		}
+
+		@ $mol_mem
+		indexes() {
+			const series_x = this.series_x()
 			const [shift_x, ] = this.shift()
 			const [scale_x, ] = this.scale()
+			const [[viewport_left,], [viewport_right,]] = this.viewport()
+			const step = this.step()
 
 			let current = 0
 
-			const points = [] as number[]
-			const labels_viewport = [] as string[]
+			const indexes = [] as number[]
 			for (let i = 0; i < series_x.length; i++) {
 				const point_x = series_x[i]
 				const scaled_x = shift_x + point_x * scale_x
 				if (scaled_x > viewport_right) continue
 				if (scaled_x < viewport_left) continue
 				if (scaled_x < current) continue
+				indexes.push(i)
 
-				labels_viewport.push(labels.length > i ? labels[i] : point_x.toFixed(precision))
-				points.push(point_x)
 				if (current === 0) current += point_x
 				current += step
 			}
 
-			return {points, labels: labels_viewport} as const
+			return indexes
+
 		}
 
 		points() {
-			return this.points_record().points
+			const [shift_x, ] = this.shift()
+			const [scale_x, ] = this.scale()
+			const series_x = this.series_x()
+
+			return this.indexes().map(index => shift_x + series_x[index] * scale_x)
 		}
 
 		labels_viewport() {
-			return this.points_record().labels
+			const labels = this.labels()
+			return this.indexes().map(index => labels[index])
 		}
 
 		label_text( viewport_index : number ) {
