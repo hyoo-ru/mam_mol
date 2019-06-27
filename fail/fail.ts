2fc5f245dbdcd48a2d5c3495508ac65b3d240b55
КарловскийДА <kard@1c.ru>
2019-06-27 17:56:54 +0300

    $mol_fail_hidden - separated for blackboxing

--- fail/fail.ts
+++ fail/fail.ts
@@ -1,9 +1,5 @@
 namespace $ {
 
-	export function $mol_fail_hidden( error : any ) : never {
-		throw error /// Use 'Never Pause Here' breakpoint in DevTools
-	}
-
 	export function $mol_fail( error : any ) : never {
 		throw error
 	}
