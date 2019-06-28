478a20803d2cf0a3e9404ca787d821457ff3433b
Stefan Zerkalica <zerkalica@gmail.com>
2019-06-29 02:09:03 +0300

    fix: bar viewport as dots

--- plot/bar/bar.view.ts
+++ plot/bar/bar.view.ts
@@ -11,11 +11,6 @@ namespace $.$$ {
 			const [scale_x, scale_y] = this.scale()
 			const points_scaled = [] as (readonly [number, number])[]
 
-			let first_x = null as readonly [number, number] | null
-			let first_y = null as readonly [number, number] | null
-			let last_x = null as readonly [number, number] | null
-			let last_y = null as readonly [number, number] | null
-
 			const series_x = this.series_x()
 			const series_y = this.series_y()
 			for (let i = 0; i < series_x.length; i++) {
@@ -24,41 +19,13 @@ namespace $.$$ {
 					Math.round( shift_y + series_y[i] * scale_y ),
 				] as const
 
-				if (scaled[0] < viewport_left) {
-					first_x = scaled
-					continue
-				}
-
-				if (scaled[1] < viewport_bottom) {
-					first_y = scaled
-					continue
-				}
-
-				if (scaled[0] > viewport_right) {
-					last_x = scaled
-					continue
-				}
-
-				if (scaled[1] > viewport_top) {
-					last_y = scaled
-					continue
-				}
-
-				if (first_x) points_scaled.push(first_x)
-				if (first_y) points_scaled.push(first_y)
+				if (scaled[0] < viewport_left) continue
+				if (scaled[1] < viewport_bottom) continue
+				if (scaled[0] > viewport_right) continue
+				if (scaled[1] > viewport_top) continue
 
 				points_scaled.push(scaled)
-
-				if (last_x) points_scaled.push(last_x)
-				if (last_y) points_scaled.push(last_y)
-
-				first_x = first_y = last_x = last_y = null
 			}
-
-			if (first_x) points_scaled.push(first_x)
-			if (first_y) points_scaled.push(first_y)
-			if (last_x) points_scaled.push(last_x)
-			if (last_y) points_scaled.push(last_y)
 
 			return points_scaled
 		}
