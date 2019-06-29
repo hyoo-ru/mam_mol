9984f982eadcc6de2ba5015eb2118729d9657aae
nin-jin <nin-jin@ya.ru>
2019-06-29 11:28:54 +0300

    $mol_form: fixed ctrl+enter

--- form/form.view.ts
+++ form/form.view.ts
@@ -5,6 +5,10 @@ namespace $.$$ {
 		submit_blocked( ) {
 			return this.form_fields().some( field => field.bid() )
 		}
+
+		keydown( next? : KeyboardEvent ) {
+			if( next.ctrlKey && next.keyCode === $mol_keyboard_code.enter && !this.submit_blocked() ) this.submit( event )
+		}
 		
 	}
 }
