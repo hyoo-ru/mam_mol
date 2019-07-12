d34f182c779bc020688b764c0ce7b6d35feeeee3
Stefan Zerkalica <zerkalica@gmail.com>
2019-07-13 01:12:04 +0300

    fix: text labels in cross

--- plot/mark/cross/cross.view.ts
+++ plot/mark/cross/cross.view.ts
@@ -7,52 +7,58 @@ namespace $.$$ {
 			const [cursor_x, cursor_y] = this.cursor_position()
 			if (Number.isNaN(cursor_x) || Number.isNaN(cursor_y)) return null
 			const graphs = this.graphs()
-			let point: $mol_vector_2d<number> = null
-			let label: $mol_vector_2d<number> = null
+			let index: number = 0
+			let graph: $.$mol_plot_graph = null
+			const [shift_x, shift_y] = this.shift()
+			const [scale_x, scale_y] = this.scale()
 			for (let current of graphs) {
 				const indexes = current.indexes()
 				const series_x = current.series_x()
 				const series_y = current.series_y()
-				const [shift_x, shift_y] = current.shift()
-				const [scale_x, scale_y] = current.scale()
 	
-				for (let index of indexes) {
-					const point_x = shift_x + series_x[index] * scale_x
-					const point_y = shift_y + series_y[index] * scale_y
+				for (let i of indexes) {
+					const point_x = shift_x + series_x[i] * scale_x
+					const point_y = shift_y + series_y[i] * scale_y
 					const diff = (point_x - cursor_x) ** 2 + (point_y - cursor_y) ** 2
 					if (diff < delta) {
 						delta = diff
-						point = new $mol_vector_2d(point_x, point_y)
-						label = new $mol_vector_2d(series_x[index], series_y[index])
+						index = i
+						graph = current
 					}
 				}
 			}
-			if (!point) return null
 
-			return {point, label}
+			if (!graph) return null
+
+			const value = new $mol_vector_2d(graph.series_x()[index], graph.series_y()[index])
+			const scaled = new $mol_vector_2d(shift_x + value.x * scale_x, shift_y + value.y * scale_y)
+
+			return {value, scaled, index}
 		}
 
 		curve() {
 			const nearest = this.nearest()
 			if (!nearest) return ''
-
-			return `M ${nearest.point.x.toFixed(3)} 1000 V 0 M 0 ${nearest.point.y.toFixed(3)} H 2000`
+			return `M ${nearest.scaled.x.toFixed(3)} 1000 V 0 M 0 ${nearest.scaled.y.toFixed(3)} H 2000`
 		}
 
 		title_x() {
 			const nearest = this.nearest()
 			if (!nearest) return ''
 
-			return String(nearest.label.x)
+			const labels = this.labels()
+
+			if (labels.length > nearest.index) return labels[nearest.index]
+
+			return String(nearest.value.x)
 		}
 
 		title_x_pos_x() {
 			const nearest = this.nearest()
 			if (!nearest) return '0'
-
 			const width = this.text_width(this.title_x())
 
-			return (nearest.point.x - width / 2).toFixed(3)
+			return (nearest.scaled.x - width / 2).toFixed(3)
 		}
 
 		title_x_pos_y() {
@@ -68,14 +74,14 @@ namespace $.$$ {
 			const nearest = this.nearest()
 			if (!nearest) return ''
 
-			return String(nearest.label.y)
+			return String(nearest.value.y)
 		}
 
 		title_y_pos_y() {
 			const nearest = this.nearest()
 			if (!nearest) return '0'
 
-			return nearest.point.y.toFixed(3)
+			return nearest.scaled.y.toFixed(3)
 		}
 	}
 }
