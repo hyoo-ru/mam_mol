96f6afccd1145d90e3308530ac16014868000c89
КарловскийДА <kard@1c.ru>
2019-07-23 18:14:37 +0300

    $mol_icon: import all MDI icons, demo with filtering

--- icon/demo/demo.view.ts
+++ icon/demo/demo.view.ts
@@ -2,26 +2,8 @@ namespace $.$$ {
 	export class $mol_icon_demo extends $.$mol_icon_demo {
 
 		@ $mol_mem
-		names() {
-			var next : string[] = []
-			for( var name in this.$ ) {
-				if( !/^\$mol_icon_/i.test( name ) ) continue
-				if( /^\$mol_icon_demo/.test( name ) ) continue
-				if( typeof this.$[ name ] !== 'function' ) continue
-				next.push( name )
-			}
-			return next
-		}
-
-		@ $mol_mem
-		icons() {
-			return this.names().map( name => this.Icon( name ) )
-		}
-
-		@ $mol_mem_key
-		Icon( name : string ) {
-			var Class : typeof $mol_view = this.$[ name ]
-			return new Class()
+		icons_filtered() {
+			return this.icons().filter( $mol_match_text( this.icons_filter() , icon => [ icon.constructor.name ] ) )
 		}
 		
 	}
