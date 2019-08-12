99571f987d8c65d4a3c194bd637eb5daf270ded4
nin-jin <nin-jin@ya.ru>
2019-08-12 22:10:02 +0300

    $mol_data_nominal: better syntax

--- data/nominal/nominal.ts
+++ data/nominal/nominal.ts
@@ -1,11 +1,17 @@
 namespace $ {
 
-	export function $mol_data_nominal< Nominal extends string >() {
-		return < Sub extends $mol_data_value >( sub : Sub )=> {
-			return ( val : Parameters<Sub>[0] ) => {
-				return sub( val ) as ReturnType<Sub> & { readonly $mol_data_nominal : Nominal }
-			}
+	export function $mol_data_nominal<
+		Nominal extends string ,
+		Sub extends $mol_data_value ,
+	>( config : { [ key in Nominal ] : Sub } ) {
+
+		const nominal = Object.keys( config )[0] as Nominal
+		const sub = config[ nominal ] as Sub
+
+		return ( val : Parameters<Sub>[0] ) => {
+			return sub( val ) as ReturnType<Sub> & { $mol_data_nominal : Nominal }
 		}
+		
 	}
 
 }
