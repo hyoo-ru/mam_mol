5f2e84bc57be61a199a18b18e71cc72365352125
nin-jin <sairi-na-tenshi@ya.ru>
2019-09-27 14:17:22 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- build/-/node.test.js
+++ build/-/node.test.js
@@ -2843,7 +2843,7 @@ var $;
             this.modEnsure(parent.path());
             var mapping = this.modMeta(parent.path());
             if (mod.exists()) {
-                if (mod.resolve('.git').type() === 'dir') {
+                if (mod.type() === 'dir' && mod.resolve('.git').type() === 'dir') {
                     try {
                         process.stdout.write($.$mol_exec(mod.path(), 'git', '--no-pager', 'log', '--oneline', 'HEAD..origin/master').stdout);
                     }
