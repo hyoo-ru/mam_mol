bc200dc32fb375f4fea972e2560e7d25d348cd6a
Slava Viktorov <vsviktorov@gmail.com>
2019-09-30 20:06:05 +0300

    $mol_data_boolean

---
+++ data/boolean/boolean.test.ts
@@ -0,0 +1,25 @@
+namespace $ {
+	$mol_test({
+
+		'Is boolean - true' () {
+			$mol_data_boolean( true )
+		} ,
+
+		'Is boolean - false' () {
+			$mol_data_boolean( false )
+		} ,
+
+		'Is not boolean' () {
+			$mol_assert_fail( ()=> {
+				$mol_data_boolean( '' as any )
+			} , 'is not a boolean' )
+		} ,
+
+		'Is object boolean' () {
+			$mol_assert_fail( ()=> {
+				$mol_data_boolean( new Boolean( '' ) as any )
+			} , 'is not a boolean' )
+		} ,
+
+	})
+}
