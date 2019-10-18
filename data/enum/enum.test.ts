e1c6bc7f65caaa123f9c894ce0d741f29f2135e9
Slava Viktorov <vsviktorov@gmail.com>
2019-10-18 11:20:41 +0300

    fix

--- data/enum/enum.test.ts
+++ data/enum/enum.test.ts
@@ -1,22 +1,34 @@
-enum Sex { male , female }
 namespace $ {
-	$mol_test({
+	enum Sex { male , female }
+	const SexParser = $mol_data_enum( 'Sex', Sex )
 
+	$mol_test({
 		'Is type of Sex enum - string' () {
-			const SexParser = $mol_data_enum( Sex )
 			SexParser('male')
 		} ,
 
 		'Is type of Sex enum - number' () {
-			const SexParser = $mol_data_enum( Sex )
 			SexParser(0)
 		} ,
 
-		'Is not a type of enum' () {
+		'Is type of Sex enum - return number' () {
+			SexParser('male') === 0
+		} ,
+
+		'Is type of Sex enum - return string' () {
+			SexParser(0) === 'male'
+		} ,
+
+		'Is not a type of enum - number' () {
 			$mol_assert_fail( ()=> {
-				const SexParser = $mol_data_enum( Sex )
-				SexParser({} as any)
-			} , 'is not a type of enum' )
+				SexParser(2)
+			} , 'is not a type of Sex enum' )
+		} ,
+
+		'Is not a type of enum - string' () {
+			$mol_assert_fail( ()=> {
+				SexParser('a')
+			} , 'is not a type of Sex enum' )
 		} ,
 
 	})
--- data/enum/enum.ts
+++ data/enum/enum.ts
@@ -1,11 +1,11 @@
 namespace $ {
 
-	export function $mol_data_enum(e: object) {
+	export function $mol_data_enum(name: string, e: Record<number|string,number|string>) {
+		const errorMessage =  `is not a type of ${name} enum`;
 		return (val : string | number ) => {
-		
-			if( (typeof val === 'string' || typeof val === 'number') && val in e) return e[val]
-			
-			return $mol_fail( new Error( 'is not a type of enum' ) )
+			if ( typeof val !== 'string' && typeof val !== 'number') return $mol_fail( new Error( errorMessage ) )
+			if ( !(val in e) ) return $mol_fail( new Error( errorMessage ) )
+			return e[val]
 		}
 	}
 	
