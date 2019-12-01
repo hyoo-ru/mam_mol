83ef090832b0e0ea603f616e6213264f6512b05b
nin-jin <nin-jin@ya.ru>
2019-12-01 18:44:19 +0300

    $mol_page: partial usage of $mol_style

--- page/page.view.ts
+++ page/page.view.ts
@@ -8,4 +8,60 @@ namespace $.$$ {
 		
 	}
 
+	$mol_style( $mol_page , {
+
+		display: 'flex' ,
+		margin: '0' ,
+		flexDirection: 'column' ,
+		flex: '1 1 auto' ,
+		position: 'relative' ,
+		alignSelf: 'stretch' ,
+		maxWidth: '100%' ,
+		maxHeight: '100%' ,
+		boxSizing: 'border-box' ,
+		background: $mol_theme.back ,
+		color: $mol_theme.text ,
+		zIndex: '0' ,
+		overflow: 'hidden' ,
+		boxShadow: `inset 0 0 0 .5px ${ $mol_theme.line }` ,
+
+		Head: {
+			display: 'flex' ,
+			flexWrap: 'wrap' ,
+			justifyContent: 'space-between' ,
+			flex: '0 0 auto' ,
+			position: 'relative' ,
+			margin: '0' ,
+			minHeight: 'calc( 1.5em + 1rem )' ,
+			padding: '.5rem' ,
+		},
+
+		Title: {
+			flex: '1000 1 50%',
+			padding: '.5rem',
+			wordBreak: 'normal',
+			cursor: 'default',		
+		},
+
+		Tools: {
+			flex: '1 1 auto',
+			display: 'flex',
+			justifyContent: 'flex-end',
+		},
+
+		Body: {
+			flex: '1000 1 100%',
+			margin: '0',
+		},
+
+		Foot: {
+			display: 'flex',
+			justifyContent: 'space-between',
+			flex: '0 0 auto',
+			margin: '0',
+			overflow: 'hidden',
+		}		
+
+	} )
+	
 }
