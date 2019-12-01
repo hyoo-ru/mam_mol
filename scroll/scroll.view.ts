5cdf1a452355ab1bc88ec766cbca32b0841a67e4
nin-jin <nin-jin@ya.ru>
2019-12-01 22:03:15 +0300

    $mol_scroll: use $mol_style

--- scroll/scroll.view.ts
+++ scroll/scroll.view.ts
@@ -91,4 +91,61 @@ namespace $.$$ {
 		
 	}
 
+
+	$mol_style_define( $mol_scroll , {
+
+		display: 'block',
+		overflow: 'auto',
+		flex: '1 1 auto',
+		alignSelf: 'stretch',
+		boxSizing: 'border-box',
+		willChange: 'scroll-position',
+		transform: 'translateZ(0)', // enforce gpu scroll in all agents
+		boxShadow: `inset 0 0 0 .5px ${ $mol_theme.line }`,
+		maxHeight: '100%',
+		maxWidth: '100%',
+		webkitOverflowScrolling: 'touch',
+		background: 'none !important', // prevent large gpu layer
+	
+		'::-webkit-scrollbar': {
+			width: '.5rem',
+			height: '.5rem',
+		},
+		
+		'::-webkit-scrollbar-corner': {
+			background: $mol_theme.line,
+		},
+
+		'::-webkit-scrollbar-track': {
+			background: $mol_theme.line,
+		},
+
+		'::-webkit-scrollbar-thumb': {
+			background: $mol_theme.control,
+		},
+
+		'>': {
+			$mol_view : {
+				transform: 'translateZ(0)',
+			}
+		},
+
+		Strut: {
+			position: 'absolute',
+			top: '0',
+			display: 'block',
+			padding: '1px 1px 0 0',
+			margin: '-1px 0 0 0',
+			zIndex: '0', 
+			transition: 'none',
+		},
+		
+		'@media' : {
+			'print' : {
+				overflow: 'visible',
+			}
+		}
+
+	})
+
 }
