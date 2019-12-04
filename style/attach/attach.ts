4525d99929b4e7f5fbaad707fcadd5f54ed29ddf
nin-jin <nin-jin@ya.ru>
2019-12-04 21:06:23 +0300

    $mol_style_attach: buffering

--- style/attach/attach.ts
+++ style/attach/attach.ts
@@ -1,17 +1,29 @@
 namespace $ {
 
+	let all = [] as string[]
+	let el : HTMLStyleElement | null = null
+	let timer : $mol_after_tick | null = null
+
 	export function $mol_style_attach(
 		id : string ,
 		text : string ,
 	) {
 
+		all.push( `/*${ id }*/\n${ text }`)
+
+		if( timer ) return el!
+		
 		const doc = $mol_dom_context.document
-		const el = doc.createElement('style');
-		
-		el.id = `$mol_style:${ id }`
-		el.innerHTML = '\n' + text
-		
+		el = doc.createElement('style')
+		el.id = `$mol_style_attach`
 		doc.head.appendChild( el )
+
+		timer = new $mol_after_tick( ()=> {
+			el.innerHTML = '\n' + all.join( '\n' )
+			all = []
+			el = null
+			timer = null
+		} )
 
 		return el
 
