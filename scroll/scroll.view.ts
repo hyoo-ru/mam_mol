b34a5e9f18d6b1d330308f5c47ba8fd7cc090066
nin-jin <nin-jin@ya.ru>
2019-12-14 13:28:27 +0300

    $mol_scroll: compatibility

--- scroll/scroll.view.ts
+++ scroll/scroll.view.ts
@@ -1,13 +1,5 @@
 namespace $.$$ {
 	
-	export function $mol_scroll_top() {
-		return 0
-	}
-	
-	export function $mol_scroll_left() {
-		return 0
-	}
-	
 	export class $mol_scroll extends $.$mol_scroll {
 
 		@ $mol_mem
@@ -20,12 +12,11 @@ namespace $.$$ {
 			return $mol_state_session.value( `${ this }.scroll_left()` , next ) || 0
 		}
 
-		event_scroll_time : $mol_after_frame
 		event_scroll( next? : Event ) {
 			
 			const el = this.dom_node() as HTMLElement
-			if( this.event_scroll_time ) this.event_scroll_time.destructor()
-			this.event_scroll_time = new $mol_after_frame( $mol_fiber_solid.func( ()=> {
+			if( this['event_scroll_timer'] ) this['event_scroll_timer'].destructor()
+			this['event_scroll_timer'] = new $mol_after_frame( $mol_fiber_solid.func( ()=> {
 				this.scroll_top( Math.max( 0 , el.scrollTop ) )
 				this.scroll_left( Math.max( 0 , el.scrollLeft ) )
 			} ) )
