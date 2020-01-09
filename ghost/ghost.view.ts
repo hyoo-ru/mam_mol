ac6fde5099892e60eb31133d488fa263d0d25974
nin-jin <nin-jin@ya.ru>
2020-01-09 08:05:15 +0300

    $mol_ghost: fixed attributes updating

--- ghost/ghost.view.ts
+++ ghost/ghost.view.ts
@@ -12,6 +12,21 @@ namespace $.$$ {
 			return node
 		}
 		
+		@ $mol_mem
+		dom_node_actual() {
+			const node = this.Sub().dom_node_actual()
+
+			const attr = this.attr()
+			const style = this.style()
+			const fields = this.field()
+
+			$mol_dom_render_attributes( node , attr )
+			$mol_dom_render_styles( node , style )
+			$mol_dom_render_fields( node , fields )
+
+			return node
+		}
+		
 		dom_tree() {
 			const Sub = this.Sub()
 			Sub.$ = this.$
