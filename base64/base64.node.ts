253d3cdae7598608e2bfd4bf6818c2d45c6dfd65
Stefan Zerkalica <zerkalica@gmail.com>
2020-03-11 09:30:43 +0300

    $mol_base64: wrap buffer to Uint8Array

--- base64/base64.node.ts
+++ base64/base64.node.ts
@@ -8,7 +8,8 @@ namespace $ {
 	$.$mol_base64_encode = $mol_base64_encode_node
 
 	export function $mol_base64_decode_node(base64Str: string): Uint8Array {
-		return Buffer.from(base64Str, 'base64')
+		// without Uint8Array breaks $mol_compare_deep
+		return new Uint8Array(Buffer.from(base64Str, 'base64'))
 	}
 
 	$.$mol_base64_decode = $mol_base64_decode_node
