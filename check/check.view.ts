4f6f82b3244cd0e2a549f4719bb8895800d7240a
nin-jin <nin-jin@ya.ru>
2020-03-11 19:36:09 +0300

    $mol_check: better click

--- check/check.view.ts
+++ check/check.view.ts
@@ -2,6 +2,10 @@ namespace $.$$ {
 	export class $mol_check extends $.$mol_check {
 
 		event_click( next? : Event ) {
+			this.click( next )
+		}
+
+		click( next? : Event ) {
 			this.checked( !this.checked() )
 			if( next ) next.preventDefault()
 		}
