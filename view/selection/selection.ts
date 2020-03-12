740fe776dc5fda75ebff4d138aff1004ca59a539
nin-jin <nin-jin@ya.ru>
2020-03-12 09:51:05 +0300

    $mol_view_selection: blur fix

--- view/selection/selection.ts
+++ view/selection/selection.ts
@@ -34,8 +34,8 @@ namespace $ {
 		
 		@ $mol_log2.method
 		static blur( event : FocusEvent ) {
-			const element = $mol_mem_cached( ()=> this.focused() )![0] as HTMLElement
-			if( element === event.target ) this.focused( [] )
+			const elements = $mol_mem_cached( ()=> this.focused() )
+			if( elements && elements[0] === event.target ) this.focused( [] )
 		}
 	}
 	
