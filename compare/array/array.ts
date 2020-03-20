6f8b09220b4d240fc6d0974993a767e5cff80237
Stefan Zerkalica <zerkalica@gmail.com>
2020-03-20 23:01:49 +0300

    $mol_compare_array: subclass return false - prototype 2

--- compare/array/array.ts
+++ compare/array/array.ts
@@ -1,7 +1,7 @@
 namespace $ {
     export function $mol_compare_array<Value extends ArrayLike<unknown>>(a: Value, b: Value): boolean {
 		if (a === b) return true
-		if (a['prototype'] !== b['prototype']) return false
+		if (Object.getPrototypeOf(a) !== Object.getPrototypeOf(b)) return false
 		if (a.length !== b.length) return false
 
 		for (let i = 0; i < a.length; i++)
