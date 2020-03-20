ea23af468631243ada7ef136c865f1ae35749bf8
Stefan Zerkalica <zerkalica@gmail.com>
2020-03-20 22:40:30 +0300

    $mol_compare_array: subclass return false

--- compare/array/array.ts
+++ compare/array/array.ts
@@ -1,6 +1,7 @@
 namespace $ {
     export function $mol_compare_array<Value extends ArrayLike<unknown>>(a: Value, b: Value): boolean {
 		if (a === b) return true
+		if (a['constructor'] !== b['constructor']) return false
 		if (a.length !== b.length) return false
 
 		for (let i = 0; i < a.length; i++)
