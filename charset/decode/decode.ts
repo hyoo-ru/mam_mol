5777bbff38f8f74bd9c7d295520231db5b6ba8fb
Stefan Zerkalica <zerkalica@gmail.com>
2020-03-20 22:06:56 +0300

    $mol_charset_decode: added mdn link to encodings

--- charset/decode/decode.ts
+++ charset/decode/decode.ts
@@ -1,6 +1,7 @@
 namespace $ {
 	const TextDecoder = globalThis.TextDecoder ?? $node.util.TextDecoder
 
+	// @see https://developer.mozilla.org/en-US/docs/Web/API/TextDecoder/encoding
 	export type $mol_charset_encoding = 'utf8' | 'ibm866' | 'iso-8859-2' | 'iso-8859-3' | 'iso-8859-4' | 'iso-8859-5' | 'iso-8859-6'
 		| 'iso-8859-7' | 'iso-8859-8' | 'iso-8859-8i' | 'iso-8859-10' | 'iso-8859-13' | 'iso-8859-14' | 'iso-8859-15' | 'iso-8859-16'
 		| 'koi8-r' | 'koi8-u' | 'koi8-r' | 'macintosh' | 'windows-874' | 'windows-1250' | 'windows-1251' | 'windows-1252' | 'windows-1253'
