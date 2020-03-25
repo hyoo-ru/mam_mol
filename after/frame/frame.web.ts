536bca01e371e6584585845e6de0075acdebfe8e
nin-jin <nin-jin@ya.ru>
2020-03-25 10:46:06 +0300

    Revert "$mol_after_frame: reduce native api usage to improve performance."
    This reverts commit 7d1a135a1f05300944f2e5c4722f0636f0b39300.

--- after/frame/frame.web.ts
+++ after/frame/frame.web.ts
@@ -2,46 +2,17 @@ namespace $ {
 
 	export class $mol_after_frame extends $mol_object2 {
 
-		static queue = new Set< ()=> void  >()
-		static scheduled = 0
-
-		static schedule( task : ()=> void ) {
-			
-			this.queue.add( task )
-			
-			if( this.scheduled ) return
-			this.scheduled = requestAnimationFrame( ()=> this.run() )
-
-		}
-
-		static run() {
-
-			this.scheduled = 0
-
-			const promise = Promise.resolve()
-			
-			for( const task of this.queue ) {
-				promise.then( task )
-			}
-
-			this.queue = new Set
-
-		}
+		id : any
 
 		constructor(
 			public task : ()=> void ,
 		) {
-
 			super()
-			
-			const Frame = this.constructor as typeof $mol_after_frame
-			Frame.schedule( task )
-			
+			this.id = requestAnimationFrame( task )
 		}
 
 		destructor() {
-			const Frame = this.constructor as typeof $mol_after_frame
-			Frame.queue.delete( this.task )
+			cancelAnimationFrame( this.id )
 		}
 
 	}
