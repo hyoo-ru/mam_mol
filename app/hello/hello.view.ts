ade47a52c965e8adfc0ef816f0be2cf95ad9e79c
nin-jin <nin-jin@ya.ru>
2020-03-26 23:11:21 +0300

    $mol_build: don't include $mol_view to $mol_build; view.css after view.tree

--- app/hello/hello.view.ts
+++ app/hello/hello.view.ts
@@ -1,8 +1,12 @@
 namespace $.$$ {
+	const sleep = $mol_fiber_sync( (n:number)=> new Promise<number>( done => setTimeout( done , n ) ) )
 	export class $mol_app_hello extends $.$mol_app_hello {
 		
+		@ $mol_mem
 		greeting() {
 			let name = this.name()
+			// sleep(1000)
+			$mol_fetch.text( '/mol/index.html?' + name )
 			return name && `Hello, ${name}!`
 		}
 		
