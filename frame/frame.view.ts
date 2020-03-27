b355741a4483cc01caae7b5050fb7d455298728c
nin-jin <nin-jin@ya.ru>
2020-03-27 21:20:12 +0300

    $mol_frame: fixed loading tracking, added demo

--- frame/frame.view.ts
+++ frame/frame.view.ts
@@ -1,19 +1,25 @@
 namespace $.$$ {
 	export class $mol_frame extends $.$mol_frame {
+
+		dom_node : ( next? : HTMLIFrameElement )=> HTMLIFrameElement
 		
 		@$mol_mem
 		window() {
+
+			const node = this.dom_node()
+			
 			this.uri();
+			
 			return $mol_fiber_sync(() => new Promise((done, fail) => {
-				const node = this.dom_node() as HTMLIFrameElement
 				node.onload = () => done( node.contentWindow )
 				node.onerror = ( event : ErrorEvent ) => fail( event.error )
 			}))()
+			
 		}
 
 		render() {
-			super.render();
 			this.window();
+			return super.render();
 		}
 		
 	}
