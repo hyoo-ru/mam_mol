7df6d430293b425ed5f1d9c05addacd87887dc46
Stefan Zerkalica <zerkalica@gmail.com>
2020-04-05 16:28:34 +0300

    $mol_view_tree_compiler: refactored test

--- view/tree/tree.test.ts
+++ view/tree/tree.test.ts
@@ -1,63 +1,12 @@
-namespace $ {
-	const ts = (tree: string) => $mol_view_tree_compile($mol_tree.fromString(tree)).script.trim()
+namespace $.$$ {
 
 	$mol_test( {
-		'empty_class'() {
-			$mol_assert_equal(ts(
-`$mol_string $mol_view`
-),
-`namespace $ { export class $mol_string extends $mol_view {
+		'simple'( $ ) {
+			const app = $mol_view_tree_test_simple.make({
+				$
+			})
 
-} }`)
-		},
-
-		'simple properties'() {
-			$mol_assert_equal(ts(`
-$mol_string $mol_view
-	dom_name \\input
-	enabled true
-	debounce 0
-	autocomplete false
-`), `namespace $ { export class $mol_string extends $mol_view {
-
-	/**
-	 *  \`\`\`
-	 *  dom_name \\input
-	 *  \`\`\`
-	 **/
-	dom_name() {
-		return "input"
-	}
-
-	/**
-	 *  \`\`\`
-	 *  enabled true
-	 *  \`\`\`
-	 **/
-	enabled() {
-		return true
-	}
-
-	/**
-	 *  \`\`\`
-	 *  debounce 0
-	 *  \`\`\`
-	 **/
-	debounce() {
-		return 0
-	}
-
-	/**
-	 *  \`\`\`
-	 *  autocomplete false
-	 *  \`\`\`
-	 **/
-	autocomplete() {
-		return false
-	}
-
-} }`)			
+			$mol_assert_equal( app.some() , 1 )
 		}
-
 	} )
 }
