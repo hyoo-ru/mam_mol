3593148c2dbe283fe279ddbb3f073a041a330187
Stefan Zerkalica <zerkalica@gmail.com>
2020-04-10 21:19:52 +0300

    $mol_view_tree_compiler: test 2

--- view/tree/tree.test.ts
+++ view/tree/tree.test.ts
@@ -7,6 +7,20 @@ namespace $.$$ {
 			})
 
 			$mol_assert_equal( app.some() , 1 )
-		}
+			$mol_assert_equal( app.bool() , true )
+			$mol_assert_equal( app.str() , 'test' )
+			$mol_assert_ok( Array.isArray(app.arr()) )
+		},
+
+		'binding'( $ ) {
+			const app = $mol_view_tree_test_binding.make({
+				$
+			})
+			app.value(1)
+			$mol_assert_equal( app.value() , 1 )
+
+			app.head_complete_enabled()
+			$mol_assert_not( app.enabled() )
+		},
 	} )
 }
