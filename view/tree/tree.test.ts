678a50d4e8aaf48c5ccc8f5f360d5d4dc1eaf12a
Stefan Zerkalica <zerkalica@gmail.com>
2020-04-11 01:15:37 +0300

    $mol_view_tree_compiler: more tests

--- view/tree/tree.test.ts
+++ view/tree/tree.test.ts
@@ -1,9 +1,9 @@
 namespace $.$$ {
 
 	$mol_test( {
-		'simple'( $ ) {
+		'simple props'( $ ) {
 			const app = $mol_view_tree_test_simple.make({
-				$
+				$,
 			})
 
 			$mol_assert_equal( app.some() , 1 )
@@ -12,15 +12,56 @@ namespace $.$$ {
 			$mol_assert_ok( Array.isArray(app.arr()) )
 		},
 
-		'binding'( $ ) {
+		'default value'( $ ) {
+			const app = $mol_view_tree_test_binding.make({
+				$
+			})
+			$mol_assert_equal( app.value() , '123' )
+		},
+
+		'both binding'( $ ) {
 			const app = $mol_view_tree_test_binding.make({
 				$
 			})
 			app.value(1)
 			$mol_assert_equal( app.value() , 1 )
+		},
 
-			app.head_complete_enabled()
+		'left binding'( $ ) {
+			const app = $mol_view_tree_test_binding.make({
+				$
+			})
+
+			$mol_assert_not( app.head_complete_enabled() )
 			$mol_assert_not( app.enabled() )
 		},
+
+		'sub component'( $ ) {
+			const app = $mol_view_tree_test_binding_right.make({
+				$
+			})
+			$mol_assert_ok( app.Test() instanceof $mol_view_tree_test_binding_right_test)
+		},
+
+		'right binding'( $ ) {
+			const app = $mol_view_tree_test_binding_right.make({
+				$
+			})
+
+			const val = 123
+
+			app.outer_width(val)
+			$mol_assert_equal( app.outer_width(), val )
+			$mol_assert_equal( app.Test().width(), val )
+		},
+
+		'attributes merging'( $ ) {
+			const app = $mol_view_tree_test_attributes.make({
+				$
+			})
+			$mol_assert_equal( app.attr().a, 1 )
+			$mol_assert_equal( app.attr().b, 2 )
+		},
+
 	} )
 }
