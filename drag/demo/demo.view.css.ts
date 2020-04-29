7c19cbb471f29bea78f5bbf1efbb029e8d3cd5e4
nin-jin <nin-jin@ya.ru>
2020-04-29 23:09:24 +0300

    $mol_drag, $mol_drop: drag and drop support

---
+++ drag/demo/demo.view.css.ts
@@ -0,0 +1,61 @@
+namespace $.$$ {
+
+	const {rem,px} = $mol_style_unit
+
+	$mol_style_define( $mol_drag_demo , {
+		
+		Product_drop:{
+			'@': {
+				mol_drop_status: {
+					drag: {
+						boxShadow: `0 -1px 0 0px ${ $mol_theme.focus }`,
+					},
+				},
+			},
+		},
+
+		List: {
+			padding: rem(.5),
+		},
+		
+		List_drop: {
+			'@': {
+				mol_drop_status: {
+					drag: {
+						'>' : {
+							$mol_view : {
+								':last-child': {
+									boxShadow: `0 1px 0 0px ${ $mol_theme.focus }`,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		
+		Product_link: {
+			padding: rem(.5),
+			display: 'block',
+		},
+		
+		Trash: {
+			padding: [ rem(.5) , rem(1) ],
+			display: 'block',
+		},
+
+		Trash_drop: {
+			'@': {
+				mol_drop_status: {
+					drag: {
+						background: {
+							color: $mol_theme.hover,
+						},
+					},
+				},
+			},
+		},
+		
+	})
+
+}
