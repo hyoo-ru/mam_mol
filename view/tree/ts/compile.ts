8ac378f12889b075bf62dea61de069a76b2ddbb9
Stefan Zerkalica <zerkalica@gmail.com>
2020-05-09 19:12:14 +0300

    compiler2: draft 12

--- view/tree/ts/compile.ts
+++ view/tree/ts/compile.ts
@@ -1,5 +1,5 @@
 namespace $ {
 	export function $mol_view_tree_ts_compile(tree: $mol_tree) {
-		return $mol_view_tree_ts_serialize(tree)
+		return $mol_view_tree_ts_serialize($mol_view_tree_ts_module(tree))
 	}
 }
