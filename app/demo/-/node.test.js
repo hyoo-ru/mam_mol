10a7338d5647d0f4ddb4aad656a5939a84df86e1
nin-jin <nin-jin@users.noreply.github.com>
2020-05-26 21:17:28 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/demo/-/node.test.js
+++ app/demo/-/node.test.js
@@ -11515,7 +11515,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $.$mol_style_attach("mol/check/icon/icon.view.css", "[mol_check_icon][mol_check_checked] {\n\tcolor: var(--mol_skin_accent);\n}\n");
+    $.$mol_style_attach("mol/check/icon/icon.view.css", "[mol_check_icon][mol_check_checked] {\n\tcolor: var(--mol_theme_focus);\n}\n");
 })($ || ($ = {}));
 //icon.view.css.js.map
 ;
--- check/icon/-css/icon.view.css.ts
+++ check/icon/-css/icon.view.css.ts
@@ -1,3 +1,3 @@
 namespace $ { $mol_style_attach( "mol/check/icon/icon.view.css",
- "[mol_check_icon][mol_check_checked] {\n\tcolor: var(--mol_skin_accent);\n}\n"
+ "[mol_check_icon][mol_check_checked] {\n\tcolor: var(--mol_theme_focus);\n}\n"
 ) }
