aca688f592cb81234f5a2fc2b83fcbc25714cf59
Stefan Zerkalica <zerkalica@gmail.com>
2020-05-28 22:47:30 +0300

    wip

--- view/tree/compile2.test.ts
+++ view/tree/compile2.test.ts
@@ -10,38 +10,12 @@ ${d}hyoo_tree ${d}mol_book2
 		compile \\
 	attr *
 		^
-		mol_theme ${d}mol_theme_auto
-	Simple null
-	Source_first ${d}mol_page
-	q <= Source_first1 ${d}mol_page
-	Index_first!id ${d}sub
-		report <= report!id null
-		width?v => outer_width?v
-	pages /
-		<= \\test
-		<= Source_second ${d}mol_page
-			q2 <= Source_first2 ${d}mol_page
-			a!id <= Index_second!id ${d}sub
-				report <= report!id null
-			arg *
-				test <= compile \\
-			title @ \\View.Tree
-			tools /
-				<= Compile ${d}mol_link
-					sub /
-						<= compile_label @ \\Compile
-			body /
-				<= Source_text ${d}mol_textarea
-					value?val <=> source?val \\
-					hint <= source_hint @ \\view.tree source
-
-${d}sub ${d}mol_view
-	width?val 0
+		mol_theme \\${d}mol_theme_auto
 `.trim())
 
 			console.log($mol_view_tree_compile(tree).script)
 			console.log('--------------------------------------------------')
-			console.log($.$mol_view_tree_compile2(tree))
+			// console.log($.$mol_view_tree_compile2(tree))
 		},
 	} )
 }
