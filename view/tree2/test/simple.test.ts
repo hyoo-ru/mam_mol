f8d4ad2a87bfa2fe56178b3c7d0cc2f739cfcd8b
Stefan Zerkalica <zerkalica@gmail.com>
2020-05-31 17:04:53 +0300

    tree-ts file compare autotests

--- view/tree2/test/simple.test.ts
+++ view/tree2/test/simple.test.ts
@@ -1,12 +1,41 @@
 namespace $.$$ {
 
 	$mol_test( {
-		async 'simple'( $ ) {
-			const rec = await $.$mol_view_tree2_test('mol/view/tree2/test/sample/simple')
-			$mol_assert_equal(rec.sample, rec.content)
-		},
+		...generate_tests([
+			'simple',
+		]),
 
 		'localized'( $ ) {
 		},
 	} )
+
+	function generate_tests(tests: string[]) {
+		const result: Record<string, Function> = {}
+
+		for (const file_part of tests) {
+			result[file_part] = async ($: $mol_ambient_context) => {
+				const rec = await compile_module($, 'mol/view/tree2/test/sample/' + file_part)
+				$mol_assert_equal(rec.sample, rec.content)	
+			}
+		}
+
+		return result
+	}
+
+	async function compile_module($: $mol_ambient_context, file: string) {
+		const tree_data = await $mol_fiber_async(() => {
+			console.log('repreprpe')
+			return $mol_file.relative(file + '.view.tree.ex').text()
+		})()
+
+		const tree = $mol_tree2.fromString(tree_data)
+		const compiled = $.$mol_view_tree2_compile(tree)
+
+		const sample = await $mol_fiber_async(() => $mol_file.relative(file + '.view.ts.ex').text().trim())()
+
+		return {
+			...compiled,
+			sample
+		}
+	}
 }
--- view/tree2/test/test.ts
+++ view/tree2/test/test.ts
@@ -1,7 +1,10 @@
 namespace $ {
-
 	export async function $mol_view_tree2_test(this: $mol_ambient_context, file: string) {
-		const tree_data = await $mol_fiber_async(() => $mol_file.relative(file + '.view.tree.ex').text())()
+		const tree_data = await $mol_fiber_async(() => {
+			console.log('repreprpe')
+			return $mol_file.relative(file + '.view.tree.ex').text()
+		})()
+
 		const tree = $mol_tree2.fromString(tree_data)
 		const compiled = this.$mol_view_tree2_compile(tree)
 
