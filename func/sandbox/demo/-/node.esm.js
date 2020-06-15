be9da6395a86e9a2e0f055a4158f887bd6070fdd
nin-jin <nin-jin@users.noreply.github.com>
2020-06-15 21:10:48 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- func/sandbox/demo/-/node.esm.js
+++ func/sandbox/demo/-/node.esm.js
@@ -2387,12 +2387,12 @@ var $;
             const node = this.dom_node(next);
             try {
                 $.$mol_dom_render_attributes(node, { mol_view_error: null });
-                this.render();
                 for (let plugin of this.plugins()) {
                     if (plugin instanceof $.$mol_plugin) {
                         plugin.render();
                     }
                 }
+                this.render();
             }
             catch (error) {
                 const need_catch = $.$mol_fail_catch(error);
