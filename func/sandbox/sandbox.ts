e42916919722a6dd31e1a38287a368b38a53028c
nin-jin <nin-jin@ya.ru>
2020-06-21 15:32:50 +0300

    Prevent a modifications out of the sandbox.

--- func/sandbox/sandbox.ts
+++ func/sandbox/sandbox.ts
@@ -2,7 +2,18 @@ namespace $ {
 
 	export class $mol_func_sandbox {
 		
-		static _make : ( contexts : Object[] )=> ( code : string )=> ()=> void
+		static blacklist = new Set([
+			( function() {} ).constructor ,
+			( async function() {} ).constructor ,
+			( function*() {} ).constructor ,
+			eval ,
+			setTimeout ,
+			setInterval ,
+		])
+
+		static whitelist = new WeakSet()
+
+		static _make : ( contexts : Object[] )=> ( code : string )=> ()=> any
 
 		static get make() {
 
@@ -13,7 +24,8 @@ namespace $ {
 			$mol_dom_context.document.body.appendChild( frame )
 
 			const win = frame.contentWindow as any as typeof globalThis
-			const SafeFunction = win.Function
+			const SafeFunc = win.Function
+			const SafeJSON = win.JSON
 			
 			win.eval( `
 
@@ -51,20 +63,84 @@ namespace $ {
 			}
 			clean( win )
 
+			const is_primitive = ( val : any )=> Object( val ) !== val
+
+			const safe_value = ( val : any ) : any => {
+
+				if( is_primitive( val ) ) return val
+				if( this.blacklist.has( val ) ) return undefined
+				if( this.whitelist.has( val ) ) return val
+				
+				const str = JSON.stringify( val )
+				if( !str ) return str
+
+				val = SafeJSON.parse( str )
+				this.whitelist.add( val )
+				return val
+
+			}
+
+			const safe_derived = ( val : any ) : any => {
+				
+				if( is_primitive( val ) ) return val
+				
+				const proxy = new Proxy( val , {
+
+					get( val , field : any ) {
+						if( field === 'valueOf' ) return safe_derived( val[field] )
+						if( field === 'toString' ) return safe_derived( val[field] )
+						return safe_value( val[field] )
+					},
+					
+					set() { return false },
+					defineProperty() { return false },
+					deleteProperty() { return false },
+					preventExtensions() { return false },
+					
+					apply( val , host , args ) {
+						return safe_value( val.call( host , ... args ) )
+					},
+
+					construct( val , args ) {
+						return safe_value( new val( ... args ) )
+					},
+
+				})
+
+				this.whitelist.add( proxy )
+
+				return proxy
+			}
+
 			return this._make = ( ( ... contexts : Object[] )=> {
 
 				const context_merged = {}
 
 				for( let context of contexts ) {
 					for( let name of Object.getOwnPropertyNames( context ) ) {
-						context_merged[ name ] = context[ name ]
+						context_merged[ name ] = safe_derived( context[ name ] )
 					}
 				}
 
 				const vars = Object.keys( context_merged )
 				const values = vars.map( name => context_merged[ name ] )
 	
-				return ( code : string )=> new SafeFunction( ... vars , '"use strict";' + code ).bind( null , ... values )
+				return ( code : string )=> {
+
+					const func = new SafeFunc( ... vars , '"use strict";' + code )
+						.bind( null , ... values )
+					
+					return ()=> {
+
+						const val = func()
+						if( is_primitive( val ) ) return val
+						
+						this.whitelist.add( val )
+
+						return val
+					}
+
+				}
 
 			} ).bind( null , context_default )
 
@@ -76,7 +152,7 @@ namespace $ {
 
 		contexts : Object[]
 		
-		_eval : ( ( code : string )=> ()=> void ) | undefined
+		_eval : ( ( code : string )=> ()=> any ) | undefined
 		get eval() {
 			if( this._eval ) return this._eval
 			return this._eval = $mol_func_sandbox.make( ... this.contexts as [Object[]] )
