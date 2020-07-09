8e9c6b0da890c2020b92755cb4ca5591fae43f57
nin-jin <nin-jin@ya.ru>
2020-07-09 21:41:54 +0300

    $mol_frame: fixed fullscreen

--- frame/frame.view.ts
+++ frame/frame.view.ts
@@ -27,12 +27,13 @@ namespace $.$$ {
 
 		allow() {
 			return [
+				... this.fullscreen() ? [ 'fullscreen' ] : [] ,
 				... this.accelerometer() ? [ 'accelerometer' ] : [] ,
 				... this.autoplay() ? [ 'autoplay' ] : [] ,
 				... this.encription() ? [ 'encrypted-media' ] : [] ,
 				... this.gyroscope() ? [ 'gyroscope' ] : [] ,
 				... this.pip() ? [ 'picture-in-picture' ] : [] ,
-			]
+			].join(';')
 		}
 		
 	}
