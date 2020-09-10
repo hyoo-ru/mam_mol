178a99a32d040e8c40028dbecfc7e018a9408430
nin-jin <nin-jin@users.noreply.github.com>
2020-09-10 14:40:04 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- store/local/local.web.ts
+++ store/local/local.web.ts
@@ -1,8 +1,13 @@
 namespace $ {
 
 	$mol_dom_context.addEventListener( 'storage' , event => {
+		
 		const store = $mol_store_local as $mol_store_local_class
-		store.value( event.key , undefined , $mol_atom_force_cache )
+		
+		if( event.key ) {
+			store.value( event.key , undefined , $mol_mem_force_cache )
+		}
+
 	} )
 
 }
