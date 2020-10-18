e0118d92f9c9da8ff77929add72cbe0ffc1a0158
jin <jin@hyoo.ru>
2020-10-18 22:20:54 +0300

    Virtualization performance improved

--- scroll/scroll.view.css.ts
+++ scroll/scroll.view.css.ts
@@ -10,15 +10,22 @@ namespace $.$$ {
 			direction: 'column',
 			grow: 1,
 			shrink: 1,
+			basis: 0,
 		},
 		outline: 'none',
 		alignSelf: 'stretch',
 		boxSizing: 'border-box',
 		willChange: 'scroll-position',
-		transform: 'translateZ(0)', // enforce gpu scroll in all agents
 		maxHeight: per(100),
 		maxWidth: per(100),
 		webkitOverflowScrolling: 'touch',
+		contain: 'strict',
+
+		'>': {
+			$mol_view: {
+				transform: 'translateZ(0)', // enforce gpu scroll in all agents
+			},
+		},
 
 		scrollbar: {
 			color: [ $mol_theme.line , 'transparent' ],
