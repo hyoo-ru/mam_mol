f4c737e30731145b67a2779893550f2487bf35ad
jin <jin@hyoo.ru>
2020-11-17 19:41:28 +0300

    $mol_text_code: separated, highlight support

---
+++ text/code/row/row.view.ts
@@ -0,0 +1,83 @@
+namespace $.$$ {
+
+	export class $mol_text_code_row extends $.$mol_text_code_row {
+		
+		maximal_width() {
+			return this.text().length * this.letter_width()
+		}
+
+		@ $mol_mem_key
+		tokens( path: number[] ) {
+
+			const tokens = [] as { name : string , found : string , chunks: string[] }[]
+			
+			const text = ( path.length > 0 )
+				// @FIXME: this logic compatible only with `string`
+				? this.tokens( path.slice( 0, path.length - 1 ) )[ path[ path.length - 1 ] ].found.slice( 1, -1 )
+				: this.text()
+			
+			this.$.$mol_syntax2_md_code.tokenize(
+				text,
+				( name , found , chunks )=> tokens.push({ name , found , chunks }),
+			)
+			
+			return tokens as Readonly< typeof tokens >
+		}
+
+		sub() {
+			return this.row_content([])
+		}
+
+		@ $mol_mem_key
+		row_content( path: number[] ) {
+			return this.tokens( path ).map( (t,i)=> this.Token([ ... path, i ]) )
+		}
+		
+		@ $mol_mem_key
+		token_type( path: number[] ) {
+			return this.tokens( [ ... path.slice( 0, path.length - 1 ) ] )[ path[ path.length - 1 ] ].name
+		}
+		
+		@ $mol_mem_key
+		token_content( path: number[] ) {
+
+			const tokens = this.tokens( [ ... path.slice( 0, path.length - 1 ) ] ) 
+			const token = tokens[ path[ path.length - 1 ] ]
+
+			switch( token.name ) {
+				
+				case 'code-string': return [
+					token.found[0],
+					... this.row_content( path ),
+					token.found[ token.found.length - 1 ],
+				] 
+				
+				default: return [ token.found ]
+			}
+
+		}
+		
+		@ $mol_mem_key
+		token_text( path: number[] ) {
+
+			const tokens = this.tokens( [ ... path.slice( 0, path.length - 1 ) ] ) 
+			const token = tokens[ path[ path.length - 1 ] ]
+
+			return token.found
+
+		}
+		
+		*view_path(
+			check: ( text: string, path : $mol_view[] )=> boolean,
+			path = [] as $mol_view[],
+		): Generator< $mol_view[] > {
+
+			path = [ ... path, this ]
+			
+			if( check( this.text(), path ) ) yield path
+			
+		}
+
+	}
+
+}
