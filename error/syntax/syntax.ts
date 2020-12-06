da9b25741307bd1d141c21bf08c1308cb3793a7d
jin <jin@hyoo.ru>
2020-12-06 20:34:36 +0300

    $mol_error_syntax: store all params to fields

--- error/syntax/syntax.ts
+++ error/syntax/syntax.ts
@@ -3,11 +3,11 @@ namespace $ {
 	/** Syntax error with cordinates and source line snippet. */
 	export class $mol_error_syntax extends SyntaxError {
 		constructor (
-			message: string,
-			line: string,
+			public reason: string,
+			public line: string,
 			public span: $mol_span,
 		) {
-			super(`${ message }\n${ span }\n${ line.substring( 0 , span.col - 1 ).replace( /\S/g, ' ' ) }${ ''.padEnd( span.length, '!' ) }\n${ line }`)
+			super(`${ reason }\n${ span }\n${ line.substring( 0 , span.col - 1 ).replace( /\S/g, ' ' ) }${ ''.padEnd( span.length, '!' ) }\n${ line }`)
 		}
 
 	}
