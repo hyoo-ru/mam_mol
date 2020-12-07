57ae94c8b40275af850df33b3d948dba1e2e8959
jin <jin@hyoo.ru>
2020-12-07 18:26:33 +0300

    $mol_exec: env override support. $mol_env - returns env vars.

---
+++ env/env.node.ts
@@ -0,0 +1,5 @@
+namespace $ {
+	$.$mol_env = function $mol_env() {
+		return this.process.env
+	}
+}
---
+++ env/env.ts
@@ -0,0 +1,5 @@
+namespace $ {
+	export function $mol_env() {
+		return {} as Record< string, string | undefined >
+	}
+}
