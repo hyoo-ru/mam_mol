1fc33d604a31eff3c7891c7810fba7f9c5dfbe42
jin <jin@hyoo.ru>
2020-12-07 19:21:56 +0300

    $mol_tree2_from_json: switch -> ifs for better typing.

--- tree2/from/json/json.ts
+++ tree2/from/json/json.ts
@@ -2,31 +2,21 @@ namespace $ {
 
 	export function $mol_tree2_from_json( json: any, span = $mol_span.unknown ): $mol_tree2 {
 
-		switch( true ) {
-
-			case typeof json === 'boolean':
-			case typeof json === 'number':
-			case json === null:
-			{
-				return new $mol_tree2( String( json ) , '' , [] , span )
-			}
-			
-			case typeof json === 'string':
-			{
-				return $mol_tree2.data( json as string , [], span )
-			}
-
-			case Array.isArray( json ):
-			{
-				const sub = ( json as any[] ).map( json => $mol_tree2_from_json( json , span ) )
-				return new $mol_tree2( '/' , '', sub, span )
-			}
-
-			case json instanceof Date:
-			{
-				return new $mol_tree2( '', ( json as Date ).toISOString() , [] , span )
-			}
+		if( typeof json === 'boolean' || typeof json === 'number' || json === null ) {
+			return new $mol_tree2( String( json ) , '' , [] , span )
+		}
 		
+		if( typeof json === 'string' ) {
+			return $mol_tree2.data( json , [], span )
+		}
+
+		if( Array.isArray( json ) ) {
+			const sub = json.map( json => $mol_tree2_from_json( json , span ) )
+			return new $mol_tree2( '/' , '', sub, span )
+		}
+			
+		if( json instanceof Date ) {
+			return new $mol_tree2( '', json.toISOString() , [] , span )
 		}
 
 		if( typeof json.toJSON === 'function' ) {
