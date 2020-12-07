ca5628d402f182e48dd3a056fe4c7510248d168a
jin <jin@hyoo.ru>
2020-12-07 20:19:03 +0300

    $mol_tree2: bench improved

--- tree2/bench/bench.ts
+++ tree2/bench/bench.ts
@@ -4,6 +4,12 @@ namespace $ {
 	let json: any
 	let out: NodeJS.WritableStream
 
+	async function measure( label: string, task: ()=> Promise<any> ) {
+		console.time( label )
+			await task()
+		console.timeEnd( label )
+	}
+
 	async function run() {
 
 		const src = 'https://raw.githubusercontent.com/discoveryjs/json-ext/b1ef3c85ac9e3224159d3747379fc024f80570f7/benchmarks/fixture/medium.json'
@@ -11,38 +17,40 @@ namespace $ {
 		const buf = await res.arrayBuffer()
 		$node.fs.writeFileSync( 'mol/tree2/bench/-big.json', Buffer.from( buf ) )
 
-		console.time('json <= JSON.parse <= file')
+		await measure( 'json <= string <= file', async()=> {
 			json = JSON.parse( $node.fs.readFileSync('mol/tree2/bench/-big.json').toString() )
-		console.timeEnd('json <= JSON.parse <= file')
+		} )
 
-		console.time('json => JSON.stringify => file')
+		await measure( 'json => string => file', async()=> {
 			$node.fs.writeFileSync( 'mol/tree2/bench/-big.json', JSON.stringify( json, null, '\t' ) )
-		console.timeEnd('json => JSON.stringify => file')
+		} )
 
-		console.time('tree <= json')
-			let tree = $mol_tree2_from_json( json )
-		console.timeEnd('tree <= json')
+		await measure( 'tree <= json', async()=> {
+			tree = $mol_tree2_from_json( json )
+		} )
+
 		json = null
 
-		console.time('tree => string => file')
+		await measure( 'tree => string => file', async()=> {
 			$node.fs.writeFileSync( 'mol/tree2/bench/-big.tree', tree.toString() )
-		console.timeEnd('tree => string => file')
+		} )
 
-		console.time('tree => file stream')
-			// $node.fs.writeFileSync( 'mol/tree2/bench/-big.tree', tree.toString() )
+		await measure( 'tree => file stream', async()=> {
 			out = $node.fs.createWriteStream('mol/tree2/bench/-big.tree')
 			await $mol_tree2_to_stream( tree, out )
 			await new Promise( done => out.end( ()=> done( null ) ) )
-		console.timeEnd('tree => file stream')
+		} )
+
 		out = null!
 		tree = null!
 
-		console.time('tree <= string <= file')
+		await measure( 'tree <= string <= file', async()=> {
 			tree = $$.$mol_tree2_from_string(
 				$node.fs.readFileSync('mol/tree2/bench/-big.tree').toString(),
 				$mol_span.begin( 'mol/tree2/bench/-big.tree' ),
 			)
-		console.timeEnd('tree <= string <= file')
+		} )
+
 		tree = null!
 
 	}
--- tree2/bench/readme.md
+++ tree2/bench/readme.md
@@ -3,8 +3,10 @@
 ```sh
 npm start mol/tree2/bench && node mol/tree2/bench/-/node.js
 
-JSON.parse: 123.025ms
-JSON => tree: 201.224ms
-tree dump: 2.822s
-tree parse: 230.949ms
+json <= string <= file: 104.564ms
+json => string => file: 175.846ms
+tree <= json: 167.657ms
+tree => string => file: 222.12ms
+tree => file stream: 2.546s
+tree <= string <= file: 278.535ms
 ```
