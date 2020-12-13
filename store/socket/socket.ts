79a894b93e29056fc55ca0d931489ac025a1bd1e
nin-jin <nin-jin@users.noreply.github.com>
2020-12-13 23:44:30 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- store/socket/socket.ts
+++ store/socket/socket.ts
@@ -13,42 +13,57 @@ namespace $ {
 
 				const socket = new $mol_dom_context.WebSocket( this.base() )
 				socket.onopen = ()=> done( socket )
-				socket.onmessage = event => {
+				socket.onmessage = $mol_fiber.func( event => {
 
 					const message = JSON.parse( event.data )
 					
 					if( !Array.isArray( message ) ) return
 					if( typeof message[0] !== 'string' ) return
-					if( typeof message[1] !== 'object' ) return
+					
+					const handler = this._handlers.get( message[0] )
+					if( handler ) {
+						this._handlers.delete( message[0] )
+						handler( message[1] ?? null )
+						return
+					}
 
+					if( typeof message[1] !== 'object' ) return
 					$mol_mem_cached( ()=> this.value( message[0] ), message[1] )
 
-				}
+				} )
 
-				socket.onclose = socket.onerror = ()=> {
+				socket.onclose = socket.onerror = $mol_fiber.func( ()=> {
 					new this.$.$mol_after_timeout( 1000, ()=> {
 						atom.complete()
 						atom.obsolete()
 						atom.schedule()
 					} )
-				}
+				} )
 				
 				return socket
 			
 			} ) )()
 		}
 
+		_handlers = new Map< string, ( a: any )=> void >()
+
 		@ $mol_mem_key
 		value( key: string, next?: any ) {
 
-			this.active()
-			
-			this.socket().send(
-				JSON.stringify([
-					key,
-					... next === undefined ? [] : [ next ]
-				])
-			)
+			$mol_fiber.run( ()=> {
+				this.socket().send(
+					JSON.stringify([
+						key,
+						... next === undefined ? [] : [ next ]
+					])
+				)
+			})
+
+			if( !next ) {
+				return $mol_fiber_sync( ()=> new Promise( done => {
+					this._handlers.set( key, done )
+				} ) )()
+			}
 
 			return next ?? null
 		}
