b330ed4585eb8c05800948094f0dc4aa0e9a5ed5
nin-jin <nin-jin@users.noreply.github.com>
2021-01-07 13:12:40 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- regexp/mt/list/list.ts
+++ regexp/mt/list/list.ts
@@ -2,7 +2,7 @@ namespace $ {
 
 	export let $mol_regexp_mt_list_item = $mol_regexp.from([
 		{ indent: $mol_regexp.repeat('  ') },
-		{ marker: '-' },
+		{ marker: [ '-', $mol_regexp.or, '+' ] },
 		' ',
 		{ content: $mol_regexp_mt_line_content },
 		$mol_regexp.line_end,
