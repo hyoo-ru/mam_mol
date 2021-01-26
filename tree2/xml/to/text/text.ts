c8e2bf0bb0c2ae48fdd3372b203b3558ab0069ac
jin <jin@hyoo.ru>
2021-01-26 12:24:26 +0800

    $mol_tree2_xml_to_text - xml.tree to XML string

---
+++ tree2/xml/to/text/text.ts
@@ -0,0 +1,93 @@
+namespace $ {
+
+	function attrs_belt( separator : string ) : Record< string, $mol_tree2_hack<never> > {
+		return {
+
+			'': ( input )=> [
+				input.data( ' ' ),
+				input.data( $mol_html_encode( input.type ) ),
+				... input.value ? [
+					input.data( '"' ),
+					input.data( $mol_html_encode( input.value ) ),
+					input.data( '"' ),
+				] : [],
+				... input.hack({
+
+					'': ( input )=> {
+
+						if( !input.type ) return [
+							input.data( separator ),
+							input.data( '"' ),
+							input.data( $mol_html_encode( input.text() ) ),
+							input.data( '"' ),
+						]
+
+						$mol_fail( input.error( 'Wrong attribute value' ) )
+
+					},
+
+				}),
+			],
+
+		}
+	}
+	
+	export function $mol_tree2_xml_to_text( xml: $mol_tree2 ) {
+		return xml.list([
+			xml.struct( 'line',
+				xml.hack({
+
+					'@': ( input, belt )=> [],
+
+					'--': ( input, belt )=> [
+						input.data( '<!-- ' ),
+						... input.hack( belt ),
+						input.data( ' -->' ),
+					],
+
+					'?': ( input, belt )=> [
+						input.data( '<?' ),
+						input.kids[0].data( input.kids[0].type ),
+						... input.kids[0].hack( attrs_belt( '=' ) ),
+						input.data( '?>' ),
+					],
+
+					'!': ( input, belt )=> [
+						input.data( '<!' ),
+						input.kids[0].data( input.kids[0].type ),
+						... input.kids[0].hack( attrs_belt( ' ' ) ),
+						input.data( '>' ),
+					],
+
+					'': ( input, belt )=> {
+
+						if( !input.type ) return [
+							input.data( $mol_html_encode( input.text() ) ),
+						]
+
+						const attrs = input.select( '@', '' ).hack( attrs_belt( '=' ) )
+						const content = input.hack( belt )
+						
+						return [
+							input.data( `<` ),
+							input.data( input.type ),
+							... attrs,
+							... content.length ? [
+								input.data( `>` ),
+								... content,
+								input.data( `</` ),
+								input.data( input.type ),
+								input.data( `>` ),
+							] : [
+								input.data( ` />` ),
+							]
+						]
+
+					},
+
+				}),
+			),
+		])
+	}
+
+}
