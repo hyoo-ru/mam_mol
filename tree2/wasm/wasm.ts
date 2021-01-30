a342fc06afe5ea48510adbab988784756d39cd20
jin <jin@hyoo.ru>
2021-01-30 19:53:04 +0800

    $mol_tree2_wasm: supports custom, type and import sections

--- tree2/wasm/wasm.ts
+++ tree2/wasm/wasm.ts
@@ -6,79 +6,135 @@ namespace $ {
 
 		const int = ( int : number , span : $mol_span ) => bytes( $mol_leb128_encode( int ) , span )
 	
-		const array = ( items : readonly $mol_tree2[] , span : $mol_span ) => [ ... int( items.length , span ) , ... items ]
+		const dyn = ( items : readonly $mol_tree2[] , span : $mol_span ) => [ ... int( items.length , span ) , ... items ]
 	
-		const str = ( str : string , span : $mol_span ) => array( $mol_tree2_bin_from_string( str , span ).kids , span )
+		const str = ( str : string , span : $mol_span ) => dyn( $mol_tree2_bin_from_string( str , span ).kids , span )
 	
-		return code.list( code.hack({
+		const prolog = [ 0 , 0x61 , 0x73 , 0x6d ]
+		const version = [ 0x1 , 0 , 0 , 0 ]
+		
+		const section = (
+			name: typeof $mol_wasm_section_types[ number ],
+		)=> {
+			return <
+				Belt extends $mol_tree2_belt<any>,
+				Context extends { up: string },
+			>(
+				input: $mol_tree2,
+				belt: Belt,
+				context: Context,
+			) => {
 
-			'' : ( input , belt )=> $mol_fail( input.error( `Unknown wasm node` ) ) ,
-			
-			'wasm.module' : ( input , belt )=> {
-
-				const prolog = [ 0 , 0x61 , 0x73 , 0x6d ]
-				const version = [ 0x1 , 0 , 0 , 0 ]
+				if( context.up !== 'module' ) {
+					$mol_fail( input.error( `${ name } should be in module` ) )
+				}
 				
-				return [
-					... bytes( [ ... prolog , ... version ] , input.span ) ,
-					... input.hack( belt , { up : input.type } ) ,
-				]
-
-		 	} ,
-			
-			'wasm.section' : ( input , belt , context ) => {
-
-				if( context.up !== 'wasm.module' ) {
-					$mol_fail( input.error( `wasm.section should be in wasm.module` ) )
+				context = {
+					... context,
+					up: input.type,
 				}
 				
 				return [
-					... input.select( 'wasm.id' ).hack( belt , { up : input.type } ) ,
-					... array( [
-						... input.select( 'wasm.name' ).hack( belt , { up : input.type } ) ,
-						... input.select( 'wasm.payload' ).hack( belt , { up : input.type } ) ,
-					] , input.span ) ,
+					... bytes( [ $mol_wasm_section_types[ name ] ], input.span ),
+					... dyn( input.hack( belt, context ), input.span ),
 				]
 	
-			} ,
-	
-			'wasm.id' : ( input , belt , context ) => {
+			}
+		}
+		
+		const import_ = (
+			name: typeof $mol_wasm_import_types[ number ],
+		)=> {
+			return <
+				Belt extends $mol_tree2_belt<any>,
+				Context extends { up: string },
+			>(
+				input: $mol_tree2,
+				belt: Belt,
+				context: Context,
+			) => {
 
-				if( context.up !== 'wasm.section' ) {
-					$mol_fail( input.error( `wasm.id should be in wasm.section` ) )
+				if( context.up !== 'import' ) {
+					$mol_fail( input.error( `${ name } should be in import` ) )
 				}
 				
-				const type_node = input.kids[0]
-				if( !type_node ) $mol_fail( input.error( `wasm.id type shoud be defined` ) )
-				
-				const type = $mol_wasm_section_types[ type_node.type.substring( 5 ) ] as number
-				if( type === undefined ) $mol_fail( type_node.error( `Unknown section type` ) )
-				
-				return bytes( [ type ] , type_node.span )
+				return [
+					... bytes( [ $mol_wasm_import_types[ name ] ], input.span ),
+					... int( Number( input.kids[0].type ), input.span ),
+				]
 	
-			} ,
-	
-			'wasm.name' : ( input , belt , context ) => {
+			}
+		}
+		
+		return code.list([
+			... bytes( prolog , code.span ) ,
+			... bytes( version , code.span ) ,
+			... code.hack({
 
-				if( context.up !== 'wasm.section' ) {
-					$mol_fail( input.error( `wasm.name should be in wasm.section` ) )
-				}
+				'' : ( input , belt )=> $mol_fail( input.error( `Unknown wasm node` ) ) ,
 				
-				return str( input.value , input.span )
-	
-			} ,
-	
-			'wasm.payload' : ( input , belt , context ) => {
+				'customsec' : section( 'custom' ) ,
+				'typesec' : section( 'type' ) ,
+				'importsec' : section( 'import' ) ,
+		
+				'functype': ( input, belt, context )=> {
 
-				if( context.up !== 'wasm.section' ) {
-					$mol_fail( input.error( `wasm.payload should be in wasm.section` ) )
-				}
-				
-				return input.hack( belt )
-	
-			} ,
-	
-		} , { up : '' } ) )
+					if( context.up !== 'typesec' ) {
+						$mol_fail( input.error( `functype should be in typesec` ) )
+					}
+					
+					return [
+						... bytes( [ 0x60 ], input.span ),
+						... input.hack( belt, context ),
+					]
+		
+				} ,
+		
+				'import': ( input, belt, context )=> {
+
+					if( context.up !== 'importsec' ) {
+						$mol_fail( input.error( `import should be in importsec` ) )
+					}
+					
+					context = {
+						... context,
+						up: input.type,
+					}
+					
+					const ext = input.kids[0]
+					
+					return [
+						... ( [] as $mol_tree2[] ).concat(
+							... ext.type.split( '.' ).map( name => str( name, ext.span ) )
+						),
+						... ext.hack( {
+							'': ( input , belt )=> $mol_fail( input.error( `Unknown import type` ) ) ,
+							func: import_( 'func' ),
+							table: import_( 'table' ),
+							mem: import_( 'mem' ),
+							global: import_( 'global' ),
+						}, context ),
+					]
+					
+				} ,
+		
+				'vec': ( input, belt, context )=> [
+					... int( input.kids.length, input.span ),
+					... input.hack( belt, context )
+				],
+		
+				'name': input => str( input.text(), input.span ),
+		
+				'i32': input => bytes( [ 0x7F ], input.span ),
+				'i64': input => bytes( [ 0x7E ], input.span ),
+				'f32': input => bytes( [ 0x7D ], input.span ),
+				'f64': input => bytes( [ 0x7C ], input.span ),
+
+			} , {
+				up: 'module',
+				section: '',
+			} )
+		])
 
 	}
 
