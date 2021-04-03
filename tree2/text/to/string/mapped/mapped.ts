a90224f06b6089b1c42e1bb4b616751b0b1e1f73
jin <jin@hyoo.ru>
2021-04-03 15:45:31 +0300

    $mol_tree2_text_to_string_mapped: string serialization with imprinted sourcemaps

---
+++ tree2/text/to/string/mapped/mapped.ts
@@ -0,0 +1,27 @@
+namespace $ {
+
+	export function $mol_tree2_text_to_string_mapped( this: $, text: $mol_tree2, type: 'js' | 'css' ) {
+		
+		const code = this.$mol_tree2_text_to_string( text )
+		const map = this.$mol_tree2_text_to_sourcemap( text )
+		
+		let res = code
+		
+		const map_uri = `data:application/json,${ this.encodeURIComponent( JSON.stringify( map ) ) }`
+		
+		if( type === 'js' ) res += `\n//# sourceMappingURL=${ map_uri }`
+		else res += `\n/*# sourceMappingURL=${ map_uri } */`
+		
+		return res
+
+	}
+
+	export function $mol_tree2_text_to_string_mapped_js( this: $, text: $mol_tree2 ) {
+		return this.$mol_tree2_text_to_string_mapped( text, 'js' )
+	}
+	
+	export function $mol_tree2_text_to_string_mapped_css( this: $, text: $mol_tree2 ) {
+		return this.$mol_tree2_text_to_string_mapped( text, 'css' )
+	}
+	
+}
