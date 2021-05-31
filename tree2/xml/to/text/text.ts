81d11f3f14e1033885a17ce8ff9d17d39fc3dca8
jin <jin@hyoo.ru>
2021-05-31 15:00:27 +0300

    $mol_tree2_xml_to_text: fixed attributes

--- tree2/xml/to/text/text.ts
+++ tree2/xml/to/text/text.ts
@@ -70,7 +70,7 @@ namespace $ {
 						input.data( $mol_html_encode( input.text() ) ),
 					]
 
-					const attrs = input.select( '@', '' ).hack( attrs_belt( '=' ) )
+					const attrs = input.select( '@', null ).hack( attrs_belt( '=' ) )
 					const content = input.hack( belt )
 					
 					return [
