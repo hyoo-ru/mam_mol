0ceeddb98be2d834b92dca47e2c5dcd9d79a3ad4
nin-jin <nin-jin@users.noreply.github.com>
2021-07-06 11:13:47 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/quine/-/node.test.js
+++ app/quine/-/node.test.js
@@ -4983,7 +4983,8 @@ var $;
                 return chunks;
             }
             strings() {
-                const regexp = $.$mol_regexp.from({ needle: this.needle() }, { ignoreCase: true });
+                const variants = { ...this.needle().split(/\s+/g).filter(Boolean) };
+                const regexp = $.$mol_regexp.from({ needle: variants }, { ignoreCase: true });
                 return this.haystack().split(regexp);
             }
             string(index) {
