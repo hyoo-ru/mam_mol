705dceab943806f7488a3e98f25c77d0abc1b6d3
nin-jin <nin-jin@users.noreply.github.com>
2021-07-11 04:26:30 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- data/optional/optional.test.ts
+++ data/optional/optional.test.ts
@@ -1,21 +1,36 @@
 namespace $ {
+	
+	const Age = $mol_data_optional( $mol_data_number )
+	const Age_or_zero = $mol_data_optional( $mol_data_number, $mol_const( 0 ) )
+	
 	$mol_test({
 
-		'Is not present' () {
-			$mol_data_optional( $mol_data_number )( undefined )
+		'Is not present'() {
+			$mol_assert_equal(
+				Age( undefined ),
+				undefined,
+			)
 		} ,
 
-		'Is present' () {
-			$mol_data_optional( $mol_data_number )( 0 )
+		'Is present'() {
+			$mol_assert_equal(
+				Age( 0 ),
+				0,
+			)
 		} ,
 
-		'Is null' () {
-			$mol_assert_fail( ()=> {
+		'Fallbacked'() {
+			$mol_assert_equal(
+				Age_or_zero( undefined ),
+				0,
+			)
+		} ,
 
-				const Type = $mol_data_optional( $mol_data_number )
-				Type( null as any )
-
-			} , 'null is not a number' )
+		'Is null'() {
+			$mol_assert_fail(
+				()=> Age( null as any ),
+				'null is not a number',
+			)
 		} ,
 
 	})
--- data/optional/optional.ts
+++ data/optional/optional.ts
@@ -1,14 +1,20 @@
 namespace $ {
 
-	export function $mol_data_optional< Sub extends $mol_data_value >( sub : Sub ) {
+	export function $mol_data_optional<
+		Sub extends $mol_data_value,
+		Fallback extends ReturnType< Sub > | undefined
+	>( 
+		sub: Sub,
+		fallback?: ()=> Fallback
+	) {
 
 		return $mol_data_setup( ( val : Parameters<Sub>[0] | undefined ) => {
 			
-			if( val === undefined ) return undefined
+			if( val === undefined ) return fallback?.() ?? undefined
 			
 			return sub( val ) as ReturnType<Sub>
 			
-		} , sub )
+		} , { sub, fallback } )
 
 	}
 	
