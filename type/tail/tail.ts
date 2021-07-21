ba721d7fb650aaafc1cb8c76ba5c81fe2af8609e
jin <jin@hyoo.ru>
2021-07-21 21:19:14 +0800

    $mol_type_tail: simplified

--- type/tail/tail.ts
+++ type/tail/tail.ts
@@ -6,12 +6,8 @@ namespace $ {
 	 * 	$mol_type_tail<[ 1 , 2 , 3 ]> // [ 2, 3 ]
 	 */
 	export type $mol_type_tail< Tuple extends readonly any[] > =
-		(
-			( ...tail : Tuple )=> any
-		) extends (
-			( head : any , ...tail : infer Tail )=> any
-		)
+		Tuple extends [ any , ... infer Tail ]
 			? Tail
-			: never
+			: []
 
 }
