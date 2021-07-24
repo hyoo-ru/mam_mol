62a39c27815c0464cc4be9541f4dd608765e9688
jin <jin@hyoo.ru>
2021-07-24 11:43:47 +0800

    $mol_db_response: simplified

--- db/response/response.ts
+++ db/response/response.ts
@@ -5,15 +5,9 @@ namespace $ {
 		request: IDBRequest< Result >
 	) {
 		
-		let done: ( result: Result )=> void
-		let fail: ( error: Error )=> void
-		
-		request.onerror = ()=> fail( new Error( request.error!.message ) )
-		request.onsuccess = ()=> done( request.result )
-			
-		return new Promise< Result >( ( d, f )=> {
-			done = d
-			fail = f
+		return new Promise< Result >( ( done, fail )=> {
+			request.onerror = ()=> fail( new Error( request.error!.message ) )
+			request.onsuccess = ()=> done( request.result )
 		} )
 		
 	}
