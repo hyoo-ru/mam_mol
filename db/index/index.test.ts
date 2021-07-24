673a6e432f018f81af0b702bd0d0a62c195412ca
nin-jin <nin-jin@users.noreply.github.com>
2021-07-24 04:44:35 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- db/index/index.test.ts
+++ db/index/index.test.ts
@@ -24,8 +24,8 @@ namespace $ {
 				await users.put( { name: 'John' }, 'john' )
 
 				const { names } = users.indexes
-				$mol_assert_like( await names.get( [ 'Jin' ], 10 ), [{ name: 'Jin' }] )
-				$mol_assert_like( await names.get( [ 'John' ], 10 ), [{ name: 'John' }] )
+				$mol_assert_like( await names.get([ 'Jin' ]), { name: 'Jin' } )
+				$mol_assert_like( await names.get([ 'John' ]), { name: 'John' } )
 				$mol_assert_like( await names.count(), 2 )
 				
 				try {
@@ -65,11 +65,17 @@ namespace $ {
 				const { users } = trans.stores
 				await users.put( { first: 'Jin', last: 'Johnson' }, 'jin' )
 				await users.put( { first: 'John', last: 'Jinson' }, 'john' )
+				await users.put( { first: 'Bond', last: 'James' }, '007' )
 				
 				const { names } = users.indexes
-				$mol_assert_like( await names.get( [ 'Jin', 'Johnson' ], 10 ), [{ first: 'Jin', last: 'Johnson' }] )
-				$mol_assert_like( await names.get( [ 'John', 'Jinson' ], 10 ), [{ first: 'John', last: 'Jinson' }] )
-				$mol_assert_like( await names.count(), 2 )
+				$mol_assert_like( await names.get([ 'Jin', 'Johnson' ]), { first: 'Jin', last: 'Johnson' } )
+				$mol_assert_like( await names.get([ 'John', 'Jinson' ]), { first: 'John', last: 'Jinson' } )
+				$mol_assert_like( await names.count(), 3 )
+				
+				$mol_assert_like(
+					await names.select( IDBKeyRange.bound( [ 'Jin', 'Johnson' ], [ 'John', 'Jinson' ] ) ),
+					[ { first: 'Jin', last: 'Johnson' }, { first: 'John', last: 'Jinson' } ],
+				)
 				
 			} finally {
 				
@@ -106,12 +112,11 @@ namespace $ {
 				
 				const { names, ages } = users.indexes
 				
-				$mol_assert_like( await names.get( [ 'Jin' ], 10 ), [ { name: 'Jin', age: 18 } ] )
-				$mol_assert_like( await names.get( [ 'John' ], 10 ), [ { name: 'John', age: 18 } ] )
+				$mol_assert_like( await names.select([ 'Jin' ]), [{ name: 'Jin', age: 18 }] )
+				$mol_assert_like( await names.select([ 'John' ]), [{ name: 'John', age: 18 }] )
 				$mol_assert_like( await names.count(), 2 )
 				
-				$mol_assert_like( await ages.get( [ 18 ] ), [ { name: 'Jin', age: 18 } ] )
-				$mol_assert_like( await ages.get( [ 18 ], 10 ), [ { name: 'Jin', age: 18 }, { name: 'John', age: 18 } ] )
+				$mol_assert_like( await ages.select([ 18 ]), [ { name: 'Jin', age: 18 }, { name: 'John', age: 18 } ] )
 				$mol_assert_like( await ages.count(), 2 )
 				
 			} finally {
