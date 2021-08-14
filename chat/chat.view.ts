fd35f3fa55f70d9121f41cb9acaab1418b294555
jin <jin@hyoo.ru>
2021-08-14 22:51:56 +0300

    $mol_chat: fixer external link

--- chat/chat.view.ts
+++ chat/chat.view.ts
@@ -12,15 +12,17 @@ namespace $.$$ {
 
 		@ $mol_mem
 		external() {
-			const seed = this.seed() || this.$.$mol_dom_context.location.host
-			return `https://talks.hyoo.ru/#!chat=${ encodeURIComponent( seed ) }`
+			const seed = this.seed()
+			const origin = new URL( this.$.$mol_state_arg.href() ).origin
+			return `https://talks.hyoo.ru/#!chat=${ encodeURIComponent( origin + '/' + seed ) }`
 		}
 		
 		@ $mol_mem
 		embed() {
+			const seed = this.seed()
 			const lights = this.$.$mol_lights() ? 'on' : 'off'
 			const embed = this.$.$mol_state_arg.href()
-			return `${ this.external() }/embed=${ encodeURIComponent( embed ) }/mol_lights=${ lights }`
+			return `https://talks.hyoo.ru/#!chat=${ encodeURIComponent( seed ) }/embed=${ encodeURIComponent( embed ) }/mol_lights=${ lights }`
 		}
 		
 	}
