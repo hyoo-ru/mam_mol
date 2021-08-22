54640a173ef977a501f0e061652a0b03df29522f
jin <jin@hyoo.ru>
2021-08-22 11:45:09 +0300

    $mol_lights: store to arg taken from arg

--- lights/lights.ts
+++ lights/lights.ts
@@ -8,14 +8,18 @@ namespace $ {
 	
 	export function $mol_lights( this : $ , next? : boolean ) {
 		
-		const base = null
-			?? parse( this.$mol_state_arg.value( 'mol_lights' ) )
-			?? this.$mol_media.match( '(prefers-color-scheme: light)' )
+		const arg = parse( this.$mol_state_arg.value( 'mol_lights' ) )
+		
+		const base = arg ?? this.$mol_media.match( '(prefers-color-scheme: light)' )
 		
 		if( next === undefined ) {
 			return this.$mol_state_local.value< boolean >( '$mol_lights' ) ?? base
 		} else {
-			this.$mol_state_local.value( '$mol_lights' , next === base ? null : next )
+			if( arg ) {
+				this.$mol_state_arg.value( 'mol_lights' , next ? 'on' : 'off' )
+			} else {
+				this.$mol_state_local.value( '$mol_lights' , next === base ? null : next )
+			}
 			return next
 		}
 		
