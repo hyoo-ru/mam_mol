6bb0251c49136a705ebfb0386b9e018d4fa77d94
nin-jin <nin-jin@users.noreply.github.com>
2021-08-25 21:53:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- plot/all/-/node.js
+++ plot/all/-/node.js
@@ -4008,12 +4008,19 @@ var $;
             }
             graphs_visible() {
                 const viewport = this.dimensions_viewport();
+                const size_real = this.size_real();
+                const max_x = (viewport.x.max - viewport.x.min) / size_real.x;
+                const max_y = (viewport.y.max - viewport.y.min) / size_real.y;
                 return this.graphs().filter(graph => {
                     const dims = graph.dimensions();
                     if (dims.x.min > dims.x.max)
                         return true;
                     if (dims.y.min > dims.y.max)
                         return true;
+                    if (dims.x.max - dims.x.min < max_x)
+                        return false;
+                    if (dims.y.max - dims.y.min < max_y)
+                        return false;
                     if (dims.x.min > viewport.x.max)
                         return false;
                     if (dims.x.max < viewport.x.min)
