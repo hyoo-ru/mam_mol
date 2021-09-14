01d9e49399457255e61486aac42e661044989d4c
jin <jin@hyoo.ru>
2021-09-14 15:00:11 +0300

    $mol_tree2_wasm_to: some funcs support

--- tree2/wasm/to/bin/bin.ts
+++ tree2/wasm/to/bin/bin.ts
@@ -37,7 +37,7 @@ namespace $ {
 				const section = [] as $mol_tree2[]
 				section.push( ... str( name.type, name.span ) )
 				
-				body.push( ... bytes( [ $mol_wasm_section_types.custom ], custom.span ) )
+				body.push( ... bytes( [ $mol_wasm_bin_section.custom ], custom.span ) )
 				body.push( ... dyn( section, custom.span ) )
 				
 			}
@@ -61,21 +61,21 @@ namespace $ {
 				const params = name.select( '=>', null )
 				section.push( ... array_prolog( params ) )
 				for( const param of params.kids ) {
-					section.push( ... bytes( [ $mol_wasm_value_types[ param.type ] ], param.span ) )
+					section.push( ... bytes( [ $mol_wasm_bin_valtype[ param.type ] ], param.span ) )
 				}
 				
 				const results = name.select( '<=', null )
 				section.push( ... array_prolog( results ) )
 				for( const result of results.kids ) {
 					section.push(
-						... bytes( [ $mol_wasm_value_types[ result.type ] ], result.span ),
+						... bytes( [ $mol_wasm_bin_valtype[ result.type ] ], result.span ),
 					)
 				}
 				
 			}
 			
 			body.push(
-				... bytes( [ $mol_wasm_section_types.type ], prolog.span ),
+				... bytes( [ $mol_wasm_bin_section.type ], prolog.span ),
 				... dyn( [
 					... array_prolog( types, prolog.span ),
 					... section,
@@ -108,7 +108,7 @@ namespace $ {
 					if( index === undefined ) this.$mol_fail( name.error( 'Unknown type' ) )
 					
 					section.push(
-						... bytes( [ $mol_wasm_import_types.func ], kind.span ),
+						... bytes( [ $mol_wasm_bin_external.func ], kind.span ),
 						... int( index, name.span ),
 					)
 					
@@ -117,9 +117,134 @@ namespace $ {
 			}
 			
 			body.push(
-				... bytes( [ $mol_wasm_section_types.import ], prolog.span ),
+				... bytes( [ $mol_wasm_bin_section.import ], prolog.span ),
 				... dyn( [
 					... array_prolog( imports, prolog.span ),
+					... section,
+				], prolog.span ),
+			)
+			
+		}
+		
+		funcs: {
+			
+			const funcs = code.select( 'func' )
+			if( funcs.kids.length === 0 ) break funcs
+			
+			const section = [] as $mol_tree2[]
+			
+			for( const func of funcs.kids ) {
+				
+				const name = func.kids[0]
+				
+				const index = types_mapping.get( name.type )
+				if( index === undefined ) this.$mol_fail( name.error( 'Unknown type' ) )
+				
+				section.push( ... int( index, name.span ) )
+				
+			}
+			
+			body.push(
+				... bytes( [ $mol_wasm_bin_section.func ], prolog.span ),
+				... dyn( [
+					... array_prolog( funcs, prolog.span ),
+					... section,
+				], prolog.span ),
+			)
+			
+		}
+		
+		exports: {
+			
+			const exports = code.select( 'export' )
+			if( exports.kids.length === 0 ) break exports
+			
+			const section = [] as $mol_tree2[]
+			
+			for( const export_ of exports.kids ) {
+				
+				const path = export_.kids[0]
+				const kind = path.kids[0]
+				
+				section.push( ... str( path.type, path.span ) )
+				
+				if( kind.type === 'func' ) {
+					
+					const name = kind.kids[0]
+					
+					const index = types_mapping.get( name.type )
+					if( index === undefined ) this.$mol_fail( name.error( 'Unknown type' ) )
+					
+					section.push(
+						... bytes( [ $mol_wasm_bin_external.func ], kind.span ),
+						... int( index, name.span ),
+					)
+					
+				}
+				
+			}
+			
+			body.push(
+				... bytes( [ $mol_wasm_bin_section.export ], prolog.span ),
+				... dyn( [
+					... array_prolog( exports, prolog.span ),
+					... section,
+				], prolog.span ),
+			)
+			
+		}
+		
+		codes: {
+			
+			const funcs = code.select( 'func' )
+			if( funcs.kids.length === 0 ) break codes
+			
+			const section = [] as $mol_tree2[]
+			
+			for( const func of funcs.kids ) {
+				
+				const body = [] as $mol_tree2[]
+				
+				// locals
+				body.push(
+					... int( 0, func.span ),
+				)
+				
+				for( const expr of func.kids[0].kids ) {
+					
+					if( typeof $mol_wasm_bin_instr_unary[ expr.type ] === 'number' ) {
+						body.push(
+							... bytes( [ $mol_wasm_bin_instr_unary[ expr.type ] ], expr.span ),
+							... int( Number( expr.kids[0].type ), expr.kids[0].span ),
+						)
+						continue
+					}
+					
+					if( typeof $mol_wasm_bin_instr_nullary[ expr.type ] === 'number' ) {
+						body.push(
+							... bytes( [ $mol_wasm_bin_instr_nullary[ expr.type ] ], expr.span ),
+						)
+						continue
+					}
+					
+					$mol_fail( expr.error( 'Unknown wasm instruction' ) )
+				}
+				
+				// end
+				body.push(
+					... bytes( [ $mol_wasm_bin_instr['end'] ], func.span ),
+				)
+				
+				section.push(
+					... dyn( body, func.span ),
+				)
+				
+			}
+			
+			body.push(
+				... bytes( [ $mol_wasm_bin_section.code ], prolog.span ),
+				... dyn( [
+					... array_prolog( funcs, prolog.span ),
 					... section,
 				], prolog.span ),
 			)
