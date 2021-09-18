5dbc64e0678dc0275cb3e6bacdb5abfef9d55103
jin <jin@hyoo.ru>
2021-09-19 00:36:55 +0300

    $mol_gallery - gracefully fills page with various size items.

---
+++ gallery/gallery.view.ts
@@ -0,0 +1,33 @@
+namespace $.$$ {
+	export class $mol_gallery extends $.$mol_gallery {
+		
+		@ $mol_mem
+		sub(): readonly $mol_view[] {
+			
+			const items = this.items()
+			if( items.length <= 3 ) return items
+			
+			return [
+				this.Side(0),
+				this.Side(1),
+			]
+			
+		}
+
+		@ $mol_mem_key
+		side_items( id: number ) {
+			const items = this.items()
+			const middle = items.length % 2
+				? Math.ceil( items.length / 3 )
+				: items.length / 2
+			return id
+				? items.slice( middle )
+				: items.slice( 0, middle )
+		}
+		
+		side_size( id: number ) {
+			return String( this.side_items( id ).length )
+		}
+		
+	}
+}
