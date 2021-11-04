3e58f9035ca0f0e368cb80287bcba22ec573082c
Vitaliy V. Makeev <w.makeev@gmail.com>
2021-11-04 10:47:46 +0500

    Remove old code

--- app/demo/demo.view.ts
+++ app/demo/demo.view.ts
@@ -6,9 +6,7 @@ namespace $.$$ {
 			const selected = this.selected()
 			if( selected ) {
 				const names = this.names_demo() 
-				/*if( names.length === 1 ) {
-					return `$${ selected.replace( /_demo.* / , '' ) }: ${ this.Sample_large( names[0] ).Widget().title() }`
-				}*/
+
 				return `$${ selected }`
 			}
 			
@@ -27,60 +25,6 @@ namespace $.$$ {
 			}
 			return next.sort()
 		}
-		
-		// @ $mol_mem
-		// nav_hierarchy() {
-		// 	const names = this.names_demo_filtered()
-			
-		// 	const hierarchy = {} as { [ prefix : string ] : $mol_grid_node }
-		// 	const root = hierarchy[ '' ] = {
-		// 		id : '' ,
-		// 		parent : null as any as $mol_grid_node ,
-		// 		sub : [] as $mol_grid_node[] ,
-		// 	}
-			
-		// 	names.forEach( name => {
-		// 		const chunks = name.split( /(?=[_.-])/ )
-		// 		let branch = root
-		// 		for( let i = 1 ; i <= chunks.length ; ++ i ) {
-		// 			const prefix = chunks.slice( 0 , i ).join( '' )
-		// 			if( !hierarchy[ prefix ] ) {
-		// 				branch.sub.push( hierarchy[ prefix ] = {
-		// 					id : prefix ,
-		// 					parent : branch ,
-		// 					sub : [] as $mol_grid_node[] ,
-		// 				} )
-		// 			}
-		// 			branch = hierarchy[ prefix ]
-		// 		}
-		// 	} )
-			
-		// 	hierarchy[ '' ].sub.map( child => reduce( child ) )
-			
-		// 	function reduce( node : $mol_grid_node ) {
-		// 		if( names.indexOf( node.id ) >= 0 ) return node
-				
-		// 		node.sub = node.sub.map( child => reduce( child ) )
-		// 		if( node.sub.length !== 1 ) return node
-				
-		// 		node.sub[0].parent = node.parent
-		// 		return node.sub[0]
-		// 	}
-			
-		// 	return hierarchy
-		// }
-		
-		// nav_option( id : string ) {
-		// 	const parent = this.nav_hierarchy()[ id ].parent
-			
-		// 	const title = `$${ id }`
-		// 	.substring( parent.id.length + 1 )
-		// 	.replace( /^[-._]|[-._]demo$/g , '' )
-		// 	.replace( /_/g , ' ' )
-		// 	.replace( /^(\w)/ , letter => letter.toUpperCase() )
-			
-		// 	return { title }
-		// }
 		
 		selected() {
 			return $mol_state_arg.value( 'demo' ) || ''
@@ -108,27 +52,6 @@ namespace $.$$ {
 		names_demo() {
 			const selected = this.selected()
 			return [ selected ]
-			// const all = this.names_demo_all()
-			
-			// const root = this.nav_hierarchy()[ selected ]
-			// if( !root ) return []
-
-			// const names : string[] = []
-			// const collect = ( node : typeof root )=> {
-			// 	const demo = `${ node.id }_demo`
-			// 	if( all.indexOf( demo ) !== -1 ) {
-			// 		if( names.indexOf( demo ) === -1 ) names.push( demo )
-			// 	} else if( all.indexOf( node.id ) !== -1 ) {
-			// 		if( names.indexOf( node.id ) === -1 ) names.push( node.id )
-			// 	} else {
-			// 		node.sub.forEach( child => collect( child ) )
-			// 	}
-			// }
-
-			// if( root.sub.length ) root.sub.forEach( child => collect( child ) )
-			// collect( root )
-			
-			// return names
 		}
 		
 		blocks() {
