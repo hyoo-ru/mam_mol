f16ba8334321b4640375bd2052274e61f5c2a1d7
nin-jin <nin-jin@users.noreply.github.com>
2021-11-25 16:41:58 +0000

    Deployed using Blazing fast GitHub Pages deploy

---
+++ report/bugsnag/bugsnag.ts
@@ -0,0 +1,69 @@
+namespace $ {
+	
+	export let $mol_report_bugsnag = ''
+	
+	self.onerror = function( msg, url, line, col, err ) {
+		
+		const el = document.activeElement
+		
+		const report = {
+			apiKey: $mol_report_bugsnag,
+			payloadVersion: 5,
+			notifier: {
+				name: '$mol_report_bugsnag',
+				version: '1',
+				url: '$mol_report_bugsnag',
+			},
+			events: [{
+				device: {
+					locale: navigator.language,
+					userAgent: navigator.userAgent,
+					time: new Date().toISOString(),
+				},
+				context: el && el.id,
+				exceptions: [{
+					message: err && err.message || err || msg,
+					errorClass: err && err.constructor.name,
+					stacktrace: [{
+						columnNumber: col,
+						file: url,
+						lineNumber: line,
+						method: '',
+					}],
+				}],
+				metaData: {
+					stack: err && err.stack,
+				},
+				request: {
+					url: document.location.href,
+					referer: document.referrer,
+				},
+			}],
+		}
+		
+		if( location.hostname === 'localhost' ) {
+			
+			console.debug( 'Error report', report )
+			
+		} else {
+		
+			fetch( 'https://notify.bugsnag.com/', {
+				method: 'post',
+				body: JSON.stringify( report ),
+			} )
+			
+		}
+		
+	}
+	
+	self.onunhandledrejection = function( event ) {
+		self.onerror( 'Unhandled Rejection', '', 0, 0, event.reason )
+	} 
+	
+	const error = console.error
+	console.error = function() {
+		error.apply( console, arguments )
+		self.onerror( 'Logged Error', '', 0, 0, arguments[0] )
+	}
+		
+}
