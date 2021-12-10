e344929e607f0508e72a0d19a91858b2716203de
jin <jin@hyoo.ru>
2021-12-10 23:38:08 +0300

    $mol_after_frame: now wait after 1 second.

--- after/frame/frame.web.ts
+++ after/frame/frame.web.ts
@@ -8,10 +8,19 @@ namespace $ {
 
 			if( this._promise ) return this._promise
 			
-			return this._promise = new Promise( done => requestAnimationFrame( ()=> {
-				this._promise = null
-				done()
-			} ) )
+			return this._promise = new Promise( done => {
+				
+				requestAnimationFrame( ()=> {
+					this._promise = null
+					done()
+				} )
+				
+				setTimeout( ()=> {
+					this._promise = null
+					done()
+				}, 100 )
+				
+			} )
 
 		}
 
