663150092b4e16219a55c9076a0caa9bcfb7a270
jin <jin@hyoo.ru>
2022-01-09 21:21:48 +0300

    $mol_tree2_edit - UI for Tree edit

---
+++ tree2/edit/edit.view.css.ts
@@ -0,0 +1,21 @@
+namespace $.$$ {
+	
+	const { rem } = $mol_style_unit
+	
+	$mol_style_define( $mol_tree2_edit, {
+		
+		Self: {
+			alignSelf: 'stretch',
+		},
+		
+		Kids: {
+			
+			padding: {
+				left: rem(1.5),
+			},
+			
+		},
+		
+	} )
+	
+}
