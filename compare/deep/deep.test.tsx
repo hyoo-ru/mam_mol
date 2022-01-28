e2a2db10ce19a5f87b615f00b5c86d3b836f71f2
jin <jin@hyoo.ru>
2022-01-28 19:04:36 +0300

    $mol_compare_deep: compare error by stack

--- compare/deep/deep.test.tsx
+++ compare/deep/deep.test.tsx
@@ -73,6 +73,22 @@ namespace $ {
 			$mol_assert_not( $mol_compare_deep( /\x22/mig , /\x22/mg ) )
 		} ,
 
+		'Error'() {
+			
+			$mol_assert_not( $mol_compare_deep( new Error( 'xxx' ) , new Error( 'xxx' ) ) )
+
+			const fail = ( message: string )=> new Error( message )
+			
+			$mol_assert_ok( $mol_compare_deep(
+				... [ 'xxx' , 'xxx' ].map( msg => new Error( msg ) ) as [ Error, Error ]
+			) )
+			
+			$mol_assert_not( $mol_compare_deep(
+				... [ 'xxx' , 'yyy' ].map( msg => new Error( msg ) ) as [ Error, Error ]
+			) )
+			
+		} ,
+
 		'Map'() {
 			$mol_assert_ok( $mol_compare_deep( new Map , new Map ) )
 			$mol_assert_ok( $mol_compare_deep( new Map([ [ 1 , [2] ] ]) , new Map([ [ 1 , [2] ] ]) ) )
