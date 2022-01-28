3fca9e27f1f6a612746bc51a76572214c22fb83d
jin <jin@hyoo.ru>
2022-01-28 12:45:30 +0300

    $mol_state_time: delay is required now

--- state/time/time.ts
+++ state/time/time.ts
@@ -3,12 +3,12 @@ namespace $ {
 	export class $mol_state_time extends $mol_object {
 		
 		@ $mol_mem_key
-		static now( precision?: number, reset?: null ) {
+		static now( precision: number, reset?: null ) {
 			
-			if( precision === undefined ) {
-				new $mol_after_work( 16, ()=> this.now( precision, null ) )
-			} else {
+			if( precision ) {
 				new $mol_after_timeout( precision, ()=> this.now( precision, null ) )
+			} else {
+				new $mol_after_frame( ()=> this.now( precision, null ) )
 			}
 		
 			return Date.now()
