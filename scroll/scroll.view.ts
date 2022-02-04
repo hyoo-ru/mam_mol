7ff4b3dd264af452becba92fa13f3d2652cbf8c5
jin <jin@hyoo.ru>
2022-02-04 14:33:09 +0300

    $mol_scroll: opt

--- scroll/scroll.view.ts
+++ scroll/scroll.view.ts
@@ -3,33 +3,27 @@ namespace $.$$ {
 	export class $mol_scroll extends $.$mol_scroll {
 
 		@ $mol_mem
-		scroll_top( next? : number ) {
-			return $mol_state_session.value( `${ this }.scroll_top()` , next ) || 0
+		scroll_pos( next? : [ number, number ] ) {
+			return $mol_state_session.value( `${ this }.scroll_pos()` , next ) || [ 0, 0 ]
 		}
 		
-		@ $mol_mem
-		scroll_left( next? : number ) {
-			return $mol_state_session.value( `${ this }.scroll_left()` , next ) || 0
+		scroll_top( next? : number ): number {
+			return this.scroll_pos( next === undefined ? undefined : [ this.scroll_left(), next ] )[1]
 		}
-
-		@ $mol_memo.method
-		_event_scroll_timer( next? : $mol_after_timeout | null ) {
-			return next
+		
+		scroll_left( next? : number ): number {
+			return this.scroll_pos( next === undefined ? undefined : [ next, this.scroll_top() ] )[0]
 		}
-
+		
 		event_scroll( next? : Event ) {
 			
-			this._event_scroll_timer()?.destructor()
-
 			const el = this.dom_node() as HTMLElement
 			
-			this._event_scroll_timer( new $mol_after_timeout( 200 , ()=> {
-
-				this.scroll_top( Math.max( 0 , el.scrollTop ) )
-				this.scroll_left( Math.max( 0 , el.scrollLeft ) )
-
-			} ) )
-
+			this.scroll_pos([
+				Math.max( 0 , el.scrollLeft ),
+				Math.max( 0 , el.scrollTop ),
+			])
+			
 		}
 
 		minimal_height() {
