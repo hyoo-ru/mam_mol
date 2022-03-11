ccb9a2cdc0f5d9fd76aa248c27c1e4de02d4b16d
jin <jin@hyoo.ru>
2022-03-11 17:40:37 +0300

    $mol_wire_dict: reactive dictionary

---
+++ wire/dict/dict.ts
@@ -0,0 +1,74 @@
+namespace $ {
+	export class $mol_wire_dict< Key, Value > extends Map< Key, Value > {
+
+		pub = new $mol_wire_pub
+		
+		// Accessors
+		
+		has( key: Key ) {
+			this.pub.promote()
+			return super.has( key )
+		}
+		
+		get( key: Key ) {
+			this.pub.promote()
+			return super.get( key )
+		}
+		
+		entries() {
+			this.pub.promote()
+			return super.entries()
+		}
+		
+		keys() {
+			this.pub.promote()
+			return super.keys()
+		}
+		
+		values() {
+			this.pub.promote()
+			return super.values()
+		}
+		
+		forEach(
+			task: ( value: Value, key: Key, dict: Map< Key, Value > ) => void,
+			self?: any
+		) {
+			this.pub.promote()
+			super.forEach( task, self )
+		}
+		
+		[Symbol.iterator]() {
+			this.pub.promote()
+			return super[Symbol.iterator]()
+		}
+		
+		get size() {
+			this.pub.promote()
+			return super.size	
+		}
+
+		// Mutators
+
+		set( key: Key, value: Value ) {
+			if( super.get( key ) === value ) return this
+			super.set( key, value )
+			this.pub.emit()
+			return this
+		}
+
+		delete( key: Key ) {
+			const res = super.delete( key )
+			if( res ) this.pub.emit()
+			return res
+		}
+		
+		clear() {
+			if( !super.size ) return
+			super.clear()
+			this.pub.emit()
+		}
+
+	}
+
+}
