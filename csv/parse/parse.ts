5aac019b4ad10d4aac2ee73ad027818c8d39d0f8
nin-jin <nin-jin@users.noreply.github.com>
2022-05-14 19:20:13 +0000

    Deployed using Blazing fast GitHub Pages deploy

---
+++ csv/parse/parse.ts
@@ -0,0 +1,27 @@
+namespace $ {
+	
+	export function $mol_csv_parse( text: string, delimiter = ';' ) {
+		
+		var lines = text.split( /\r?\n/g )
+		var header = lines.shift()!.split( delimiter )
+		
+		var res : Record< string, any >[] = []
+		
+		for( const line of lines ) {
+			
+			if( !line ) continue
+			
+			var row : { [ key : string ] : any } = {}
+			
+			for( const [ index, val ] of line.split( delimiter ).entries() ) {
+				row[ header[ index ] ] = val.replace( /^"|"$/g, '' ).replace( /""/g, '"' )
+			}
+			
+			res.push( row )
+			
+		}
+		
+		return res
+	}
+
+}
