395a538e296487dddacbe5fccf1a7a70e9019aa2
jin <jin@hyoo.ru>
2022-05-22 00:20:47 +0300

    $mol_dump: better dom view

--- dump/value/value.view.ts
+++ dump/value/value.view.ts
@@ -31,9 +31,21 @@ namespace $.$$ {
 			if( value instanceof RegExp ) return String( value )
 			if( value instanceof Date ) return value.toISOString()
 			
-			return Reflect.getOwnPropertyDescriptor( value, Symbol.toStringTag )?.value
+			const kind = Reflect.getOwnPropertyDescriptor( value, Symbol.toStringTag )?.value
 				?? Reflect.getPrototypeOf( value )?.constructor.name
 				?? 'Object'
+			
+			if( value instanceof Node ) {
+				try {
+					switch( value.nodeType ) {
+						case value.TEXT_NODE: return kind + ' ' + value.nodeValue?.trim()
+						case value.ELEMENT_NODE: return kind + ' ' + ( value as Element ).id
+						case value.DOCUMENT_NODE: return kind + ' ' + value.baseURI
+					}
+				} catch {}
+			}
+			
+			return kind
 		}
 		
 		@ $mol_mem
@@ -41,31 +53,41 @@ namespace $.$$ {
 			
 			let value = this.value()
 			
-			const self = [] as any[][]
+			const res = [] as any[][]
+			
+			if( value instanceof Map ) {
+				for( const [ key, val ] of value ) {
+					res.push([ key, '🡒', val ])
+				}
+			}
+			
+			if( value instanceof Set ) {
+				for( const val of value ) {
+					res.push([ val ])
+				}
+			}
+			
+			if( value instanceof Element ) {
+				try {
+					for( const kid of value.childNodes ) {
+						res.push([ kid ])
+					}
+					for( const attr of value.attributes ) {
+						res.push([ attr.nodeName, '=', attr.nodeValue ])
+					}
+				} catch {}
+			}
+				
 			for( const key of Reflect.ownKeys( value ) ) {
 				const prefix = String( key ) + '∶'
 				const descr = Reflect.getOwnPropertyDescriptor( value, key )!
-				if( 'value' in descr ) self.push([ prefix, descr.value ])
-				else self.push([ prefix, descr.get, descr.set ])
+				if( 'value' in descr ) res.push([ prefix, descr.value ])
+				else res.push([ prefix, descr.get, descr.set ])
 			}
 			
-			const map = value instanceof Map
-				? [ ... value ].map( ([ key, val ])=> [ key, '🡒', val ] )
-				: []
+			res.push([ '__proto__:', Reflect.getPrototypeOf( value ) ])
 			
-			const set = value instanceof Set
-				? [ ... value ].map( val => [ val ] )
-				: []
-				
-			const proto = Reflect.getPrototypeOf( value )
-			
-			return [
-				... self,
-				... map,
-				... set,
-				[ '__proto__:', proto ]
-			]
-			
+			return res
 		}
 		
 		@ $mol_mem
