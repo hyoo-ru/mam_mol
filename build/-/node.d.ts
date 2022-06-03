3406a32fdb161f5acd27b8025b1b9667d087c0fb
nin-jin <nin-jin@users.noreply.github.com>
2022-06-03 07:24:59 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- build/-/node.d.ts
+++ build/-/node.d.ts
@@ -1250,6 +1250,7 @@ declare namespace $ {
             _targets: Map<string, import("puppeteer").Target>;
             process: () => import("child_process").ChildProcess | null;
             _setIsPageTargetCallback: (isPageTargetCallback?: import("puppeteer").IsPageTargetCallback | undefined) => void;
+            _getIsPageTargetCallback: () => import("puppeteer").IsPageTargetCallback | undefined;
             createIncognitoBrowserContext: (options?: import("puppeteer").BrowserContextOptions | undefined) => import("puppeteer").BrowserContext;
             browserContexts: () => import("puppeteer").BrowserContext[];
             defaultBrowserContext: () => import("puppeteer").BrowserContext;
