74618de70a06dd06f88b6f4806d53bd83400ace0
jin <jin@hyoo.ru>
2022-06-03 15:44:36 +0300

    $node.colorete -> $mol_term_color

--- log3/log3.node.ts
+++ log3/log3.node.ts
@@ -4,7 +4,7 @@ namespace $ {
 		level : keyof Console ,
 		output : 'stdout' | 'stderr',
 		type : string ,
-		color: keyof typeof $node.colorette ,
+		color: ( str: string )=> string ,
 	) {
 
 		return function $mol_log3_logger(
@@ -15,11 +15,7 @@ namespace $ {
 			if( !event.time ) event = { time : new Date().toISOString() , ... event }
 
 			const tree = this.$mol_tree.fromJSON( event ).clone({ type })
-			let str = tree.toString()
-
-			if( process[output].isTTY ) {
-				str = $node.colorette[ color + 'Bright' ]( str )
-			}
+			let str = color( tree.toString() )
 
 			;( this.console[ level ] as any )( str )
 	
@@ -30,11 +26,11 @@ namespace $ {
 
 	}
 
-	$.$mol_log3_come = $mol_log3_node_make( 'info' , 'stdout' , 'come' , 'blue' )
-	$.$mol_log3_done = $mol_log3_node_make( 'info' , 'stdout' , 'done' , 'green' )
-	$.$mol_log3_fail = $mol_log3_node_make( 'error' , 'stderr' , 'fail' , 'red' )
-	$.$mol_log3_warn = $mol_log3_node_make( 'warn' , 'stderr' , 'warn' , 'yellow' )
-	$.$mol_log3_rise = $mol_log3_node_make( 'log' , 'stdout' , 'rise' , 'magenta' )
-	$.$mol_log3_area = $mol_log3_node_make( 'log' , 'stdout' , 'area' , 'cyan' )
+	$.$mol_log3_come = $mol_log3_node_make( 'info' , 'stdout' , 'come' , $mol_term_color.blue )
+	$.$mol_log3_done = $mol_log3_node_make( 'info' , 'stdout' , 'done' , $mol_term_color.green )
+	$.$mol_log3_fail = $mol_log3_node_make( 'error' , 'stderr' , 'fail' , $mol_term_color.red )
+	$.$mol_log3_warn = $mol_log3_node_make( 'warn' , 'stderr' , 'warn' , $mol_term_color.yellow )
+	$.$mol_log3_rise = $mol_log3_node_make( 'log' , 'stdout' , 'rise' , $mol_term_color.magenta )
+	$.$mol_log3_area = $mol_log3_node_make( 'log' , 'stdout' , 'area' , $mol_term_color.cyan )
 
 }
