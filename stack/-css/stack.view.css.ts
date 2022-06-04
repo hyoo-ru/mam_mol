b899b679e524cdf24d06aa14b0a3e10210db561c
nin-jin <nin-jin@users.noreply.github.com>
2022-06-04 16:10:49 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- stack/-css/stack.view.css.ts
+++ stack/-css/stack.view.css.ts
@@ -1,3 +1,3 @@
 namespace $ { $mol_style_attach( "mol/stack/stack.view.css",
- "[mol_stack] {\n\tdisplay: grid;\n}\n\n[mol_stack] > * {\n\tgrid-area: 1/1;\n}\n"
+ "[mol_stack] {\n\tdisplay: grid;\n\twidth: max-content;\n\theight: max-content;\n}\n\n[mol_stack] > * {\n\tgrid-area: 1/1;\n}\n"
 ) }
