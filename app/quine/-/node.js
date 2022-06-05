8dd964f9c8b55e67f434946dd16adf82ff067df5
nin-jin <nin-jin@users.noreply.github.com>
2022-06-05 12:47:16 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/quine/-/node.js
+++ app/quine/-/node.js
@@ -4728,7 +4728,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("mol/stack/stack.view.css", "[mol_stack] {\n\tdisplay: grid;\n\twidth: max-content;\n\theight: max-content;\n}\n\n[mol_stack] > * {\n\tgrid-area: 1/1;\n}\n");
+    $mol_style_attach("mol/stack/stack.view.css", "[mol_stack] {\n\tdisplay: grid;\n\t/* width: max-content; */\n\t/* height: max-content; */\n}\n\n[mol_stack] > * {\n\tgrid-area: 1/1;\n}\n");
 })($ || ($ = {}));
 //mol/stack/-css/stack.view.css.ts
 ;
--- stack/-css/stack.view.css.ts
+++ stack/-css/stack.view.css.ts
@@ -1,3 +1,3 @@
 namespace $ { $mol_style_attach( "mol/stack/stack.view.css",
- "[mol_stack] {\n\tdisplay: grid;\n\twidth: max-content;\n\theight: max-content;\n}\n\n[mol_stack] > * {\n\tgrid-area: 1/1;\n}\n"
+ "[mol_stack] {\n\tdisplay: grid;\n\t/* width: max-content; */\n\t/* height: max-content; */\n}\n\n[mol_stack] > * {\n\tgrid-area: 1/1;\n}\n"
 ) }
