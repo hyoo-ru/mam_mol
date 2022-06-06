42152fa47192bafd3141154b9f14676df402d776
jin <jin@hyoo.ru>
2022-06-06 20:23:47 +0300

    $mol_hint - unobtrusive hints

---
+++ hint/hint.view.css.ts
@@ -0,0 +1,17 @@
+namespace $.$$ {
+	
+	$mol_style_define( $mol_hint, {
+		
+		color: $mol_theme.shade,
+		
+		flex: {
+			shrink: 1,
+		},
+		
+		Icon: {
+			color: $mol_theme.control,
+		},
+		
+	} )
+
+}
