359887bfabfe1c9d7b446ee2c33ed3825ea9c696
jin <jin@hyoo.ru>
2022-06-06 16:15:56 +0300

    $mol_infinite: refactoring, $mol_scroll: paddings

--- infinite/demo/demo.view.ts
+++ infinite/demo/demo.view.ts
@@ -2,12 +2,21 @@ namespace $.$$ {
 	
 	export class $mol_infinite_demo extends $.$mol_infinite_demo {
 		
-		@ $mol_action
+		// @ $mol_mem_key
+		// before( anchor_id = 0 ) {
+		// 	this.$.$mol_wait_timeout( 10000 )
+		// 	return Array.from(
+		// 		{ length: this.chunk_size() },
+		// 		( _, index )=> anchor_id - index - 1,
+		// 	).reverse()
+		// }
+		
+		@ $mol_mem_key
 		after( anchor_id = 0 ) {
 			this.$.$mol_wait_timeout( 1000 )
 			return Array.from(
 				{ length: this.chunk_size() },
-				( _, index )=> anchor_id + index,
+				( _, index )=> anchor_id + index + 1,
 			)
 		}
 		
