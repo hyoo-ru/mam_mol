4052af78a6c162eb267e5431fcce006f861a4db8
jin <jin@hyoo.ru>
2022-06-15 21:46:58 +0300

    $mol_jsx: better guids

--- jsx/attach/attach.test.tsx
+++ jsx/attach/attach.test.tsx
@@ -5,11 +5,11 @@ namespace $ {
 		
 		'Attach to document'() {
 
-			const doc = $mol_dom_parse( '<html><body id="/foo"></body></html>' )
+			const doc = $mol_dom_parse( '<html><body id="foo"></body></html>' )
 
-			$mol_jsx_attach( doc , ()=> <body id="/foo">bar</body> )
+			$mol_jsx_attach( doc , ()=> <body id="foo">bar</body> )
 
-			$mol_assert_equal( doc.documentElement.outerHTML , '<html><body id="/foo">bar</body></html>' )
+			$mol_assert_equal( doc.documentElement.outerHTML , '<html><body id="foo">bar</body></html>' )
 			
 		} ,		
 		
