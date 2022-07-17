db4367c7a62154fdde59b25c2e2fbed0135e1fce
nin-jin <nin-jin@users.noreply.github.com>
2022-07-17 08:43:09 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/quine/-/node.js
+++ app/quine/-/node.js
@@ -4796,7 +4796,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("mol/stack/stack.view.css", "[mol_stack] {\n\tdisplay: grid;\n\t/* width: max-content; */\n\t/* height: max-content; */\n}\n\n[mol_stack] > * {\n\tgrid-area: 1/1;\n}\n");
+    $mol_style_attach("mol/stack/stack.view.css", "[mol_stack] {\n\tdisplay: grid;\n\t/* width: max-content; */\n\t/* height: max-content; */\n\talign-items: flex-start;\n\tjustify-items: flex-start;\n}\n\n[mol_stack] > * {\n\tgrid-area: 1/1;\n}\n");
 })($ || ($ = {}));
 //mol/stack/-css/stack.view.css.ts
 ;
--- int62/int62.ts
+++ int62/int62.ts
@@ -1,54 +1,56 @@
 namespace $ {
 	
+	export type $mol_int62_pair = {
+		readonly lo: number,
+		readonly hi: number,
+	}
+	
 	export const $mol_int62_max = ( 2 ** 30 ) - 1
 	export const $mol_int62_min = - ( 2 ** 30 )
 	export const $mol_int62_range = $mol_int62_max - $mol_int62_min + 1
 	
-	export function $mol_int62_dump(
-		hi: number,
-		lo: number,
-	) {
-		hi = ( hi + $mol_int62_range ) % $mol_int62_range
+	export function $mol_int62_dump( { lo, hi }: $mol_int62_pair ) {
 		lo = ( lo + $mol_int62_range ) % $mol_int62_range
-		return `${ hi.toString(36) }_${ lo.toString(36) }` as const
+		hi = ( hi + $mol_int62_range ) % $mol_int62_range
+		return `${ lo.toString(36) }_${ hi.toString(36) }` as const
 	}
 
 	export function $mol_int62_compare(
-		left_hi: number,
 		left_lo: number,
-		right_hi: number,
+		left_hi: number,
 		right_lo: number,
+		right_hi: number,
 	) {
 		return ( right_hi - left_hi ) || ( right_lo - left_lo )
 	}
 
 	export function $mol_int62_inc(
-		hi: number,
 		lo: number,
+		hi: number,
 		max = $mol_int62_max,
-	) {
+	): $mol_int62_pair {
 		if( lo === max ) {
-			return [ hi + 1, -max ] as const
+			return { lo: -max, hi: hi + 1 }
 		} else {
-			return [ hi, lo + 1 ] as const
+			return { lo: lo + 1, hi }
 		}
 	}
 
-	export function $mol_int62_random() {
-		return [
-			Math.floor( Math.random() * $mol_int62_range + $mol_int62_min ),
-			Math.floor( Math.random() * $mol_int62_range + $mol_int62_min ),
-		] as const
+	export function $mol_int62_random(): $mol_int62_pair {
+		return {
+			lo: Math.floor( Math.random() * $mol_int62_range + $mol_int62_min ),
+			hi: Math.floor( Math.random() * $mol_int62_range + $mol_int62_min ),
+		}
 	}
 
-	export function $mol_int62_hash_string( str: string, seed_hi = 0, seed_lo = 0 ) {
-		return $mol_int62_hash_buffer( $mol_charset_encode( str ), seed_hi, seed_lo )
+	export function $mol_int62_hash_string( str: string, seed_lo = 0, seed_hi = 0 ) {
+		return $mol_int62_hash_buffer( $mol_charset_encode( str ), seed_lo, seed_hi )
 	}
 	
-	export function $mol_int62_hash_buffer( buf: Uint8Array, seed_hi = 0, seed_lo = 0 ) {
+	export function $mol_int62_hash_buffer( buf: Uint8Array, seed_lo = 0, seed_hi = 0 ): $mol_int62_pair {
 		
-		let h1 = 0xdeadbeef ^ seed_hi
-		let h2 = 0x41c6ce57 ^ seed_lo
+		let h1 = 0xdeadbeef ^ seed_lo
+		let h2 = 0x41c6ce57 ^ seed_hi
 		
 		for( const byte of buf ) {
 			h1 = Math.imul( h1 ^ byte, 2654435761 )
@@ -58,7 +60,7 @@ namespace $ {
 		h1 = Math.imul( h1 ^ ( h1 >>> 16 ), 2246822507 ) ^ Math.imul( h2 ^ ( h2 >>> 13 ), 3266489909 )
 		h2 = Math.imul( h2 ^ ( h2 >>> 16 ), 2246822507 ) ^ Math.imul( h1 ^ ( h1 >>> 13 ), 3266489909 )
 		
-		return [ h1 << 1 >> 1, h2 << 1 >> 1 ] as const
+		return { lo: h1 << 1 >> 1, hi: h2 << 1 >> 1 }
 	}
 	
 }
