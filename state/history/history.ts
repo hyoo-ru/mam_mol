a6d5ba7f20dd295a287a5bc9ff14acf64ad956af
jin <jin@hyoo.ru>
2022-08-24 14:23:38 +0300

    $mol_state_history: simplified

--- state/history/history.ts
+++ state/history/history.ts
@@ -2,27 +2,33 @@ namespace $ {
 	
 	export class $mol_state_history< Value > extends $mol_object {
 		
+		@ $mol_mem
+		static data( next?: {} | null ) {
+			
+			const prev = history.state || {}
+			if( !next ) return prev
+			
+			history.replaceState( next, $mol_dom_context.document.title, $mol_dom_context.document.location.href )
+			
+			return next
+		}
+		
 		@ $mol_mem_key
 		static value< Value >( key : string , next? : Value ) {
-			return $mol_state_session.value( `$mol_state_history:id(${this.id()}):${key}` , next )
+			
+			const prev = this.data()
+			if( next === undefined ) return prev[ key ] ?? null
+			
+			const state = { ... prev, [ key ]: next }
+			this.data( state )
+			
+			return state[ key ] ?? null
 		}
 		
 		prefix() { return '' }
 		
 		value( key : string , next? : Value ) {
 			return $mol_state_local.value( this.prefix() + '.' + key , next )
-		}
-		
-		@ $mol_mem
-		static id( next? : string ) {
-			try {
-				if( history.state ) return history.state as string
-			} catch( error: any ) {
-				// IE11
-			}
-			const id = Date.now().toString( 16 )
-			history.replaceState( id , $mol_dom_context.document.title , $mol_dom_context.document.location.href )
-			return id
 		}
 		
 	}
