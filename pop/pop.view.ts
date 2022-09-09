5cf15d191231cb09c296ca8f6cffd3d734b01587
jin <jin@hyoo.ru>
2022-09-09 15:06:30 +0300

    $mol_pop: track offset parent instead of window to align

--- pop/pop.view.ts
+++ pop/pop.view.ts
@@ -35,22 +35,34 @@ namespace $.$$ {
 
 		@ $mol_mem
 		align_vert() {
-			const viewport = this.$.$mol_window.size()
+			const viewport = this.view_port()
 			
 			const rect_bubble = this.view_rect()
 			if( !rect_bubble ) return 'suspense'
 			
-			return rect_bubble.top > ( viewport.height - rect_bubble.bottom ) ? 'top' : 'bottom'
+			return rect_bubble.top > ( viewport.top + viewport.height - rect_bubble.bottom ) ? 'top' : 'bottom'
 		}
 
 		@ $mol_mem
 		align_hor() {
-			const viewport = this.$.$mol_window.size()
+			const viewport = this.view_port()
 			
 			const rect_bubble = this.view_rect()
 			if( !rect_bubble ) return 'suspense'
 			
-			return rect_bubble.left > ( viewport.width - rect_bubble.right ) ? 'left' : 'right'
+			return rect_bubble.left > ( viewport.left + viewport.width - rect_bubble.right ) ? 'left' : 'right'
+		}
+		
+		@ $mol_mem
+		View_port() {
+			const view = new $mol_view
+			view.dom_node( ( this.dom_node() as HTMLElement ).offsetParent! )
+			return view
+		}
+		
+		@ $mol_mem
+		view_port() {
+			return this.View_port().view_rect() ?? { ... this.$.$mol_window.size(), left: 0, top: 0 }
 		}
 
 	}
