5f8318b3d1816f05158e47d76572f592589eb043
nin-jin <nin-jin@users.noreply.github.com>
2022-10-29 10:54:39 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- blob/blob.ts
+++ blob/blob.ts
@@ -1,4 +1,4 @@
 namespace $ {
 	export type $mol_blob = Blob
-	export let $mol_blob = $mol_dom_context.Blob ?? $node.buffer.Blob
+	export let $mol_blob = $node.buffer?.Blob ?? $mol_dom_context.Blob
 }
