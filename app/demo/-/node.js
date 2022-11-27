939409d61670ef2694cb7df28a14f95eaa067d65
nin-jin <nin-jin@users.noreply.github.com>
2022-11-27 08:41:53 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/demo/-/node.js
+++ app/demo/-/node.js
@@ -30446,7 +30446,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("mol/string/button/button.view.css", "[mol_string_button]:not(:placeholder-shown):not(:focus):not(:hover) {\n\tcolor: var(--mol_theme_control);\n\tbackground: transparent;\n\tbox-shadow: none;\n}\n");
+    $mol_style_attach("mol/string/button/button.view.css", "[mol_string_button]:not(:placeholder-shown):not(:focus):not(:hover):not(:disabled) {\n\tcolor: var(--mol_theme_control);\n\tbackground: transparent;\n\tbox-shadow: none;\n}\n");
 })($ || ($ = {}));
 //mol/string/button/-css/button.view.css.ts
 ;
