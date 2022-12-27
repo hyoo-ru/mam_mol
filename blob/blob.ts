70782010216805d5349286de0db03b4b48d15115
Pavel Zubkov <zubkov_p@mail.ru>
2022-12-27 19:28:03 +0300

    $mol_blob: Separate $mol_blob_text and $mol_blob_json onto files

--- blob/blob.ts
+++ blob/blob.ts
@@ -1,18 +1,7 @@
 namespace $ {
+
 	export type $mol_blob = Blob
+
 	export let $mol_blob = ( $node.buffer?.Blob ?? $mol_dom_context.Blob ) as any as typeof Blob
 
-	export function $mol_blob_text( blob: $mol_blob ) {
-		return new Promise< string >( ( done, fail )=> {
-			const reader = new FileReader
-			reader.onerror = fail
-			reader.onload = event => done( event.target!.result as string )
-			reader.readAsText( blob )
-		} )
-	}
-
-	export async function $mol_blob_json( blob: $mol_blob ) {
-		const json = await $mol_blob_text( blob )
-		return JSON.parse(json)
-	}
 }
