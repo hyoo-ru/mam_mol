4528f53c09bdf4e798fbde5ee0d9de6cb4ce44e3
nin-jin <nin-jin@users.noreply.github.com>
2023-01-23 16:09:34 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- mutable/mutable.ts
+++ mutable/mutable.ts
@@ -1,37 +1,36 @@
 namespace $ {
 	
 	export type $mol_mutable_wrapper< Value > = {
-		( patch?: ( next: $mol_type_immutable_deep< Value > )=> $mol_type_immutable_deep< Value > ): $mol_type_immutable_deep< Value >
+		(
+			patch?: ( next: $mol_type_immutable_deep< Value > )=> $mol_type_immutable_deep< Value >
+		): $mol_type_immutable_deep< Value >
 	} & {
 		[ Field in keyof Value ]: $mol_mutable_wrapper< Value[ Field ] >
 	}
 	
 	export function $mol_mutable< Value >(
-		value: Value,
-		update: ( next: Value )=> Value = next => value = next,
+		input: Value,
+		update: ( next: Value )=> Value = next => input = next,
 	): $mol_mutable_wrapper< Value > {
+		
+		let output = undefined as undefined | Value
+		
+		const clone = Array.isArray( input )
+			? ()=> [ ... input as any ] as Value
+			: ()=> ({ ... input })
 		
 		return new Proxy( $mol_mutable, {
 			
-			get: ( mutable, field )=> mutable(
-				value[ field ],
-				Array.isArray( value )
-					? next => update( value = [
-						... ( value as any[] ).slice( 0, Number( field ) ),
-						next,
-						... ( value as any[] ).slice( Number( field ) + 1 ),
-					] as Value )[ field ]
-					: next => update( value = { ... value, [ field ]: next } )[ field ],
+			get: ( Mut, field )=> Mut(
+				input[ field ],
+				next => ( output ?? ( output = update( clone() ) ) )[ field ] = next,
 			),
 			
-			set: ( mutable, field, next )=> {
-				update( value = next )
-				return true
-			},
+			set: ()=> false,
 			
-			apply: ( mutable, self, [ patch ] )=> {
-				if( patch ) update( value = patch( value ) )
-				return value
+			apply: ( Mut, self, [ patch ] )=> {
+				if( patch ) update( output = input = patch( input ) )
+				return output ?? input
 			},
 			
 		} ) as any
