858bd6d3934ef0322c8994615ea9182247f75fa6
jin <jin@hyoo.ru>
2023-01-30 22:57:37 +0300

    $mol_canvas: native resolution

--- canvas/canvas.view.ts
+++ canvas/canvas.view.ts
@@ -7,6 +7,16 @@ namespace $.$$ {
 			return ( this.dom_node() as HTMLCanvasElement ).getContext( '2d' )!
 		}
 
+		@ $mol_mem
+		width() {
+			return Math.ceil( ( this.view_rect()?.width ?? 0 ) * this.$.$mol_dom_context.devicePixelRatio )
+		}
+		
+		@ $mol_mem
+		height() {
+			return Math.ceil( ( this.view_rect()?.height ?? 0 ) * this.$.$mol_dom_context.devicePixelRatio )
+		}
+		
 		render() {
 			super.render()
 			this.paint()
