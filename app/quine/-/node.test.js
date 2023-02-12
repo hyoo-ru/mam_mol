8b8f65cfb9da11896fa971bb41889632490a6a20
nin-jin <nin-jin@users.noreply.github.com>
2023-02-12 12:50:09 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/quine/-/node.test.js
+++ app/quine/-/node.test.js
@@ -7146,7 +7146,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("mol/embed/youtube/youtube.view.css", "[mol_embed_youtube] {\n\tpadding: 0;\n}\n\n[mol_embed_youtube_image]:not(:hover):not(:focus) {\n\topacity: .75;\n}\n");
+    $mol_style_attach("mol/embed/youtube/youtube.view.css", "[mol_embed_youtube] {\n\tpadding: 0;\n\tmax-width: 100%;\n}\n\n[mol_embed_youtube_image]:not(:hover):not(:focus) {\n\topacity: .75;\n}\n");
 })($ || ($ = {}));
 //mol/embed/youtube/-css/youtube.view.css.ts
 ;
