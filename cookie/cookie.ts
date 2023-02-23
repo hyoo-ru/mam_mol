c6c94244c0c6ee71b732ee9691940f131b63dce4
Ilya Grigorev <guardiner@mail.ru>
2023-02-24 01:43:29 +0700

    Rewrite realization of $mol_cookie.all()

--- cookie/cookie.ts
+++ cookie/cookie.ts
@@ -3,18 +3,12 @@ namespace $ {
 	export class $mol_cookie {
 
 		all() : { [key: string]: string} {
-			return Object.fromEntries(
-					$mol_dom_context.document.cookie.split(/; ?/g).map(
-						s => [ 
-								s.substring( 0,  s.indexOf('=') ),
-								s.substring( 1 + s.indexOf('=') ) 
-							]
-					)
-				);
+			return Object.fromEntries( 
+				$mol_dom_context.document.cookie.split(/; ?/g).map( s => ( s.match( /^(.*?)=(.*)$/ ) ?? [] ).slice(1) ) );
 		}
 
 		set( cookies: { [key: string]: string} ): string {
-			return $mol_dom_context.document.cookie = Object.keys( cookies ).map( key => key + '=' + cookies[key] ).join( '; ' );
+			return $mol_dom_context.document.cookie = Object.keys( cookies ).map( key => key + '=' + cookies[key] ).join('; ');
 		}
 		
 	}
