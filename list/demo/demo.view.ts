d452c09973c82b5e97bab3fcfdbb090d9d0d3c4d
Vitaliy V. Makeev <w.makeev@gmail.com>
2023-02-25 19:45:07 +0500

    $mol_list: add demo

--- list/demo/demo.view.ts
+++ list/demo/demo.view.ts
@@ -1,15 +1,21 @@
 namespace $.$$ {
 
 	export class $mol_list_demo extends $.$mol_list_demo {
+		
 		override item_title( id : number ) : string {
 			return `Item #${ id + 1 }`
 		}
 
-		override list_rows() {
-			return [ ...Array( this.rows() ).keys() ].map( key => {
-				return this.Item( key )
-			} )
+		override list_items() {
+			const rows = []
+
+			for ( let key = 0 ; key < this.items_сount() ; key++ ) {
+				rows.push( this.Item( key ) )
+			}
+			
+			return rows
 		}
+		
 	}
 
 }
