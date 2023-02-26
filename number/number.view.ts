f033e16bc31847a577558ebc6169f3947976ff88
Vitaliy V. Makeev <w.makeev@gmail.com>
2023-02-26 11:51:00 +0500

    $mol_number: refactoring

--- number/number.view.ts
+++ number/number.view.ts
@@ -1,51 +1,42 @@
 namespace $.$$ {
 	export class $mol_number extends $.$mol_number {
+		
+		value_limited( next? : any ) : number {
+			if ( next === undefined ) return this.value()
+			if ( next === '' ) return this.value( null )
 
-		value_in_range( val : number ) : number {			
 			const min = this.value_min()
 			const max = this.value_max()
 
-			if( val < min ) return min
-			if( val > max ) return max
-			
-			return val
-		}
+			const val = Number( next )
 
-		value_change( step : number ) {
-			this.value(
-				this.value_in_range(
-					( Number( this.value() ) || 0 ) + step 
-				)
-			)
+			if( val < min ) return this.value( min )
+			if( val > max ) return this.value( max )
+			
+			return this.value( val )
 		}
 
 	 	override event_dec( next? : Event ) {
-			this.value_change( -this.precision_change() )
+			this.value_limited( ( this.value_limited() || 0 ) - this.precision_change() )
 		}
 
 		override event_inc( next? : Event ) {
-			this.value_change( this.precision_change() )
+			this.value_limited( ( this.value_limited() || 0 ) + this.precision_change() )
 		}
 		
 		override value_string( next? : string ) {
-			const next_num = this.value_in_range( Number( next ) )
-
-			// Value changed manualy in the field
-			if( next !== void 0 ) {
-				this.value( next === '' ? null : next_num )
-			}
+			const next_num = this.value_limited( next )
 
 			const precisionView = this.precision_view()
-			const value = next ? next_num : this.value()
 
-			if( value === 0 ) return '0'
-			if( !value ) return ''
+			if( next_num === 0 ) return '0'
+			if( !next_num ) return ''
 
 			if( precisionView >= 1 ) {
-				return ( value / precisionView ).toFixed()
+				return ( next_num / precisionView ).toFixed()
 			} else {
 				const fixedNumber = Math.log10( 1 / precisionView )
-				return value.toFixed( Math.ceil( fixedNumber ) )
+				return next_num.toFixed( Math.ceil( fixedNumber ) )
 			}
 		}
 		
