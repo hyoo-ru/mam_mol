9b403dd75cfb0127afaaea53043fb331110b8acd
stan-donarise <111230475+stan-donarise@users.noreply.github.com>
2023-03-09 13:44:36 +1100

    $mol_date: fix value bindings

--- date/date.view.ts
+++ date/date.view.ts
@@ -2,7 +2,7 @@ namespace $.$$ {
 	export class $mol_date extends $.$mol_date {
 
 		trigger_content() {
-			return [ this.value_moment()?.toString( 'YYYY-MM-DD hh:mm' ) ?? this.Icon() ]
+			return [ this.value_generic_source() || this.Icon() ]
 		}
 
 		input_mask( val : string ) {
@@ -18,24 +18,43 @@ namespace $.$$ {
 		}
 
 		@ $mol_mem
-		value( val? : string ) {
+		value_generic_source( next? : string ): string {
 
+			const old_value = $mol_wire_probe( () => this.value_generic_source() )
+
+			const value = this.value()
 			const moment = this.value_moment()
+			const value_number = this.value_number()
 
-			if( val === undefined ) return moment?.toString( 'YYYY-MM-DD hh:mm' ) ?? ''
+			if( next === undefined ) {
+				if( old_value !== value ) {
+					const moment_try = $mol_try( ()=> new $mol_time_moment( value ) )
+					const moment = moment_try instanceof Error? new $mol_time_moment() : moment_try
+					this.value_moment( moment )
+					this.value_number( moment.valueOf() )
+					return value
+				}
+				const moment_str = moment.toString( 'YYYY-MM-DD hh:mm' )
+				if( old_value !== moment_str ) {
+					this.value( moment_str )
+					this.value_number( moment.valueOf() )
+					return moment_str
+				} else {
+					const moment_try = $mol_try( ()=> new $mol_time_moment( value ) )
+					const moment = moment_try instanceof Error? new $mol_time_moment() : moment_try
+					const moment_str = moment.toString( 'YYYY-MM-DD hh:mm' )
+					this.value( moment_str )
+					this.value_moment( moment )
+					return moment_str
+				}
+			}
 
-			const moment2 = $mol_try( ()=> val && new $mol_time_moment( val ) ) || null
-			if( moment2 instanceof Error ) return val
-
-			this.value_moment( moment2! )
-
-			return val
-		}
-
-		@ $mol_mem
-		value_moment( val? : $mol_time_moment ) {
-			const stamp = this.value_number( val && val.valueOf() )
-			return isNaN( stamp ) ? null! : new $mol_time_moment( stamp )
+			const moment_try = $mol_try( ()=> new $mol_time_moment( next ) )
+			const next_moment = moment_try instanceof Error? new $mol_time_moment() : moment_try
+			this.value( next )
+			this.value_moment( next_moment )
+			this.value_number( next_moment.valueOf() )
+			return next
 		}
 
 		@ $mol_mem
