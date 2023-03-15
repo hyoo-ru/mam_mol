9304854188929edb6af7c18b6f3fca46cf660372
jin <jin@hyoo.ru>
2023-03-15 23:50:27 +0300

    $mol_frame: no warnings

--- frame/frame.view.ts
+++ frame/frame.view.ts
@@ -14,9 +14,9 @@ namespace $.$$ {
 				... this.encription() ? [ 'encrypted-media' ] : [] ,
 				... this.gyroscope() ? [ 'gyroscope' ] : [] ,
 				... this.pip() ? [ 'picture-in-picture' ] : [] ,
-				... this.clipboard_read() ? [ `clipboard-read self ${ this.uri() }` ] : [],
-				... this.clipboard_write() ? [ `clipboard-write self ${ this.uri() }` ] : [],
-			].join(';')
+				... this.clipboard_read() ? [ `clipboard-read ${ this.uri() }` ] : [],
+				... this.clipboard_write() ? [ `clipboard-write ${ this.uri() }` ] : [],
+			].join('; ')
 		}
 		
 	}
