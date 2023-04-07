a87499023217e043010abf0304dd7e34ff8faa6b
nin-jin <nin-jin@users.noreply.github.com>
2023-04-07 19:43:05 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/docs/-/node.js
+++ app/docs/-/node.js
@@ -9824,7 +9824,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("mol/image/image.view.css", "[mol_image] {\n\tborder-radius: var(--mol_gap_round);\n\toverflow: hidden;\n\tflex: 0 1 auto;\n\tmax-width: 100%;\n\tobject-fit: cover;\n}\n");
+    $mol_style_attach("mol/image/image.view.css", "[mol_image] {\n\tborder-radius: var(--mol_gap_round);\n\toverflow: hidden;\n\tflex: 0 1 auto;\n\tmax-width: 100%;\n\tobject-fit: cover;\n\theight: fit-content;\n}\n");
 })($ || ($ = {}));
 //mol/image/-css/image.view.css.ts
 ;
