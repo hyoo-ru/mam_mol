74a217bd03dbf35311fe29124727ef06963bf9ec
jin <jin@hyoo.ru>
2023-04-14 20:07:11 +0300

    fixed deo

--- fetch/demo/demo.view.ts
+++ fetch/demo/demo.view.ts
@@ -1,15 +1,9 @@
 namespace $.$$ {
 	export class $mol_fetch_demo extends $.$mol_fetch_demo {
 		
-		@ $mol_mem
 		fetch_data(){
-			const data = $mol_fetch.json(this.url())
-			this.data(JSON.stringify(data, null, 2))
+			this.data( $mol_fetch.json( this.url() ) )
 		}
-
-		@ $mol_mem
-		data(next?: any) {
-			return next ?? ''
-		}
+		
 	}
 }
