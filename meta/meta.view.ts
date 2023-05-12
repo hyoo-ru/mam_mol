a6bc8dd0918f788833f67ec2a517efde8cdd1db3
koplenov <koplenov@gmail.com>
2023-05-13 01:39:02 +0300

    $mol_meta: completely rename

--- meta/meta.view.ts
+++ meta/meta.view.ts
@@ -1,6 +1,6 @@
 namespace $.$$ {
 
-	export class $mol_seo_canonical extends $.$mol_seo_canonical {
+	export class $mol_meta_canonical extends $.$mol_meta_canonical {
 		@$mol_mem
 		content() {
 			// self-referencing by default
