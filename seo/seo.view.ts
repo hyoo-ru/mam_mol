1c3ea6c74dba89dc017718f756dbcbce1c007785
koplenov <koplenov@gmail.com>
2023-05-13 00:43:32 +0300

    $mol_seo_canonical: set default behavior

---
+++ seo/seo.view.ts
@@ -0,0 +1,11 @@
+namespace $.$$ {
+
+	export class $mol_seo_canonical extends $.$mol_seo_canonical {
+		@$mol_mem
+		content() {
+			// self-referencing by default
+			return $mol_state_arg.href()
+		}
+	}
+
+}
