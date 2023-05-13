958a9f28335766600978964dd3360a08669bc862
jin <jin@hyoo.ru>
2023-05-13 13:39:49 +0300

    $mol_wire_probe: default val

--- wire/probe/probe.ts
+++ wire/probe/probe.ts
@@ -2,14 +2,16 @@ namespace $ {
 	
 	/** Run code without state changes */
 	export function $mol_wire_probe< Value >(
-		task : ()=> Value, next?: Value
+		task : ()=> Value, def?: Value
 	) : Value | undefined {
 		
 		const warm = $mol_wire_fiber.warm
 		
 		try {
 			$mol_wire_fiber.warm = false
-			return task()
+			const res = task()
+			if( res === undefined ) return def
+			return res
 		} finally {
 			$mol_wire_fiber.warm = warm
 		}
