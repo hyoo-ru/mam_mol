3a40a9cda08271856c87ca5e328b43496c87f0be
jin <jin@hyoo.ru>
2023-06-01 21:56:31 +0300

    $mol_video_camera: better camera settings

--- video/camera/camera.view.ts
+++ video/camera/camera.view.ts
@@ -7,18 +7,29 @@ namespace $.$$ {
 		}
 		
 		@ $mol_mem
-		stream() {
+		stream_raw() {
 			
 			const stream = $mol_wire_sync( navigator.mediaDevices ).getUserMedia({
-				video: {
-					facingMode: this.facing(),
-				},
+				video: this.video_constraints(),
 			})
 			
 			return Object.assign( stream, {
 				destructor: ()=> stream.getTracks().forEach( track => track.stop() )
 			} )
 			
+		}
+		
+		@ $mol_mem
+		stream() {
+			
+			const settings = this.video_settings()
+			const stream = this.stream_raw()
+			
+			for( const track of stream.getVideoTracks() ) {
+				track.applyConstraints( settings )
+			}
+			
+			return stream
 		}
 		
 	}
