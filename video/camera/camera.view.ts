00e7a204904ac06d670d1b7ba3587fab25c923c2
jin <jin@hyoo.ru>
2023-06-01 22:10:03 +0300

    $mol_video_camera: simplify

--- video/camera/camera.view.ts
+++ video/camera/camera.view.ts
@@ -2,11 +2,6 @@ namespace $.$$ {
 	export class $mol_video_camera extends $.$mol_video_camera {
 		
 		@ $mol_mem
-		facing( next = 'user' as 'user' | 'environment' ) {
-			return next
-		}
-		
-		@ $mol_mem
 		stream_raw() {
 			
 			const stream = $mol_wire_sync( navigator.mediaDevices ).getUserMedia({
