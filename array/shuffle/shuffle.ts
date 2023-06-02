7dc4d10b531dd1a36c3e5f9e08913af65dd62ddb
nin-jin <nin-jin@users.noreply.github.com>
2023-06-02 09:55:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

---
+++ array/shuffle/shuffle.ts
@@ -0,0 +1,20 @@
+namespace $ {
+
+	export function $mol_array_shuffle<Item>(
+		array: Item[],
+	) {
+
+		const res = array.slice()
+
+		for( let index = res.length - 1; index > 0; index-- ) {
+			const index_swap = Math.floor( Math.random() * ( index + 1 ) )
+			const temp = res[ index ]
+			res[ index ] = res[ index_swap ]
+			res[ index_swap ] = temp
+		}
+
+		return res
+
+	}
+
+}
