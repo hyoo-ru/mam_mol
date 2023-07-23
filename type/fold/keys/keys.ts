09d7e9875f55d997215593a167bf85ae90050f59
spleekz <spleekz@zohomail.com>
2023-07-23 10:21:14 +0300

    $mol_type_fold_keys: remove check for Function

--- type/fold/keys/keys.ts
+++ type/fold/keys/keys.ts
@@ -8,25 +8,22 @@ namespace $ {
 	export type $mol_type_fold_keys< T > =
 		T extends object
 		?
-			T extends Function
+			T extends Array< any >
 			? ''
 			:
-				T extends Array< any >
-				? ''
-				:
-					{
-						[ Key in keyof T ]-?:
-							Key extends string
-							?
-								| Key
-								| $mol_type_case_dot<
-									Key,
-									$mol_type_fold_keys<
-										Required< T >[ Key ]
-									>
-								 >
-							: never
-					}[ keyof T ]
+				{
+					[ Key in keyof T ]-?:
+						Key extends string
+						?
+							| Key
+							| $mol_type_case_dot<
+								Key,
+								$mol_type_fold_keys<
+									Required< T >[ Key ]
+								>
+							 >
+						: never
+				}[ keyof T ]
 		: ''
 
 	/**
