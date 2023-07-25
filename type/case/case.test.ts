5d9488b9c154932ebb76bdb387d419d852fb0212
spleekz <spleekz@zohomail.com>
2023-07-25 22:03:10 +0300

    $mol_type_case_kebab: join strings in kebab case

--- type/case/case.test.ts
+++ type/case/case.test.ts
@@ -9,4 +9,13 @@ namespace $ {
 
 	>
 
+	type kebab1 = $mol_type_assert<
+
+		$mol_type_case_kebab<
+			[ 'foo', 'bar', 'wee' ]
+		>,
+		'foo-bar-wee'
+
+	>
+
 }
--- type/case/case.ts
+++ type/case/case.ts
@@ -9,4 +9,13 @@ namespace $ {
 	export type $mol_type_case_snake< Parts extends Array< string > > =
 		$mol_type_string_join< Parts, '_' >
 
+	/**
+	 * Join strings in kebab-case.
+	 *
+	 * 	// 'foo-bar-wee'
+	 * 	$mol_type_case_kebab< [ 'foo', 'bar', 'wee' ] >
+	 */
+	export type $mol_type_case_kebab< Parts extends Array< string > > =
+		$mol_type_string_join< Parts, '-' >
+
 }
