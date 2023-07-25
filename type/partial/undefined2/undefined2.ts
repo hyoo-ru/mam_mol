833f28fefb6feb4f95cc721dc710f1abf73bc134
spleekz <spleekz@zohomail.com>
2023-07-25 12:50:36 +0300

    $mol_type_partial_undefined2: merge result intersection

--- type/partial/undefined2/undefined2.ts
+++ type/partial/undefined2/undefined2.ts
@@ -7,12 +7,14 @@ namespace $ {
 	 * 	type optional = $mol_type_partial_undefined2< { a: number | undefined; b: string } >
 	 */
 	export type $mol_type_partial_undefined2< T > =
-		$mol_type_override<
-			T,
-			{
-				[ K in $mol_type_keys_undefined< T > ]?:
-					T[ K ]
-			}
+		$mol_type_merge<
+			$mol_type_override<
+				T,
+				{
+					[ K in $mol_type_keys_undefined< T > ]?:
+						T[ K ]
+				}
+			>
 		>
 
 }
