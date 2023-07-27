0d07d337808c2a662b693d2d42f116f2841b7b18
spleekz <spleekz@zohomail.com>
2023-07-27 21:01:10 +0300

    $mol_type_volume_value: semantic type names

--- type/volume/value/value.ts
+++ type/volume/value/value.ts
@@ -6,13 +6,13 @@ namespace $ {
 	 * 	// number
 	 * 	type abc_type = $mol_type_volume_value< { a: { b: { c: number }; d: string }, 'a.b.c' } >
 	 */
-	export type $mol_type_volume_value< T, K extends string | number > =
-		$mol_type_access< T, K > extends never
-		? K extends `${ infer Left }.${ infer Right }`
-			? $mol_type_access< T, Left > extends never
+	export type $mol_type_volume_value< Type, Key extends string > =
+		$mol_type_access< Type, Key > extends never
+		? Key extends `${ infer Left }.${ infer Right }`
+			? $mol_type_access< Type, Left > extends never
 				? never
-				: $mol_type_volume_value< $mol_type_access< T, Left >, Right >
+				: $mol_type_volume_value< $mol_type_access< Type, Left >, Right >
 			: never
-		: $mol_type_access< T, K >
+		: $mol_type_access< Type, Key >
 
 }
