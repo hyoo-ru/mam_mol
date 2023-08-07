8ae3bbe25da6358544c9ee8a9fc1296d035d2af6
spleekz <spleekz@zohomail.com>
2023-08-07 19:49:06 +0300

    $mol_layer: use $mol_style_prop

--- layer/layer.ts
+++ layer/layer.ts
@@ -1,14 +1,18 @@
 namespace $ {
-	const { vary } = $mol_style_func
+
 	/**
 	 * Z-index values for layers
 	 * https://page.hyoo.ru/#!=xthcpx_wqmiba
 	 */
-	export let $mol_layer = {
-		hover: vary( '--mol_layer_hover' ),
-		focus: vary( '--mol_layer_focus' ),
-		speck: vary( '--mol_layer_speck' ),
-		float: vary( '--mol_layer_float' ),
-		popup: vary( '--mol_layer_popup' ),
-	} as const
+	export let $mol_layer = $mol_style_prop(
+		'mol_layer',
+		[
+			'hover',
+			'focus',
+			'speck',
+			'float',
+			'popup',
+		]
+	)
+
 }
