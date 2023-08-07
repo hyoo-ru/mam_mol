bc3fadfd304d52f194564474772379719ab0e75f
spleekz <spleekz@zohomail.com>
2023-08-07 19:45:56 +0300

    $mol_gap: use $mol_style_prop

--- gap/gap.ts
+++ gap/gap.ts
@@ -1,14 +1,18 @@
 namespace $ {
-	const { vary } = $mol_style_func
+
 	/**
 	 * Gap in CSS
 	 * @see https://page.hyoo.ru/#!=msdb74_bm7nsq
 	 */
-	export let $mol_gap = {
-		block: vary( '--mol_gap_block' ) ,
-		text: vary( '--mol_gap_text' ),
-		round: vary( '--mol_gap_round' ),
-		space: vary( '--mol_gap_space' ),
-		blur: vary( '--mol_gap_blur' ),
-	} as const
+	export let $mol_gap = $mol_style_prop(
+		'mol_gap',
+		[
+			'block',
+			'text',
+			'round',
+			'space',
+			'blur',
+		]
+	)
+
 }
