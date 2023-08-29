841a0f157ac52ddfae3efc5a7e807c65c204ac83
Stefan Zerkalica <zerkalica@gmail.com>
2023-08-29 16:52:43 +0300

    $mol_jwt_decode, added test

---
+++ jwt/jwt.test.ts
@@ -0,0 +1,13 @@
+namespace $ {
+	const token= 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+
+	$mol_test({
+		'jwt decode'() {
+			$mol_assert_like($mol_jwt_decode(token), {
+				"sub": "1234567890",
+				"name": "John Doe",
+				"iat": 1516239022
+			  })
+		},
+	})
+}
--- jwt/jwt.ts
+++ jwt/jwt.ts
@@ -1,29 +1,27 @@
 namespace $ {
-	export class $mol_jwt extends $mol_object {
-		decode(raw: string): unknown {
-			let line = 'split'
-			try {
-				const token = raw.split('.')[1]
-				if (! token) throw new Error(`No second chunk, splitted by "." in token`)
+	export function $mol_jwt_decode(raw: string): unknown {
+		let line = 'split'
+		try {
+			const token = raw.split('.')[1]
+			if (! token) throw new Error(`No second chunk, splitted by "." in token`)
 
-				line = 'decode'
-				const str = this.$.$mol_base64_decode_string(token)
+			line = 'decode'
+			const str = $mol_base64_decode_string(token)
 
-				line = 'parse'
-				return JSON.parse(str)
-			} catch (e) {
-				if ($mol_promise_like(e)) return $mol_fail_hidden(e)
+			line = 'parse'
+			return JSON.parse(str)
+		} catch (e) {
+			if ($mol_promise_like(e)) return $mol_fail_hidden(e)
 
-				if (e instanceof Error) {
-					if (!e.cause) e.cause = {} as { raw?: string }
+			if (e instanceof Error) {
+				if (!e.cause) e.cause = {} as { raw?: string }
 
-					(e.cause as { raw?: string}).raw = raw
+				(e.cause as { raw?: string}).raw = raw
 
-					e.message += `, ${line} error`
-				}
-
-				$mol_fail_hidden(e)
+				e.message += `, ${line} error`
 			}
+
+			$mol_fail_hidden(e)
 		}
 	}
 }
