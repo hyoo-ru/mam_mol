dcc089186f02359e93a52981d2ee0be7368ad244
nin-jin <nin-jin@users.noreply.github.com>
2023-09-17 15:23:41 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/docs/-/node.test.js
+++ app/docs/-/node.test.js
@@ -1190,7 +1190,7 @@ var $;
             const field = task.name + '()';
             let dict = Object.getOwnPropertyDescriptor(host ?? task, field)?.value;
             const prefix = host?.[Symbol.toStringTag] ?? (host instanceof Function ? $$.$mol_func_name(host) : host);
-            const id = `${prefix}.${task.name}(${$mol_key(key)})`;
+            const id = `${prefix}.${task.name}(${$mol_key(key).replace(/^"|"$/g, "'")})`;
             if (dict) {
                 const existen = dict.get(id);
                 if (existen)
