cb4db67b7701f3d5428c2c72a252002e7597347a
jin <jin@hyoo.ru>
2023-09-22 12:19:10 +0300

    $mol_compare_deep: All $mol_range2 is unique

--- compare/deep/deep.ts
+++ compare/deep/deep.ts
@@ -47,12 +47,12 @@ namespace $ {
 			
 			if( !left_proto ) result = compare_pojo( left, right as any )
 			else if( !Reflect.getPrototypeOf( left_proto ) ) result = compare_pojo( left, right as any )
+			else if( Symbol.toPrimitive in left ) result = compare_primitive( left, right )
 			else if( Array.isArray( left ) ) result = compare_array( left, right as any )
 			else if( left instanceof Set ) result = compare_set( left, right as any )
 			else if( left instanceof Map ) result = compare_map( left, right as any )
 			else if( ArrayBuffer.isView( left ) ) result = compare_buffer( left, right as any )
 			else if( Symbol.iterator in left ) result = compare_iterator( ( left as any )[ Symbol.iterator ](), ( right as any )[ Symbol.iterator ]() )
-			else if( Symbol.toPrimitive in left ) result = compare_primitive( left, right )
 			else result = false
 
 		} finally {
