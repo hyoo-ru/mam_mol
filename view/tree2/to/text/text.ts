98be9e9d0e2c9578edd2088d7f5f7956588a47ed
Stefan Zerkalica <zerkalica@gmail.com>
2023-10-24 01:51:37 +0300

    $mol_view_tree2_ts remove old compiler

--- view/tree2/to/text/text.ts
+++
@@ -1,7 +0,0 @@
-namespace $ {
-	export function $mol_view_tree2_to_text(this: $, tree2_module: $mol_tree2) {
-		const locales: $mol_view_tree2_locales = {}
-		const ts_module = this.$mol_view_tree2_ts_module(tree2_module, locales)
-		return ts_module
-	}
-}
