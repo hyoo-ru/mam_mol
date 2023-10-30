de59426bba1903e8d2a312395d45965907950068
nin-jin <nin-jin@users.noreply.github.com>
2023-10-30 23:54:54 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- gallery/-css/gallery.view.css.ts
+++ gallery/-css/gallery.view.css.ts
@@ -1,3 +1,3 @@
 namespace $ { $mol_style_attach( "mol/gallery/gallery.view.css",
- "[mol_gallery] {\n\tflex-wrap: wrap;\n\tflex: 1 1 auto;\n\talign-items: flex-start;\n    align-content: flex-start;\n}\n"
+ "[mol_gallery] {\n\tflex-wrap: wrap;\n\tflex: 1 1 auto;\n\talign-items: stretch;\n    align-content: stretch;\n}\n"
 ) }
