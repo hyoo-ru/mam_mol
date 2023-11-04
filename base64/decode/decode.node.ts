1b1d0deb28d7ff0aa99da9d515e95cd78b7dfd7a
jin <jin@hyoo.ru>
2023-11-05 00:05:56 +0300

    $mol_base64_decode: safe support

--- base64/decode/decode.node.ts
+++ base64/decode/decode.node.ts
@@ -1,5 +1,8 @@
 namespace $ {
 	export function $mol_base64_decode_node(base64Str: string): Uint8Array {
+		
+		base64Str = base64Str.replace( /-/g, '+' ).replace( /_/g, '/' )
+		
 		// without Uint8Array breaks $mol_compare_deep
 		const buffer = Buffer.from(base64Str, 'base64')
 
