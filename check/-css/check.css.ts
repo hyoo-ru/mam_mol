e83db8a144cd745e78f45a8baf86a7338134bd2f
nin-jin <nin-jin@users.noreply.github.com>
2023-11-16 10:12:25 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- check/-css/check.css.ts
+++ check/-css/check.css.ts
@@ -1,3 +1,3 @@
 namespace $ { $mol_style_attach( "mol/check/check.css",
- "[mol_check] {\n\tflex: 0 0 auto;\n\tjustify-content: flex-start;\n\talign-content: center;\n\talign-items: flex-start;\n\tborder: none;\n\tfont-weight: inherit;\n\tbox-shadow: none;\n\ttext-align: left;\n\tdisplay: inline-flex;\n\tflex-wrap: nowrap;\n}\n\n[mol_check_title] {\n\tflex-shrink: 1;\n}\n"
+ "[mol_check] {\n\tflex: 0 0 auto;\n\tjustify-content: flex-start;\n\talign-content: center;\n\t/* align-items: flex-start; */\n\tborder: none;\n\tfont-weight: inherit;\n\tbox-shadow: none;\n\ttext-align: left;\n\tdisplay: inline-flex;\n\tflex-wrap: nowrap;\n}\n\n[mol_check_title] {\n\tflex-shrink: 1;\n}\n"
 ) }
