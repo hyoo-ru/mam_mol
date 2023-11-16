0a789cd8cb66f33ce63eb851393f9bb63d8da588
nin-jin <nin-jin@users.noreply.github.com>
2023-11-16 14:39:14 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/docs/-/web.js
+++ app/docs/-/web.js
@@ -14532,7 +14532,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("mol/app/questions/questions.view.css", "[mol_app_questions_menu] {\n\tflex: 0 0 40rem;\n}\n\n[mol_app_questions_details_descr] {\n\tmargin: var(--mol_gap_block);\n}\n\n[mol_app_questions_question_link] {\n\tdisplay: block;\n\tjustify-content: flex-start;\n\talign-items: flex-start;\n}\n\n[mol_app_questions_question_tags] {\n\tflex: 1 0 auto;\n\tcolor: var(--mol_theme_shade);\n\tdisplay: flex;\n\tflex-wrap: wrap;\n}\n\n[mol_app_questions_tag] {\n\tmargin-right: .5rem;\n}\n\n[mol_app_questions_question_title] {\n\tflex: 1000 1 auto;\n}\n\n[mol_app_questions_details] {\n\tflex: 1 0 60rem;\n}\n\n[mol_app_questions_details_body] {\n\tpadding: 0;\n}\n\n[mol_app_questions_answer] {\n\tmargin: 1rem;\n}\n");
+    $mol_style_attach("mol/app/questions/questions.view.css", "[mol_app_questions_menu] {\n\tflex: 0 0 40rem;\n}\n\n[mol_app_questions_question_link] {\n\tdisplay: block;\n\tjustify-content: flex-start;\n\talign-items: flex-start;\n}\n\n[mol_app_questions_question_tags] {\n\tflex: 1 0 auto;\n\tcolor: var(--mol_theme_shade);\n\tdisplay: flex;\n\tflex-wrap: wrap;\n}\n\n[mol_app_questions_tag] {\n\tmargin-right: .5rem;\n}\n\n[mol_app_questions_question_title] {\n\tflex: 1000 1 auto;\n}\n\n[mol_app_questions_details] {\n\tflex: 1 0 60rem;\n}\n\n[mol_app_questions_details_body] {\n\tpadding: 0;\n}\n\n[mol_app_questions_answer] {\n\tmargin: 1rem;\n}\n");
 })($ || ($ = {}));
 //mol/app/questions/-css/questions.view.css.ts
 ;
