27bf8ce3f81bca1e82f0cd8dfe9a68421bf2dada
Sergey Yuferev <zerkalica@gmail.com>
2023-12-30 00:37:41 +0300

    $mol_audio_context removed node version

--- audio/context/context.ts
+++ audio/context/context.ts
@@ -1,12 +1,8 @@
 namespace $ {
 	export class $mol_audio_context extends $mol_object2 {
-		static create_context() {
-			return new this.$.$mol_dom_context.AudioContext()
-		}
-
 		@ $mol_memo.method
 		static context() {
-			return this.create_context()
+			return new this.$.$mol_dom_context.AudioContext() || this.$.$node['web-audio-api'].AudioContext
 		}
 	}
 }
