fc89ecd43747dbcbf8ec1c10d6ab7b09e752e2c8
Sergey Yuferev <zerkalica@gmail.com>
2023-12-30 00:41:43 +0300

    $mol_audio_context fix AudioContext

--- audio/context/context.ts
+++ audio/context/context.ts
@@ -2,7 +2,8 @@ namespace $ {
 	export class $mol_audio_context extends $mol_object2 {
 		@ $mol_memo.method
 		static context() {
-			return new this.$.$mol_dom_context.AudioContext() || this.$.$node['web-audio-api'].AudioContext
+			const AudioContext = this.$.$mol_dom_context.AudioContext || this.$.$node['web-audio-api'].AudioContext
+			return new AudioContext()
 		}
 	}
 }
