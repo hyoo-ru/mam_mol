aefb8884876be1e23291f9754f0d87c630cb80d6
nin-jin <nin-jin@users.noreply.github.com>
2024-01-11 15:55:29 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- app/docs/-/node.test.js
+++ app/docs/-/node.test.js
@@ -34785,7 +34785,7 @@ var $;
         code(next) {
             if (next !== undefined)
                 return next;
-            return "const person = [{id: '10', name: 'Jhon'}, false]\n\nconst mut = $mol_mutable(person)\n\nmut[1].id(v => '42')\nmut[1].name(() => 'Dave') \nmut[2]((v) => !v)\n\nconst next = mut()";
+            return "const articles_immutable = {\n\thello: {\n\t\ttitle: 'Hello, World',\n\t\ttags: [ 'javascript', 'immutablity' ],\n\t\tauthor: {\n\t\t\tname: 'Jin',\n\t\t},\n\t},\n}\n\nconst articles_mutable = $mol_mutable( articles_immutable )\n\narticles_mutable.hello.title( prev => prev + '!' )\narticles_mutable.hello.tags( prev => [ ... prev, 'hello' ] )\narticles_mutable.hello.author.name( ()=> 'John' )\narticles_mutable.bye( ()=> ({\n\ttitle: 'Bye, World!',\n\ttags: [],\n\tauthor: null,\n}) )\n\nconst articles_new = articles_mutable()";
         }
         aspects() {
             return [
--- mutable/demo/-view.tree/demo.view.tree.ts
+++ mutable/demo/-view.tree/demo.view.tree.ts
@@ -4,21 +4,34 @@ namespace $ {
 		/**
 		 * ```tree
 		 * code? \
-		 * 	\const person = [{id: '10', name: 'Jhon'}, false]
+		 * 	\const articles_immutable = {
+		 * 	\	hello: {
+		 * 	\		title: 'Hello, World',
+		 * 	\		tags: [ 'javascript', 'immutablity' ],
+		 * 	\		author: {
+		 * 	\			name: 'Jin',
+		 * 	\		},
+		 * 	\	},
+		 * 	\}
 		 * 	\
-		 * 	\const mut = $mol_mutable(person)
+		 * 	\const articles_mutable = $mol_mutable( articles_immutable )
 		 * 	\
-		 * 	\mut[1].id(v => '42')
-		 * 	\mut[1].name(() => 'Dave') 
-		 * 	\mut[2]((v) => !v)
+		 * 	\articles_mutable.hello.title( prev => prev + '!' )
+		 * 	\articles_mutable.hello.tags( prev => [ ... prev, 'hello' ] )
+		 * 	\articles_mutable.hello.author.name( ()=> 'John' )
+		 * 	\articles_mutable.bye( ()=> ({
+		 * 	\	title: 'Bye, World!',
+		 * 	\	tags: [],
+		 * 	\	author: null,
+		 * 	\}) )
 		 * 	\
-		 * 	\const next = mut()
+		 * 	\const articles_new = articles_mutable()
 		 * ```
 		 */
 		@ $mol_mem
 		code(next?: any) {
 			if ( next !== undefined ) return next as never
-			return "const person = [{id: '10', name: 'Jhon'}, false]\n\nconst mut = $mol_mutable(person)\n\nmut[1].id(v => '42')\nmut[1].name(() => 'Dave') \nmut[2]((v) => !v)\n\nconst next = mut()"
+			return "const articles_immutable = {\n\thello: {\n\t\ttitle: 'Hello, World',\n\t\ttags: [ 'javascript', 'immutablity' ],\n\t\tauthor: {\n\t\t\tname: 'Jin',\n\t\t},\n\t},\n}\n\nconst articles_mutable = $mol_mutable( articles_immutable )\n\narticles_mutable.hello.title( prev => prev + '!' )\narticles_mutable.hello.tags( prev => [ ... prev, 'hello' ] )\narticles_mutable.hello.author.name( ()=> 'John' )\narticles_mutable.bye( ()=> ({\n\ttitle: 'Bye, World!',\n\ttags: [],\n\tauthor: null,\n}) )\n\nconst articles_new = articles_mutable()"
 		}
 		
 		/**
