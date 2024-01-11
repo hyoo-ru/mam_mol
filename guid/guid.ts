286e2e3ab37d50136e4423ec0d2c24d46366076d
Mikhail Ivanov <m.ivanov1225@gmail.com>
2024-01-11 15:53:17 +0000

    add guid demo

--- guid/guid.ts
+++ guid/guid.ts
@@ -1,6 +1,9 @@
 namespace $ {
 
-	/** Generates unique identifier. */
+	/** 
+	 * Generates unique identifier. 
+	 * @see https://mol.hyoo.ru/#!section=demos/demo=mol_guid_demo
+	 * */
 	export function $mol_guid(
 		length = 8,
 		exists: ( id: string )=> boolean = ()=> false,
