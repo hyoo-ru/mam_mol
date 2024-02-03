b9906c5856e60c51029e8caec0b428e573571f45
jin <jin@hyoo.ru>
2024-02-03 13:04:55 +0300

    $mol_rest_server: + remote info

--- rest/server/server.node.ts
+++ rest/server/server.node.ts
@@ -55,6 +55,7 @@ namespace $ {
 				place: this,
 				message: msg.method(),
 				url: msg.uri(),
+				remote: req.socket.remoteAddress + ':' + req.socket.remotePort
 			})
 			
 			$mol_wire_sync( res ).setHeader( 'Access-Control-Allow-Origin', '*' )
