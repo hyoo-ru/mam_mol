0907c1f208b3763e06340edfae96e955eaf28797
jin <jin@hyoo.ru>
2024-02-23 18:50:31 +0300

    $mol_error_mix: refactoring

--- error/mix/mix.ts
+++ error/mix/mix.ts
@@ -1,41 +1,30 @@
 namespace $ {
 
 	export class $mol_error_mix extends AggregateError {
-
-		name = $$.$mol_func_name( this.constructor )
 		
+		name = $$.$mol_func_name( this.constructor ).replace( /^\$/, '' ) + '_Error'
+
 		constructor( message: string, ... errors: Error[] ) {
+			
 			super(
 				errors,
-				[ message, ... errors.map( e => e.message.replace( /^/gm, '  ' ) ) ].join( '\n' ),
+				message,
 			)
-		}
-		
-		get cause() {
-			return ( [] as any[] ).concat(
-				... this.errors.map( e => e.cause ).filter( Boolean )
-			)
+			
+			const stack_get = Object.getOwnPropertyDescriptor( this, 'stack' )?.get!
+			
+			Object.defineProperty( this, 'stack', {
+				get: ()=> stack_get.call( this ) + '\n' + this.errors.map(
+					e => e.stack.trim().replace( /at /gm, '   at ' ).replace( /^(?!    )(.*)/gm, '    at [$1] (#)' )
+				).join('\n')
+			} )
+			
 		}
 		
 		toJSON() {
 			return this.errors.map( e => e.message )
 		}
 		
-		pick< Class extends typeof Error >( Class: Class ): InstanceType< Class > | null {
-			
-			if( ( this as any ) instanceof Class ) return this as any
-			
-			for( const e of this.errors ) {
-				if( e instanceof Class ) return e as any
-			}
-			
-			for( const e of this.cause ) {
-				if( e && e instanceof Class ) return e as any
-			}
-			
-			return null
-		}
-
 	}
 
 }
