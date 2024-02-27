2cdf2b41a1789d9490e3b30aee5f37fcb969d5eb
nin-jin <sairi-na-tenshi@ya.ru>
2024-02-27 22:48:14 +0300

    Update build.node.ts

--- build/build.node.ts
+++ build/build.node.ts
@@ -592,7 +592,7 @@ namespace $ {
 		}
 
 		interactive() {
-			return ! process.stdout.isTTY
+			return process.stdout.isTTY
 		}
 
 		@ $mol_mem
@@ -607,7 +607,7 @@ namespace $ {
 		gitPull(path: string) {
 			const args = [ 'pull' ]
 
-			if ( this.interactive() ) {
+			if ( ! this.interactive() ) {
 				// depth и deepen не годятся для локальной разработки, поэтому оставляем ограничение глубины пула только для CI
 				// --depth=1 в сочетании с сабмодулями обрезает историю, кроме первого коммита
 				// --deepen=1 в git-конфиге сабмодуля выставляет bare=true, после этого все команды падают с сообщением
