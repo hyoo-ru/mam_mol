d38e7f394d35b26fc9d581079bfb607e96c5cebd
jin <jin@hyoo.ru>
2024-03-13 20:01:54 +0300

    $mol_scroll: Fixed virt render

--- support/css/css.ts
+++ support/css/css.ts
@@ -3,10 +3,7 @@ namespace $ {
 	let cache = null as null | boolean
 	export function $mol_support_css_overflow_anchor( this: $ ) {
 		return cache ?? (
-			cache = (
-				!/Gecko\//.test( navigator.userAgent ) // FF doesn't anchow while scroll animates
-				&& this.$mol_dom_context.CSS?.supports( 'overflow-anchor:auto' )
-			) ?? false
+			cache = this.$mol_dom_context.CSS?.supports( 'overflow-anchor:auto' ) ?? false
 		)
 	}
 
