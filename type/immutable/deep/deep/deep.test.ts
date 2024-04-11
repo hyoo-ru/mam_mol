f0f144cfb6f2fc4721e3d319288d92ee87907cb5
jin <jin@hyoo.ru>
2024-04-11 09:23:28 +0300

    $mol_type_immutable_deep: more test

--- type/immutable/deep/deep/deep.test.ts
+++ type/immutable/deep/deep/deep.test.ts
@@ -1,8 +1,18 @@
 namespace $ {
 
-	type deep_immutable = $mol_type_assert<
+	type deep_immutable_object = $mol_type_assert<
 		$mol_type_immutable_deep< { foo: number[] } > ,
 		{ readonly foo: readonly number[] }
+	>
+
+	type deep_immutable_array = $mol_type_assert<
+		$mol_type_immutable_deep< number[][] > ,
+		readonly ( readonly number[] )[]
+	>
+
+	type deep_immutable_tuple = $mol_type_assert<
+		$mol_type_immutable_deep< [[number]] > ,
+		readonly[ readonly[ number ] ]
 	>
 
 }
