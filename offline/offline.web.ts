e1911e7e5687ce01c470e221bc719bbbdeb1700c
jin <jin@hyoo.ru>
2024-07-10 17:00:21 +0300

    $mol_offline: autoreload on service worker update

--- offline/offline.web.ts
+++ offline/offline.web.ts
@@ -99,7 +99,15 @@ namespace $ {
 		} else if( !navigator.serviceWorker ) {
 			console.warn( 'Service Worker is not supported.' )
 		} else {
-			navigator.serviceWorker.register( 'web.js' )
+			navigator.serviceWorker.register( 'web.js' ).then( reg => {
+				reg.addEventListener( 'updatefound', ()=> {
+					const worker = reg.installing!
+					worker.addEventListener( 'statechange', ()=> {
+						if( worker.state !== 'activated' ) return
+						window.location.reload()
+					} )
+				} )
+			} )
 		}
 
 	}
