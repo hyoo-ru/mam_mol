8f180ba1b2d892cefe039d949a6ad5e311a4936f
jin <jin@hyoo.ru>
2024-07-20 23:40:28 +0300

    $mol_promise_like: error proof

--- promise/like/like.ts
+++ promise/like/like.ts
@@ -1,7 +1,11 @@
 namespace $ {
 	
 	export function $mol_promise_like( val: any ): val is Promise<any> {
-		return val && typeof val === 'object' && 'then' in val && typeof val.then === 'function'
+		try {
+			return val && typeof val === 'object' && 'then' in val && typeof val.then === 'function'
+		} catch {
+			return false
+		}
 	}
 
 }
