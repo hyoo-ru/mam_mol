9a1b6eb861d06fccc2656a8790ecb58c48aa0992
jin <jin@hyoo.ru>
2024-08-10 18:28:55 +0300

    $mol_embed: rutube support, fullscreen fix

--- embed/any/any.view.ts
+++ embed/any/any.view.ts
@@ -12,6 +12,7 @@ namespace $.$$ {
 				if( /\b(png|gif|jpg|jpeg|jfif|webp|svg)\b/.test( uri ) ) return 'image'
 				if( /^https:\/\/www\.youtube\.com\//.test( uri ) ) return 'youtube'
 				if( /^https:\/\/youtu\.be\//.test( uri ) ) return 'youtube'
+				if( /^https:\/\/rutube\.ru\//.test( uri ) ) return 'rutube'
 				
 			} catch( error ) {
 				$mol_fail_log( error )
@@ -26,6 +27,7 @@ namespace $.$$ {
 			switch( this.type() ) {
 				case 'image': return [ this.Image() ]
 				case 'youtube': return [ this.Youtube() ]
+				case 'rutube': return [ this.Rutube() ]
 				default: return [ this.Object() ]
 			}
 		}
