86e4de39aad3274e3b7e7d5088581148de8b2c47
jin <jin@hyoo.ru>
2024-09-14 14:37:22 +0300

    chokidar workaround

--- file/file.node.ts
+++ file/file.node.ts
@@ -58,7 +58,7 @@ namespace $ {
 			
 			const watcher = $node.chokidar.watch( this.path() , {
 				persistent : true ,
-				ignored: /(^\.|___$)/ ,
+				ignored: path => /([\/\\]\.|___$)/.test( path ) ,
 				depth :  0 ,
 				ignoreInitial : true ,
 				awaitWriteFinish: {
