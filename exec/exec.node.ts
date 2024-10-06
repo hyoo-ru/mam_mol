ab11a10c7f35d51d5aeb12329b14129ecd46a5a6
Stefan Zerkalica <zerkalica@gmail.com>
2024-10-06 13:32:03 +0300

    $mol_exec autokill if no output

--- exec/exec.node.ts
+++ exec/exec.node.ts
@@ -1,11 +1,12 @@
 namespace $ {
-	export function $mol_exec(
+	export const $mol_exec_deadline = 5000
+
+	export function $mol_exec_async(
 		this : $ ,
 		dir : string ,
 		command : string ,
 		...args : string[]
 	) {
-
 		let [ app , ... args0 ] = command.split( ' ' )
 		args = [ ... args0 , ... args ]
 
@@ -16,22 +17,84 @@ namespace $ {
 			command: `${app} ${ args.join(' ') }` ,
 		})
 
-		var res = $node['child_process'].spawnSync(
-			app ,
-			args,
-			{
-				cwd : $node.path.resolve( dir ) ,
-				shell : true ,
-				env: this.$mol_env(),
-			}
-		)
-		
-		if( res.status || res.error ) {
-			return $mol_fail( res.error || new Error( res.stderr.toString(), { cause: res.stdout } ) )
-		}
-		
-		if( !res.stdout ) res.stdout = Buffer.from([])
+		const sub = $node['child_process'].spawn(app, args, {
+			stdio: 'inherit',
+			shell: true,
+			cwd: dir,
+			env: this.$mol_env(),
+		})
 
-		return res
+		let first = true
+		let error = null as null | Error
+		let timer: undefined | ReturnType<typeof setTimeout>
+
+		const std_data = [] as Buffer[]
+		const error_data = [] as Buffer[]
+
+		const reset = (std_chunk?: Buffer, error_chunk?: Buffer) => {
+			if (std_chunk) std_data.push(std_chunk)
+			if (error_chunk) error_data.push(error_chunk)
+
+			clearTimeout(timer)
+
+			timer = setTimeout(() => {
+				sub.kill(first ? 'SIGTERM' : 'SIGKILL')
+
+				if (first) reset()
+
+				first = false
+			}, this.$mol_exec_deadline)
+		}
+
+		reset()
+
+		sub.stderr!.on('data', data => reset(data) )
+		sub.stdout!.on('data', data => reset(undefined, data) )
+		sub.on('error', err => { error = err })
+
+		let destructed = false
+
+		const promise = new Promise<{
+			pid?: number
+			stdout: Buffer
+			stderr: Buffer
+			status?: number | null
+			signal: NodeJS.Signals | null,
+		}>((done, fail) => {
+			sub.on('close', (status, signal) => {
+				if (destructed) return
+				clearTimeout(timer)
+				const stderr = Buffer.concat(error_data)
+				const stdout = Buffer.concat(std_data)
+
+				if( status || error ) {
+					return fail( error || new Error( stderr.toString(), { cause: stdout } ) )
+				}
+				
+				done({
+					pid: sub.pid,
+					stdout,
+					stderr,
+					status,
+					signal,
+				})
+
+			})
+		})
+
+		return Object.assign(promise, { destructor: () => {
+			destructed = true
+			clearTimeout(timer)
+			sub.kill('SIGKILL')
+		} })
+	}
+
+	export function $mol_exec(
+		this : $ ,
+		dir : string ,
+		command : string ,
+		...args : string[]
+	) {
+		return $mol_wire_sync(this).$mol_exec_async( dir, command, ...args )
 	}
 }
