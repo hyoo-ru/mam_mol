a1a34a680e4c010cf8f7bf3b0debd8a281eb7b4a
luckylionheart <uyscuti0513@gmail.com>
2024-10-08 22:49:46 -0400

    add: missing preview feature

--- drag/drag.view.ts
+++ drag/drag.view.ts
@@ -30,6 +30,16 @@ namespace $.$$ {
 
             (event as DragEvent).dataTransfer!.setDragImage(this.image(), 0, -32);
 
+			const effects = [] as string[]
+			if( this.allow_copy() ) effects.push( 'Copy' )
+			if( this.allow_link() ) effects.push( 'Link' )
+			if( this.allow_move() ) effects.push( 'Move' )
+
+			let effectAllowed = effects[0].toLowerCase() + effects.slice(1).join('')
+			if( effectAllowed === 'copyLinkMove' ) effectAllowed = 'all';
+
+			(event as DragEvent).dataTransfer!.effectAllowed = effectAllowed as DataTransfer['effectAllowed']
+			
             this.start(event as DragEvent);
         }
 
