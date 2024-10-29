7d3ce8a24c83d018500f8e7e3d03ed72327410aa
Stefan Zerkalica <zerkalica@gmail.com>
2024-10-29 23:00:50 +0300

    $mol_fetch_service fix bug with fetch_event binded scope

--- fetch/service/service.web.ts
+++ fetch/service/service.web.ts
@@ -1,22 +1,22 @@
 namespace $ {
 	export class $mol_fetch_service_web extends $mol_fetch_service {
 		override init() {
-			const service = this.$.$mol_service_web
-			const worker = service.worker()
-			worker.addEventListener('fetch', this.$.$mol_fetch_service_web.fetch_event.bind(this))
+			const worker = this.$.$mol_service_web.worker()
+			const fetch_service = this.$.$mol_fetch_service_web
+
+			worker.addEventListener('fetch', fetch_service.fetch_event.bind(fetch_service))
 		}
 
 		static fetch_event(event: FetchEvent) {
 			const request = event.request
-			const plugins = this.$.$mol_fetch_service.plugins
 
-			for (const plugin of plugins) {
+			for (const plugin of this.plugins) {
 				if (plugin.blocked(request)) {
 					return event.respondWith(this.blocked_response())
 				}
 			}
 
-			for (const plugin of plugins) {
+			for (const plugin of this.plugins) {
 				const response = plugin.modify(request)
 				if (response) return event.respondWith(response)
 			}
