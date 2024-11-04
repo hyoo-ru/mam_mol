a2aa4c3b49b0a9b52b15fe90e6be3478e7928abd
Stefan Zerkalica <zerkalica@gmail.com>
2024-11-04 20:29:53 +0300

    $mol_offline fix prompt offline

--- offline/offline.web.ts
+++ offline/offline.web.ts
@@ -9,5 +9,7 @@ namespace $ {
 	$.$mol_offline = $mol_offline_web
 	$mol_service_plugin.$mol_offline = $.$mol_offline_web
 
-	new $mol_after_tick(() => $mol_offline_web.prompt())
+	if (! $mol_service_ensure()) {
+		new $mol_after_tick(() => $mol_offline_web.prompt())
+	}
 }
