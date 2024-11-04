21e056ec430790b36dfe23001def1acf5046da7a
Stefan Zerkalica <zerkalica@gmail.com>
2024-11-04 15:57:34 +0300

    $mol_wire_sync: idempotent new operator support

--- wire/sync/sync.test.ts
+++ wire/sync/sync.test.ts
@@ -15,6 +15,79 @@ namespace $ {
 		
 			type Check = $mol_type_assert<ReturnType<typeof A['b']>, string>
 		},
+
+		async 'test method from host'( $ ) {
+			let count = 0
+			class A {
+				static a() {
+					return $mol_wire_sync(this).b()
+				}
+
+				static b() { return Promise.resolve(++count) }
+			}
+
+			
+			$mol_assert_equal(await $mol_wire_async(A).a(), 1, count)
+
+		},
+
+		async 'test function'( $ ) {
+			let count = 0
+			class A {
+				static a() {
+					return $mol_wire_sync(this.b)()
+				}
+
+				static b() { return Promise.resolve(++count) }
+			}
+
+			
+			$mol_assert_equal(await $mol_wire_async(A).a(), 1, count)
+
+		},
+
+		async 'test construct from scope'( $ ) {
+			class A {
+				static instances = [] as A[]
+
+				static a() {
+					const a = new ($mol_wire_sync(scope).A)()
+					this.instances.push( a )
+					$mol_wire_sync(this).b()
+				}
+
+				static b() { return Promise.resolve() }
+			}
+			const scope = { A }
+
+			await $mol_wire_async(A).a()
+			$mol_assert_equal(A.instances.length, 2)
+			$mol_assert_equal(A.instances[0] instanceof A)
+
+			$mol_assert_equal(A.instances[0], A.instances[1])
+
+		},
+
+		async 'test construct itself'( $ ) {
+			class A {
+				static instances = [] as A[]
+
+				static a() {
+					const a = new ($mol_wire_sync_make(A))()
+					this.instances.push( a )
+					$mol_wire_sync(this).b()
+				}
+
+				static b() { return Promise.resolve() }
+			}
+
+			await $mol_wire_async(A).a()
+			$mol_assert_equal(A.instances.length, 2)
+			$mol_assert_equal(A.instances[0] instanceof A)
+
+			$mol_assert_equal(A.instances[0], A.instances[1])
+
+		}
 	})
 
 }
--- wire/sync/sync.ts
+++ wire/sync/sync.ts
@@ -1,5 +1,20 @@
 namespace $ {
-	
+	const constructors = new WeakMap<Function, Function>()
+
+	function $mol_wire_sync_factory<Args extends unknown[], Result>(
+		val: new (...args: Args) => Result
+	) {
+		let make = constructors.get(val) as null | ((...args: Args) => Result)
+
+		if (! make) {
+			make = $mol_func_name_from((...args: Args) => new val(...args), val)
+
+			constructors.set(val, make)
+		}
+
+		return make
+	}
+
 	/**
 	 * Convert asynchronous (promise-based) API to synchronous by wrapping function and method calls in a fiber.
 	 * @see https://mol.hyoo.ru/#!section=docs/=1fcpsq_1wh0h2
@@ -9,24 +24,37 @@ namespace $ {
 			
 			get( obj, field ) {
 				
-				const val = (obj as any)[ field ]
+				let val = (obj as any)[ field ]
 				if( typeof val !== 'function' ) return val
-				
-				const temp = $mol_wire_task.getter( val )
-				return function $mol_wire_sync( this: Host, ... args: any[] ) {
-					const fiber = temp( obj, args )
-					return fiber.sync()
-				}
-				
+
+				return new Proxy( val, {
+					apply( target, self, args ) {
+						return $mol_wire_task.get(target, obj, args ).sync()
+					},
+					construct(target, args) {
+						const constr = $mol_wire_sync_factory(target)
+						return $mol_wire_task.get(constr , obj, args ).sync() as object
+					},
+					
+				})
 			},
-			
+
+			construct(obj, args) {
+				const constr = $mol_wire_sync_factory(obj as new ( ... args: unknown[] )=> unknown)
+				return $mol_wire_task.get( constr , obj, args ).sync() as object
+			},
+
 			apply( obj, self, args ) {
-				const temp = $mol_wire_task.getter( obj as ( ... args: any[] )=> any )
-				const fiber = temp( self, args )
-				return fiber.sync()
+				return $mol_wire_task.get(obj as ( ... args: any[] )=> any, self, args).sync()
 			},
 			
 		} ) as unknown as ObjectOrFunctionResultAwaited<Host>
+	}
+
+	export function $mol_wire_sync_make< Constructor extends (new (...args: any[]) => unknown) > (
+		obj: Constructor
+	) {
+		return $mol_wire_sync(obj) as typeof obj
 	}
 
 	type FunctionResultAwaited<Some> = Some extends (...args: infer Args) => infer Res
