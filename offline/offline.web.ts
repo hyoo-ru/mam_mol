1fd1309666c6e0f666933e99a8749d7bcc5540c6
jin <jin@hyoo.ru>
2025-01-02 19:47:07 +0300

    -1

--- offline/offline.web.ts
+++ offline/offline.web.ts
@@ -53,6 +53,7 @@ namespace $ {
 					const headers = new Headers( response.headers )
 					headers.set( "Cross-Origin-Embedder-Policy", "credentialless" )
 					headers.set( "Cross-Origin-Opener-Policy", "same-origin" )
+					headers.set( "$mol_offline", "" )
 					
 					response = new Response( response.body, {
 						status: response.status,
