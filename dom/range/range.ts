f06daaa8bf6badee12946b597040311effea9027
nin-jin <sairi-na-tenshi@ya.ru>
2025-01-04 23:16:11 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@cfbd96c60857922365efc3e5e813be1fcdbee48e 🚀

--- dom/range/range.ts
+++ dom/range/range.ts
@@ -2,9 +2,14 @@ namespace $ {
 	export class $mol_dom_range extends Object {
 		
 		constructor(
-			readonly head: $mol_dom_point,
-			readonly foot: $mol_dom_point,
+			readonly anchor: $mol_dom_point,
+			readonly extend: $mol_dom_point,
 		) { super() }
+		
+		/** Iterator for deep comparison. */
+		[ Symbol.iterator ]() {
+			return [ this.anchor, this.extend ].values()
+		}
 		
 		static from_selection( sel = $mol_dom_context.getSelection()! ) {
 			return this.from_native( sel?.getRangeAt(0) )
@@ -19,31 +24,46 @@ namespace $ {
 		
 		static inside( node: Node ) {
 			return new this(
-				$mol_dom_point.start( node ),
-				$mol_dom_point.end( node ),
+				$mol_dom_point.head( node ),
+				$mol_dom_point.foot( node ),
 			)
 		}
 		
 		static around( node: Node ) {
-			
-			const parent = node.parentNode!
-			const pos = [ ... parent.childNodes ].indexOf( node as ChildNode )
-			
-			return new this(
-				new $mol_dom_point( parent, pos ),
-				new $mol_dom_point( parent, pos + 1 ),
-			)
-			
+			return $mol_dom_range.inside( node ).expand()
 		}
 		
 		is_empty() {
-			return this.head.node === this.foot.node && this.head.pos === this.foot.pos
+			return this.anchor.node === this.extend.node && this.anchor.pos === this.extend.pos
+		}
+
+		expand() {
+			return new $mol_dom_range(
+				this.anchor.jump(-1)!,
+				this.extend.jump(+1)!,
+			)
 		}
 		
 		clear() {
 			this.native().deleteContents()
 		}
+
+		surround( el: Element ) {
+			this.native().surroundContents( el )
+		}
+
+		point_compare( point: $mol_dom_point ) {
+			if( point.is_tail() ) return this.native().comparePoint(
+				point.node.parentNode!,
+				[ ... point.node.parentNode!.childNodes ].indexOf( point.node as ChildNode ),
+			)
+			else return this.native().comparePoint( point.node, point.pos )
+		}
 		
+		range_contains( range: $mol_dom_range ) {
+			return ( this.point_compare( range.anchor ) === 0 )&&( this.point_compare( range.extend ) === 0 )
+		}
+
 		select() {
 			const sel = $mol_dom_context.document.getSelection()!
 			sel.removeAllRanges()
@@ -51,9 +71,15 @@ namespace $ {
 		}
 		
 		native() {
+
 			const range = $mol_dom_context.document.createRange()
-			range.setEnd( this.foot.node, this.foot.pos )
-			range.setStart( this.head.node, this.head.pos )
+
+			if( this.extend.is_tail() ) range.setEndAfter( this.extend.node )
+			else range.setEnd( this.extend.node, this.extend.pos )
+		
+			if( this.anchor.is_tail() ) range.setStartAfter( this.anchor.node )
+			else range.setStart( this.anchor.node, this.anchor.pos )
+
 			return range
 		}
 		
