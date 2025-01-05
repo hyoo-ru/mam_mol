b63aa88ef5949790ff1d8be90ec7d0c3e533ddb6
nin-jin <sairi-na-tenshi@ya.ru>
2025-01-05 18:34:10 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@4917f911df67f4a2007ca90762661bfc3c55477a 🚀

--- build/-/node.test.js
+++ build/-/node.test.js
@@ -3645,6 +3645,11 @@ var $;
 "use strict";
 var $;
 (function ($) {
+    function is_identifier(tree) {
+        if (tree.type)
+            return false;
+        return /^[a-z_$][a-z_$0-9]*$/i.test(tree.text());
+    }
     function $mol_tree2_js_to_text(js) {
         function sequence(open, separator, close) {
             return (input, belt) => [
@@ -3745,25 +3750,21 @@ var $;
             '{}': block('{', '', '}'),
             '[]': (input, belt) => {
                 const first = input.kids[0];
-                if (first.type)
+                if (!is_identifier(first))
                     return sequence('[', '', ']')(input, belt);
                 else
                     return [input.data('.' + first.text())];
             },
             '?.[]': (input, belt) => {
                 const first = input.kids[0];
-                if (first.type)
+                if (!is_identifier(first))
                     return sequence('?.[', '', ']')(input, belt);
                 else
                     return [input.data('?.' + first.text())];
             },
-            ':': (input, belt) => {
-                const first = input.kids[0];
-                if (first.type)
-                    return duplet('[', ']: ')(input, belt);
-                else
-                    return duplet('', ': ')(input, belt);
-            },
+            ':': (input, belt) => input.kids[0].type
+                ? duplet('[', ']: ')(input, belt)
+                : duplet('', ': ')(input, belt),
             'let': duplet('let ', ' = '),
             'const': duplet('const ', ' = '),
             'var': duplet('var ', ' = '),
@@ -3794,7 +3795,7 @@ var $;
             '?:': triplet('', ' ? ', ' : '),
             '.': (input, belt) => {
                 const first = input.kids[0];
-                if (first.type)
+                if (!is_identifier(first))
                     return triplet('[', ']')(input, belt);
                 else
                     return [
