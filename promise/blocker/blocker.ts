989419cafaaa2b1cffa9e4d25e8dcf4af4953c2f
Stefan Zerkalica <zerkalica@gmail.com>
2025-02-10 10:58:19 +0300

    $mol_promise_blocker change constructor name with $

--- promise/blocker/blocker.ts
+++ promise/blocker/blocker.ts
@@ -1,7 +1,7 @@
 namespace $ {
 
 	export class $mol_promise_blocker<Result> extends $mol_promise<Result> {
-		static [Symbol.toStringTag] = 'mol_promise_blocker'
+		static [Symbol.toStringTag] = '$mol_promise_blocker'
 	}
 
 }
