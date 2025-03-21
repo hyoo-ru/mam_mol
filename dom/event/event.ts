d38a161e5a890b8d48d883be56d13e2bc77deb6c
Stefan Zerkalica <zerkalica@gmail.com>
2025-03-21 11:37:46 +0300

    $mol_dom_event wrapper

---
+++ dom/event/event.ts
@@ -0,0 +1,21 @@
+namespace $ {
+	export class $mol_dom_event_wrapper<EventType extends Event> extends $mol_object {
+		constructor(readonly native: EventType) { super() }
+
+		prevented(next?: boolean) {
+			if (next) this.native.preventDefault()
+			return this.native.defaultPrevented
+		}
+
+		@ $mol_action
+		static wrap<EventType extends Event>(event: EventType) {
+			return new this.$.$mol_dom_event_wrapper($mol_wire_sync(event))
+		}
+	}
+
+	export function $mol_dom_event<EventType extends Event>(
+		event: EventType
+	) {
+		return $mol_dom_event_wrapper.wrap(event)
+	}
+}
