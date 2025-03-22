b930abfaa3a8b84b2af0b4f455e0aa917489d9de
Stefan Zerkalica <zerkalica@gmail.com>
2025-03-22 13:39:38 +0300

    $mol_book2_catalog menu item

---
+++ book2/catalog/catalog.view.css.ts
@@ -0,0 +1,15 @@
+namespace $.$$ {
+	$mol_style_define( $mol_book2_catalog, {
+		Menu_filter: {
+			flex: {
+				shrink: 0,
+				grow: 0,
+			},
+			alignSelf: 'stretch',
+		},
+		Menu_item: {
+			flex: { direction: 'column' },
+		},
+	})
+
+}
