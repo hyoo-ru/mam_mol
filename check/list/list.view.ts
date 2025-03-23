f9a2ddd21ec769001c033326cb4db5c3b077b2da
Stefan Zerkalica <zerkalica@gmail.com>
2025-03-23 14:57:56 +0300

    $mol_switch icons with text

--- check/list/list.view.ts
+++ check/list/list.view.ts
@@ -6,14 +6,6 @@ namespace $.$$ {
 	 */
 	export class $mol_check_list extends $.$mol_check_list {
 
-		options() : { [ key : string ] : string } {
-			return {}
-		}
-
-		override dictionary(next?: Record<string, boolean>) {
-			return next ?? {}
-		}
-
 		override option_checked(id: string, next?: boolean | null) {
 			const prev = this.dictionary()
 			if (next === undefined) return prev[id] ?? null
@@ -37,7 +29,28 @@ namespace $.$$ {
 		option_title( key : string ) {
 			return this.options()[key] || key
 		}
-		
+
+		@ $mol_mem
+		has_icons() {
+			return Object.keys(this.icons()).length > 0
+		}
+
+		override option_hint(id: string) {
+			return this.has_icons() && this.icon_only()
+				? this.option_title(id)
+				: super.option_hint(id)
+		}
+
+		override option_label( id: string ) {
+			const label = super.option_label(id)
+
+			if (this.has_icons()) {
+				const icon = this.icons()[id]
+				return this.icon_only() ? [ icon ] : [ icon, ...label ]
+			}
+
+			return label
+		}
 	}
 
 }
