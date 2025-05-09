96f669b4ea2a05d1024fb72f6ee43f4d408fb663
jin <jin@hyoo.ru>
2025-05-09 23:30:06 +0300

    $mol_offline: wait for html load to prevent fail in chrome in iframe (maybe)

--- offline/offline.web.ts
+++ offline/offline.web.ts
@@ -116,15 +116,17 @@ namespace $ {
 		} else if( !navigator.serviceWorker ) {
 			console.warn( 'Service Worker is not supported.' )
 		} else {
-			navigator.serviceWorker.register( 'web.js' ).then( reg => {
-				// reg.addEventListener( 'updatefound', ()=> {
-				// 	const worker = reg.installing!
-				// 	worker.addEventListener( 'statechange', ()=> {
-				// 		if( worker.state !== 'activated' ) return
-				// 		window.location.reload()
-				// 	} )
-				// } )
-			} )
+			$mol_dom.addEventListener( 'DOMContentLoaded', ()=> {
+				navigator.serviceWorker.register( 'web.js' ).then( reg => {
+					// reg.addEventListener( 'updatefound', ()=> {
+					// 	const worker = reg.installing!
+					// 	worker.addEventListener( 'statechange', ()=> {
+					// 		if( worker.state !== 'activated' ) return
+					// 		window.location.reload()
+					// 	} )
+					// } )
+				} )
+			}
 		}
 
 	}
