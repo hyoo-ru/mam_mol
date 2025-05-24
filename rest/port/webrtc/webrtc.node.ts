0b0b07967541ea8a700672bf7f31a6a05fd7e0b9
nin-jin <sairi-na-tenshi@ya.ru>
2025-05-24 13:42:53 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@e52fbafd5d57c55517f7d87cbb6bd727ee41a6ec 🚀

--- rest/port/webrtc/webrtc.node.ts
+++ rest/port/webrtc/webrtc.node.ts
@@ -2,7 +2,7 @@ namespace $ {
 	
 	export class $mol_rest_port_webrtc extends $mol_rest_port {
 		
-		channel!: InstanceType< typeof import( 'node-datachannel/polyfill' ).RTCDataChannel >
+		channel!: RTCDataChannel
 		
 		@ $mol_action
 		send_bin( data: Uint8Array ) {
