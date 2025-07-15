6003d429ea37e55d4d86570854e1bd81d989a1af
jin <jin@hyoo.ru>
2025-07-15 14:06:49 +0300

    $mol_buffer: right bigint comparison

--- buffer/buffer.ts
+++ buffer/buffer.ts
@@ -36,74 +36,87 @@ namespace $ {
 			}
 		}
 		
+		/** 1-byte signed integer channel for offset. */
 		int8( offset: number, next?: number ) {
 			if( next === undefined ) return this.getInt8( offset )
 			if( next >= -(2**7) && next < 2**7 ) return this.setInt8( offset, next ), next
 			$mol_fail( new Error( `Wrong int8 value ${ next }` ) )
 		}
 		
+		/** 1-byte unsigned integer channel for offset. */
 		uint8( offset: number, next?: number ) {
 			if( next === undefined ) return this.getUint8( offset )
 			if( next >= 0 && next < 2**8 ) return this.setUint8( offset, next ), next
 			$mol_fail( new Error( `Wrong uint8 value ${ next }` ) )
 		}
 		
+		/** 2-byte signed integer little-endian channel for offset. */
 		int16( offset: number, next?: number ) {
 			if( next === undefined ) return this.getInt16( offset, true )
 			if( next >= -(2**15) && next < 2**15 ) return this.setInt16( offset, next, true ), next
 			$mol_fail( new Error( `Wrong int16 value ${ next }` ) )
 		}
 		
+		/** 2-byte unsigned integer little-endian channel for offset. */
 		uint16( offset: number, next?: number ) {
 			if( next === undefined ) return this.getUint16( offset, true )
 			if( next >= 0 && next < 2**16 ) return this.setUint16( offset, next, true ), next
 			$mol_fail( new Error( `Wrong uint16 value ${ next }` ) )
 		}
 		
+		/** 4-byte signed integer little-endian channel for offset. */
 		int32( offset: number, next?: number ) {
 			if( next === undefined ) return this.getInt32( offset, true )
 			if( next >= -(2**31) && next < 2**31 ) return this.setInt32( offset, next, true ), next
 			$mol_fail( new Error( `Wrong int32 value ${ next }` ) )
 		}
 	
+		/** 4-byte unsigned integer little-endian channel for offset. */
 		uint32( offset: number, next?: number ) {
 			if( next === undefined ) return this.getUint32( offset, true )
 			if( next >= 0 && next < 2**32 ) return this.setUint32( offset, next, true ), next
 			$mol_fail( new Error( `Wrong uint32 value ${ next }` ) )
 		}
 		
+		/** 8-byte signed integer little-endian channel for offset. */
+		int64( offset: number, next?: bigint ) {
+			if( next === undefined ) return this.getBigInt64( offset, true )
+			if( next >= -(2n**63n) && next < 2n**63n ) return this.setBigInt64( offset, next, true ), next
+			$mol_fail( new Error( `Wrong int64 value ${ next }` ) )
+		}
+		
+		/** 6-byte unsigned integer little-endian channel for offset. */
 		uint48( offset: number, next?: number ) {
 			if( next === undefined ) return this.getUint48( offset, true )
 			if( next >= 0 && next < 2**48 ) return this.setUint48( offset, next, true ), next
 			$mol_fail( new Error( `Wrong uint48 value ${ next }` ) )
 		}
 		
-		int64( offset: number, next?: bigint ) {
-			if( next === undefined ) return this.getBigInt64( offset, true )
-			if( next >= -(2**63) && next < 2**63 ) return this.setBigInt64( offset, next, true ), next
-			$mol_fail( new Error( `Wrong int64 value ${ next }` ) )
-		}
-		
+		/** 8-byte unsigned integer little-endian channel for offset. */
 		uint64( offset: number, next?: bigint ) {
 			if( next === undefined ) return this.getBigUint64( offset, true )
-			if( next >= 0 && next < 2**64 ) return this.setBigUint64( offset, next, true ), next
+			if( next >= 0n && next < 2n**64n ) return this.setBigUint64( offset, next, true ), next
 			$mol_fail( new Error( `Wrong uint64 value ${ next }` ) )
 		}
 		
+		/** 4-byte float little-endian channel for offset. */
 		float32( offset: number, next?: number ) {
 			if( next !== undefined ) this.setFloat32( offset, next, true )
 			return this.getFloat32( offset, true )
 		}
 		
+		/** 8-byte float little-endian channel for offset. */
 		float64( offset: number, next?: number ) {
 			if( next !== undefined ) this.setFloat64( offset, next, true )
 			return this.getFloat64( offset, true )
 		}
 		
+		/** A Uint8Array view for the same buffer. */
 		asArray() {
 			return new Uint8Array( this.buffer, this.byteOffset, this.byteLength )
 		}
 		
+		/** base64ae string from buffer. */
 		toString() {
 			return $mol_base64_ae_encode( this.asArray() )
 		}
