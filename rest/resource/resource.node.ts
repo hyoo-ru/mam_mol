466de8f6149d9e5fc29300b0104cef7e28c42435
nin-jin <sairi-na-tenshi@ya.ru>
2025-08-25 16:06:26 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@90e9c27b2e0c65096ccaf104f41913f9cd59faa8 🚀

--- rest/resource/resource.node.ts
+++ rest/resource/resource.node.ts
@@ -22,68 +22,68 @@ namespace $ {
 			return ( $mol_wire_sync( this )[ msg.method() as never ] as any )( msg )
 		}
 		
-		async OPTIONS( msg: $mol_rest_message ) {
+		// async OPTIONS( msg: $mol_rest_message ) {
 			
-			if( msg.type() !== 'application/sdp' ) return msg.reply( null )
+		// 	if( msg.type() !== 'application/sdp' ) return msg.reply( null )
 			
-			const { RTCPeerConnection } = await import( 'node-datachannel/polyfill' )
-			const connection = new RTCPeerConnection
+		// 	const { RTCPeerConnection } = await import( 'node-datachannel/polyfill' )
+		// 	const connection = new RTCPeerConnection
 			
-			const channel = connection.createDataChannel( msg.uri().toString(), { negotiated: true, id: 0 } )
-			const port = $mol_rest_port_webrtc.make({ channel })
+		// 	const channel = connection.createDataChannel( msg.uri().toString(), { negotiated: true, id: 0 } )
+		// 	const port = $mol_rest_port_webrtc.make({ channel })
 			
-			$mol_wire_sync( this.$ ).$mol_log3_come({
-				place: this,
-				message: 'OPEN',
-				url: msg.uri(),
-				port: $mol_key( port ),
-			})
+		// 	$mol_wire_sync( this.$ ).$mol_log3_come({
+		// 		place: this,
+		// 		message: 'OPEN',
+		// 		url: msg.uri(),
+		// 		port: $mol_key( port ),
+		// 	})
 			
-			$mol_wire_sync( this ).REQUEST(
-				msg.derive( 'OPEN', null )
-			)
+		// 	$mol_wire_sync( this ).REQUEST(
+		// 		msg.derive( 'OPEN', null )
+		// 	)
 			
-			channel.onmessage = event => {
+		// 	channel.onmessage = event => {
 				
-				const message = msg.derive( 'POST', event.data )
-				message.port = port
+		// 		const message = msg.derive( 'POST', event.data )
+		// 		message.port = port
 				
-				this.$.$mol_log3_rise({
-					place: this,
-					message: message.method(),
-					url: message.uri(),
-					port: $mol_key( port ),
-				})
+		// 		this.$.$mol_log3_rise({
+		// 			place: this,
+		// 			message: message.method(),
+		// 			url: message.uri(),
+		// 			port: $mol_key( port ),
+		// 		})
 
-				$mol_wire_async( this ).POST( message )
+		// 		$mol_wire_async( this ).POST( message )
 				
-			}
+		// 	}
 			
-			channel.onclose = ()=> {
+		// 	channel.onclose = ()=> {
 				
-				this.$.$mol_log3_done({
-					place: this,
-					message: 'CLOSE',
-					url: msg.uri(),
-					port: $mol_key( port ),
-				})
+		// 		this.$.$mol_log3_done({
+		// 			place: this,
+		// 			message: 'CLOSE',
+		// 			url: msg.uri(),
+		// 			port: $mol_key( port ),
+		// 		})
 				
-				$mol_wire_sync( this ).REQUEST(
-					msg.derive( 'CLOSE', null )
-				)
+		// 		$mol_wire_sync( this ).REQUEST(
+		// 			msg.derive( 'CLOSE', null )
+		// 		)
 				
-			}
+		// 	}
 			
-			const sdp = await $mol_wire_async( msg ).text()
-			await connection.setRemoteDescription({ sdp, type: 'offer' })
+		// 	const sdp = await $mol_wire_async( msg ).text()
+		// 	await connection.setRemoteDescription({ sdp, type: 'offer' })
 			
-			connection.setLocalDescription({ type: 'answer' })
-			await new Promise( done => connection.onicecandidate = ({ candidate })=> done( candidate ) )
+		// 	connection.setLocalDescription({ type: 'answer' })
+		// 	await new Promise( done => connection.onicecandidate = ({ candidate })=> done( candidate ) )
 			
-			msg.port.send_type( 'application/sdp' )
-			msg.port.send_text( connection.localDescription!.sdp )
+		// 	msg.port.send_type( 'application/sdp' )
+		// 	msg.port.send_text( connection.localDescription!.sdp )
 			
-		}
+		// }
 		
 		OPEN( msg: $mol_rest_message ) {}
 		CLOSE( msg: $mol_rest_message ) {}
