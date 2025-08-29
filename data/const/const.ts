1925781d063049d0eac9aa189e436123c7cec1ad
jin <jin@hyoo.ru>
2025-08-29 21:52:52 +0300

    $mol_data_const: precise type

--- data/const/const.ts
+++ data/const/const.ts
@@ -4,7 +4,7 @@ namespace $ {
 	 * Checks for equality to given value and returns expected type.
 	 * @see https://mol.hyoo.ru/#!section=demos/demo=mol_data_const_demo
 	 */
-	export function $mol_data_const< Val >( ref : Val ) {
+	export function $mol_data_const< const Val >( ref : Val ) {
 
 		return $mol_data_setup( ( val : Val ) => {
 			
