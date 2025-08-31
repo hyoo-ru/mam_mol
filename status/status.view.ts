717989319a2238bf31638a9848ecd28112d2c275
nin-jin <sairi-na-tenshi@ya.ru>
2025-08-31 20:50:32 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@15ac7b285c13c958f3f78904d8c316cea80dfb99 🚀

--- status/status.view.ts
+++ status/status.view.ts
@@ -7,6 +7,7 @@ namespace $.$$ {
 				return this.status() ?? null
 			} catch( error: any ) {
 				if( error instanceof Promise ) $mol_fail_hidden( error )
+				$mol_fail_log( error )
 				return error.message
 			}
 		}
