39a737363721adb0cfdf004a77f04e502cf6ebf1
Stefan Zerkalica <zerkalica@gmail.com>
2025-10-16 14:13:47 +0300

    Dropping autodetect in $mol_base64_decode_node

--- base64/decode/decode.node.ts
+++ base64/decode/decode.node.ts
@@ -4,10 +4,9 @@ namespace $ {
 	export function $mol_base64_decode_node(base64Str: string , options?: { alphabet?: 'base64url' }): Uint8Array< ArrayBuffer > {
 
 		if ('fromBase64' in Uint8Array) return base_decode(base64Str, options)
-		base64Str = base64Str.replace( /-/g, '+' ).replace( /_/g, '/' )
-		
+
 		// without Uint8Array breaks $mol_compare_deep
-		const buffer = Buffer.from(base64Str, 'base64')
+		const buffer = Buffer.from(base64Str, options?.alphabet ?? 'base64')
 
 		return new Uint8Array(buffer.buffer, buffer.byteOffset, buffer.byteLength)
 	}
--- base64/decode/decode.web.ts
+++ base64/decode/decode.web.ts
@@ -4,7 +4,8 @@ namespace $ {
 	export function $mol_base64_decode_web(base64Str: string, options?: { alphabet?: 'base64url' }): Uint8Array< ArrayBuffer > {
 
 		if ('fromBase64' in Uint8Array) return base_decode(base64Str, options)
-		base64Str = base64Str.replace( /-/g, '+' ).replace( /_/g, '/' )
+
+		if (options?.alphabet === 'base64url') base64Str = base64Str.replace( /-/g, '+' ).replace( /_/g, '/' )
 		
 		const buf = Uint8Array.from($mol_dom_context.atob(base64Str), c => c.charCodeAt(0))
 
