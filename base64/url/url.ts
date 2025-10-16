de267167140631fcc457c8637ac79fac356ccf95
Stefan Zerkalica <zerkalica@gmail.com>
2025-10-16 13:20:29 +0300

    $mol_base64_url - remove replacers

--- base64/url/url.ts
+++ base64/url/url.ts
@@ -1,11 +1,11 @@
 namespace $ {
 	
 	export function $mol_base64_url_encode( buffer: Uint8Array< ArrayBuffer > ) {
-		return $mol_base64_encode( buffer ).replace( /\+/g, '-' ).replace( /\//g, '_' ).replace( /=/g, '' )
+		return $mol_base64_encode( buffer, { alphabet: 'base64url', omitPadding: true } )
 	}
 	
 	export function $mol_base64_url_decode( str: string ) {
-		return $mol_base64_decode( str.replace( /-/g, '+' ).replace( /_/g, '/' ) )
+		return $mol_base64_decode( str )
 	}
 	
 }
