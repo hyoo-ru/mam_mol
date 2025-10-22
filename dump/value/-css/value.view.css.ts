91b1e01a5441e8a65e02c9aa305e2ebf4135b9fa
nin-jin <sairi-na-tenshi@ya.ru>
2025-10-22 14:08:53 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@8cb9e7e5cc3bc8f8cd8181e3214376b49b806141 🚀

--- dump/value/-css/value.view.css.ts
+++ dump/value/-css/value.view.css.ts
@@ -1,3 +1,3 @@
 namespace $ { $mol_style_attach( "mol/dump/value/value.view.css",
- "[mol_dump_value] {\n\tmin-height: 2.5rem;\n\tmin-width: 2.5rem;\n}\n\n[mol_dump_value_simple] {\n\tpadding: 0;\n}\n\n[mol_dump_value_expand_content] {\n\tpadding-left: 1.5rem;\n\talign-items: flex-start;\n}\n\n[mol_dump_value_expand_title_rows],\n[mol_dump_value_simple_rows],\n[mol_dump_value_expand_head] {\n\tpadding: 0;\n\tgap: 0;\n}\n"
+ "[mol_dump_value] {\n\tmin-height: 1.5rem;\n\tmin-width: 2.5rem;\n}\n\n[mol_dump_value_simple] {\n\tpadding: 0;\n}\n\n[mol_dump_value_expand_head] {\n\tmin-height: 1.5rem;\n}\n\n[mol_dump_value_expand_content] {\n\tpadding-left: 1.5rem;\n\talign-items: flex-start;\n}\n\n[mol_dump_value_expand_title_rows],\n[mol_dump_value_simple_rows],\n[mol_dump_value_expand_head] {\n\tpadding: 0;\n\tgap: 0;\n}\n"
 ) }
