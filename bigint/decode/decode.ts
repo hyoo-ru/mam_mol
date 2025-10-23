16fc84b58617dbf8162b6e8d2d4e4ad57c0911be
nin-jin <sairi-na-tenshi@ya.ru>
2025-10-23 22:23:58 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@cfb4b8220e82d53cea3a0628ef81392e5db3d1cb 🚀

--- bigint/decode/decode.ts
+++ bigint/decode/decode.ts
@@ -2,6 +2,11 @@ namespace $ {
 	
 	export function $mol_bigint_decode( buf: Uint8Array< ArrayBuffer > ) {
 		
+		if( buf.length === 8 ) return new BigInt64Array( buf.buffer, buf.byteOffset, 1 )[0]
+		if( buf.length === 4 ) return BigInt( new Int32Array( buf.buffer, buf.byteOffset, 1 )[0] )
+		if( buf.length === 2 ) return BigInt( new Int16Array( buf.buffer, buf.byteOffset, 1 )[0] )
+		if( buf.length === 1 ) return BigInt( new Int8Array( buf.buffer, buf.byteOffset, 1 )[0] )
+		
 		const minus = ( buf.at(-1)! & 128 ) ? 255 : 0
 		
 		let result = 0n
