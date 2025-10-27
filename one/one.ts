4c669484464cd676fcada04d2ed7c6e91361c53c
zerkalica <zerkalica@gmail.com>
2025-10-27 13:39:53 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@6b8872ed1c19216977eea1e07cc6af628c45288e 🚀

---
+++ one/one.ts
@@ -0,0 +1,41 @@
+
+namespace $ {
+	type Instances<Obj> = {
+		[K in keyof Obj]: Obj[K] extends new (...args: any) => infer Instance ? Instance : Obj[K]
+	}
+
+	export let $mol_one = {} as Instances<$> & (<Instance>(constructor: new (...args: any) => Instance) => Instance)
+
+	const cache = new WeakMap<new (...args: any) => any, {}>()
+
+	function singletone<Instance extends { [$mol_ambient_ref]?: typeof $ }>(
+		this: typeof $,
+		Origin: new (...args: any) => Instance
+	) {
+		const Contexted = this.$mol_static(Origin)
+		let instance = cache.get(Contexted) as Instance | undefined
+
+		if (instance) return instance
+
+		instance = new Contexted()
+		instance[$mol_ambient_ref] = this
+		cache.set(Contexted, instance)
+
+		return instance
+	}
+
+	Object.defineProperty($, '$mol_one', {
+		get() {
+			const t = this
+
+			return new Proxy(singletone, {
+				get(self, k) {
+					const val = t[k as keyof typeof t]
+					if (typeof val !== 'function') return val
+					return singletone.call(t, val)
+				}
+			})
+		}
+	})
+
+}
---
+++ static/README.md
@@ -0,0 +1,11 @@
+# $mol_static
+
+Extends and cache static class and sets context.
+
+```ts
+	export class $my_session {
+		static tokens = []
+	}
+
+	this.$.$mol_static.$my_session.$ === this.$
+```
