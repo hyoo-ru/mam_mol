ee4c690a6f24eb96226cd293b88e409d2b19ab69
jin <jin@hyoo.ru>
2025-11-07 16:59:24 +0300

    + Date

--- vary/vary.ts
+++ vary/vary.ts
@@ -735,4 +735,13 @@ namespace $ {
 		],
 	)
 	
+	/** Native Date support */
+	$mol_vary.register(
+		( ts = 0 )=> new Date( ts * 1000 ),
+		obj => [
+			[ 'unix_time' ],
+			[ obj.valueOf() / 1000 ],
+		],
+	)
+	
 }
