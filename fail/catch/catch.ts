4192a37cf9796da223ef02fb85c5597e6dd69bca
Stefan Zerkalica <zerkalica@gmail.com>
2025-11-13 13:53:47 +0300

    Cached moved to weak set

--- fail/catch/catch.ts
+++ fail/catch/catch.ts
@@ -1,19 +1,14 @@
 namespace $ {
 
-	const catched = new WeakMap< any , string >()
-
-	export function $mol_fail_catch_id( error: unknown ) {
-		if( typeof error !== 'object' ) return null
-		return catched.get( error ) ?? null
-	}
+	const catched = new WeakSet< any >()
 
 	export function $mol_fail_catch( error: unknown ) {
 		
 		if( typeof error !== 'object' ) return false
 		if( $mol_promise_like( error ) ) $mol_fail_hidden( error )
-		if( catched.get( error ) ) return false
+		if( catched.has( error ) ) return false
 		
-		catched.set( error , $mol_guid() )
+		catched.add( error  )
 		return true
 		
 	}
