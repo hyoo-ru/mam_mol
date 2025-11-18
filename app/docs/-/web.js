8b9ebbca008a2b680528db8e9f79698012b0cbab
nin-jin <sairi-na-tenshi@ya.ru>
2025-11-18 13:18:03 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@f7495e0feacb62c081187eb048a061da42b252b5 🚀

--- app/docs/-/web.js
+++ app/docs/-/web.js
@@ -2507,7 +2507,7 @@ var $;
 "use strict";
 var $;
 (function ($) {
-    $mol_style_attach("mol/gap/gap.css", ":root {\n\t--mol_gap_page: 6rem;\n\t--mol_gap_block: .75rem;\n\t--mol_gap_text: .5rem .75rem;\n\t--mol_gap_round: .25rem;\n\t--mol_gap_space: .25rem;\n\t--mol_gap_blur: .5rem;\n}\n");
+    $mol_style_attach("mol/gap/gap.css", ":root {\n\t--mol_gap_page: 3rem;\n\t--mol_gap_block: .75rem;\n\t--mol_gap_text: .5rem .75rem;\n\t--mol_gap_round: .25rem;\n\t--mol_gap_space: .25rem;\n\t--mol_gap_blur: .5rem;\n}\n");
 })($ || ($ = {}));
 
 ;
