6e83ce5d056a97797abb1037d80105de690422a6
Stefan Zerkalica <zerkalica@gmail.com>
2025-12-12 14:50:34 +0300

    Pass origin promise if no fallback

--- error/fence/fence.ts
+++ error/fence/fence.ts
@@ -1,8 +1,12 @@
 namespace $ {
+	function pass<Data>(data: Data) {
+		return data
+	}
+
 	export function $mol_error_fence<Data>(
 		task: () => Data,
 		fallback: (parent: Error) => Error | Data | PromiseLike<Data>,
-		loading?: (parent: PromiseLike<Data>) => Error | Data | PromiseLike<Data>
+		loading: (parent: PromiseLike<Data>) => Error | Data | PromiseLike<Data> = pass
 	) {
 		try {
 			return task()
@@ -10,7 +14,7 @@ namespace $ {
 			let normalized
 
 			try {
-				normalized = $mol_promise_like(error) ? loading?.(error) : fallback(error as Error)
+				normalized = $mol_promise_like(error) ? loading(error) : fallback(error as Error)
 			} catch (sub_error) {
 				$mol_fail_log(sub_error)
 			}
