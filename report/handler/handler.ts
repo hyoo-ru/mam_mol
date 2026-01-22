93d1de895a75ad94fcc1f74b4d15be7eb439c8cf
zerkalica <zerkalica@gmail.com>
2026-01-22 17:43:51 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@a24478f6d17acea76a3d61eac99b23412f41e2ac 🚀

---
+++ report/handler/handler.ts
@@ -0,0 +1,25 @@
+namespace $ {
+
+	type Error_handler = (event: Event | string, url?: string, line?: number, col?: number, error?: Error) => void
+
+	export const $mol_report_handler_all = new Set<Error_handler>()
+
+	function handler(event: Event | string, url?: string, line?: number, col?: number, error?: Error) {
+		for (const handler of $mol_report_handler_all) {
+			try {
+				handler(event, url, line, col, error)
+			} catch (e) {}
+		}
+	}
+	const handler_promise = (event: PromiseRejectionEvent) => handler('Unhandled Rejection', '', 0, 0, event.reason )
+
+	globalThis.addEventListener('error', handler)
+	globalThis.addEventListener('unhandledrejection', handler_promise)
+
+	const console_error = console.error
+	console.error = function console_error_custom( ... args ) {
+		handler( 'Logged Error', '', 0, 0, arguments[0] )
+		console_error.apply( console, args )
+	}
+
+}
