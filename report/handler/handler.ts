0a5e915a2c6fdf4405f557d6ac18107fc471ce88
Stefan Zerkalica <zerkalica@gmail.com>
2026-01-22 20:22:53 +0300

    Report error handler set to public

--- report/handler/handler.ts
+++ report/handler/handler.ts
@@ -2,10 +2,10 @@ namespace $ {
 
 	type Error_handler = (event: Event | string, url?: string, line?: number, col?: number, error?: Error) => void
 
-	let handlers = [] as Error_handler[]
+	export const $mol_report_handler_set = new Set<Error_handler>()
 
 	function handler(event: Event | string, url?: string, line?: number, col?: number, error?: Error) {
-		for (const handler of handlers) {
+		for (const handler of $mol_report_handler_set) {
 			try {
 				handler(event, url, line, col, error)
 			} catch (e) {}
@@ -20,14 +20,6 @@ namespace $ {
 	console.error = function console_error_custom( ... args ) {
 		handler( 'Logged Error', '', 0, 0, arguments[0] )
 		console_error.apply( console, args )
-	}
-
-	export function $mol_report_handler_add(handler: Error_handler) {
-		handlers.push(handler)
-	}
-
-	export function $mol_report_handler_remove(handler: Error_handler) {
-		handlers = handlers.filter(target => target !== handler)
 	}
 
 }
