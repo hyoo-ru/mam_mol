4a711b786c4dda95e62c42203b3f3ae6b125a542
Stefan Zerkalica <zerkalica@gmail.com>
2026-01-23 15:58:56 +0300

    Used array shift in console.error report handler 7

--- report/handler/handler.ts
+++ report/handler/handler.ts
@@ -21,15 +21,14 @@ namespace $ {
 
 	console.error = function console_error_custom( ... args ) {
 
-		const format = (val: string, spec = '') => spec === '%c' ? '' :
-			typeof val === 'string'
-				? val.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
-				: JSON.stringify(val)
+		const format = (val: unknown) => typeof val === 'string'
+			? val.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
+			: JSON.stringify(val)
 
 		let secondary = args.slice(1)
 		const first = typeof args[0] === 'string'
-			? args[0].replaceAll( /%(?:\.\d+)?[disfcoO]/g, spec => format(secondary.shift(), spec) )
-			: format(args[0])
+			? args[0].replaceAll( /%(?:\.\d+)?[disfcoO]/g, spec => spec === '%c' ? (secondary.shift(), '') : secondary.shift() )
+			: args[0]
 
 		secondary.unshift(first)
 		let result = secondary.map(val => format(val)).join(' ')
