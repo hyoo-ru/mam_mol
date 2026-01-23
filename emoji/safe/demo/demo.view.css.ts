c0b63d6b72279411624f5c819501495af6ec7f4d
jin <jin@hyoo.ru>
2026-01-23 11:11:14 +0300

    emojis improved

--- emoji/safe/demo/demo.view.css.ts
+++ emoji/safe/demo/demo.view.css.ts
@@ -2,10 +2,18 @@ namespace $.$$ {
 	
 	$mol_style_define( $mol_emoji_safe_demo, {
 		
+		Group_title: {
+			textShadow: '0 0',
+		},
+		
 		Emojis: {
 			flex: {
 				wrap: 'wrap',
 			},
+		},
+		
+		Emoji: {
+			padding: $mol_gap.block,
 		},
 		
 	} )
