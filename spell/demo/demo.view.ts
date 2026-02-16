0f7d2c7bc2f8281dfe75927a569846b04e3bf190
jin <jin@hyoo.ru>
2026-02-16 14:23:37 +0300

    ++

--- spell/demo/demo.view.ts
+++ spell/demo/demo.view.ts
@@ -2,10 +2,15 @@ namespace $.$$ {
 	export class $mol_spell_demo extends $.$mol_spell_demo {
 		
 		@ $mol_mem
+		words() {
+			return [ ... this.article().toLowerCase().match( /\p{Letter}{1,}/ug )! ]
+		}
+		
+		@ $mol_mem
 		report() {
 			
 			const wrong = [] as string[]
-			const words = new Set( this.article().toLowerCase().match( /\p{Letter}{2,}/ug )! )
+			const words = new Set( this.words() )
 			
 			for( const word of words ) {
 				if( $mol_spell_any.check( word ) ) continue
@@ -13,6 +18,11 @@ namespace $.$$ {
 			}
 			
 			return wrong.join( '\n' )
+		}
+		
+		@ $mol_mem
+		segments() {
+			return this.words().map( word => $mol_spell_ru.split( word ).join( '-' ) ).join( ' ' )
 		}
 		
 	}
