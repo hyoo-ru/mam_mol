dd43eb5e473eb65e4cad1995910479e29e20e299
Stefan Zerkalica <zerkalica@gmail.com>
2026-02-17 22:32:36 +0300

    $mol_fetch pass response instead of response.native + response.request

--- fetch/fetch.ts
+++ fetch/fetch.ts
@@ -51,7 +51,7 @@ namespace $ {
 		protected safe_call<V>(cb: () => V) {
 			return $mol_error_fence(
 				cb,
-				e => new $mol_error_mix(e.message, { request: this.request, response : this.native }, e),
+				e => new $mol_error_mix(e.message, { response : this }, e),
 			)
 		}
 
