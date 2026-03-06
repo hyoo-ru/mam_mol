77d29972c2d55679c6cece0bb535c4bb3ea16c52
Stefan Zerkalica <zerkalica@gmail.com>
2026-03-06 22:55:24 +0300

    Fix type in $mol_store_safe

--- store/safe/safe.ts
+++ store/safe/safe.ts
@@ -1,5 +1,8 @@
 namespace $ {
-	export function $mol_store_safe(cb: () => ReturnType<typeof $mol_store_mock> | null) {
+	export function $mol_store_safe<Store extends {
+		setItem(key: string, value: string ): void
+		removeItem(key: string): void
+	}>(cb: () => Store | null) {
 		const native = cb()
 		if (! native ) return null
 
