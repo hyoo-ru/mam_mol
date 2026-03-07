7c2e1a78e9cc8def7525a903fc48d150b5d88b10
Stefan Zerkalica <zerkalica@gmail.com>
2026-03-07 14:26:52 +0300

    Removed unusable $mol_store_mem fallback from local storage

--- store/local/local.ts
+++ store/local/local.ts
@@ -4,11 +4,6 @@ namespace $ {
 		override native() {
 			return $mol_store_safe( () => this.$.$mol_dom_context.localStorage )
 		}
-
-		override fallback() {
-			return this._fallback = this._fallback
-				?? this.$.$mol_store_mem.sub<string, $mol_store<Record<string, any>>>('$mol_store_local')
-		}
 	}
 
 	export let $mol_store_local : $mol_store< Record< string , any > > = new $mol_store_local_class()
