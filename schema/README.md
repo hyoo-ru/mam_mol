0686107f6c7a21ac14c73031507d6bb8319a8493
nin-jin <sairi-na-tenshi@ya.ru>
2026-03-27 11:24:43 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@e91345223e749c5b15c48813024837f236e45eea 🚀

---
+++ schema/README.md
@@ -0,0 +1,74 @@
+# $mol_schema
+
+> Runtime typing schemas
+
+## Schema API
+
+- `toString` - identity of schema.
+- `check(val)` - type guard.
+- `guard(val)` - strict parser.
+- `cast(val)` - relaxed caster.
+- `default` - default value for casting.
+
+## Leaf schemas
+
+- `$mol_schema_enum` - some of constant values (first value as default).
+- `$mol_schema_boolean` - true or false (default).
+- `$mol_schema_string` - any string ("" as default).
+- `$mol_schema_float` - any number (NaN as default).
+- `$mol_schema_integer` - integer number (0 as default).
+- `$mol_schema_range` - range of values between min (default) and max.
+  - `$mol_schema_positive` - >= 0
+  - `$mol_schema_negative` - <= 0
+  - `$mol_schema_natural` - natural numbers from 0 (default).
+
+## Coposition schemas
+
+- `$mol_schema_some` - some of schemas (default of first as default).
+- `$mol_schema_every` - every of schemas (first default casted through all of them).
+- `$mol_schema_list` - array of any values ([] as default).
+- `$mol_schema_array` - typed by schema array ([] as default).
+- `$mol_schema_object` - any object ({} as default).
+- `$mol_schema_record` - object with typed fields (objects with default fields values by default).
+
+## Complex example
+
+```ts
+export abstract class $my_article_search extends $mol_schema_some([
+	$mol_schema_record({
+		fail: $mol_schema_string,
+	}),
+	$mol_schema_record({
+		data: $mol_schema_array( $my_article_full ),
+	}),
+])
+
+export abstract class $my_article_full extends $mol_schema_record({
+	title: $mol_schema_string,
+	body: $mol_schema_string,
+	author: $my_user_main,
+}) {}
+
+export abstract class $my_user_main extends $mol_schema_record({
+	name: $mol_schema_string,
+	age: $mol_schema_natural,
+}) {}
+```
+
+```ts
+response = $my_article_search.guard( response ) // Exception on wrong data
+```
+
+```ts
+response = $my_article_search.cast( response ) // Defaults instead wrong data
+```
+
+```ts
+if( $my_user_main.check( user ) ) {
+	// user is $my_user_main now
+}
+```
+
+```ts
+const article = $my_article_full.default // default article state
+```
---
+++ schema/schema.test.ts
@@ -0,0 +1,195 @@
+namespace $.$$ {
+	$mol_test({
+		
+		"Composed keys"( $ ) {
+			
+			$mol_assert_equal(
+				'$mol_schema_boolean',
+				$mol_schema_boolean + '',
+			)
+			
+			$mol_assert_equal(
+				'$mol_schema_enum<[false,123,"foo"]>',
+				$mol_schema_enum([ false, 123, 'foo' ]) + '',
+				$mol_key( $mol_schema_enum([ false, 123, 'foo' ]) ),
+			)
+			
+			$mol_assert_equal(
+				'$mol_schema_some<[$mol_schema_float,$mol_schema_string]>',
+				$mol_schema_some([ $mol_schema_float, $mol_schema_string ]) + '',
+			)
+			
+			$mol_assert_equal(
+				'$mol_schema_every<[$mol_schema_integer,$mol_schema_range<5,9>]>',
+				$mol_schema_every([ $mol_schema_integer, $mol_schema_range( 5, 9 ) ]) + '',
+			)
+			
+			$mol_assert_equal(
+				'$mol_schema_array<$mol_schema_float>',
+				$mol_schema_array( $mol_schema_float ) + '',
+			)
+			
+			$mol_assert_equal(
+				'$mol_schema_record<{"name":$mol_schema_string}>',
+				$mol_schema_record({ name: $mol_schema_string }) + '',
+			)
+			
+			$mol_assert_equal(
+				'Some',
+				class Some extends $mol_schema_enum([ 123 ]) {} + '',
+				class Some extends $mol_schema_some([ $mol_schema_float ]) {} + '',
+				class Some extends $mol_schema_every([ $mol_schema_float ]) {} + '',
+				class Some extends $mol_schema_array( $mol_schema_float ) {} + '',
+				class Some extends $mol_schema_record({ name: $mol_schema_string }) {} + '',
+			)
+			
+		},
+		
+		"String schema"( $ ) {
+			
+			$mol_assert_equal( true, $mol_schema_string.check( 'foo' ) )
+			$mol_assert_equal( false, $mol_schema_string.check( 123 ) )
+			
+			$mol_assert_equal( 'foo', $mol_schema_string.cast( 'foo' ) )
+			$mol_assert_equal( '', $mol_schema_string.cast( 123 ) )
+			
+		},
+		
+		"Integer schema"( $ ) {
+			
+			$mol_assert_equal( true, $mol_schema_integer.check( Number.MAX_SAFE_INTEGER ) )
+			$mol_assert_equal( true, $mol_schema_integer.check( Number.MIN_SAFE_INTEGER ) )
+			$mol_assert_equal( true, $mol_schema_integer.check( 0 ) )
+			
+			$mol_assert_equal( false, $mol_schema_integer.check( Number.EPSILON ) )
+			$mol_assert_equal( false, $mol_schema_integer.check( Number.POSITIVE_INFINITY ) )
+			$mol_assert_equal( false, $mol_schema_integer.check( Number.NEGATIVE_INFINITY ) )
+			
+			$mol_assert_equal( Number.MAX_SAFE_INTEGER, $mol_schema_integer.cast( Number.MAX_SAFE_INTEGER ) )
+			$mol_assert_equal( 0, $mol_schema_integer.cast( Number.EPSILON ) )
+			$mol_assert_equal( 0, $mol_schema_integer.cast( 1.5 ) )
+			
+		},
+		
+		"Enum options schema"( $ ) {
+			
+			const Config = $mol_schema_enum([ 123, 'foo' ])
+			
+			$mol_assert_equal( true, Config.check( 123 ) )
+			$mol_assert_equal( true, Config.check( 'foo' ) )
+			
+			$mol_assert_equal( false, Config.check( true ) )
+			$mol_assert_equal( false, Config.check( 321 ) )
+			$mol_assert_equal( false, Config.check( 'bar' ) )
+			
+			$mol_assert_equal( 123, Config.cast( 123 ) )
+			$mol_assert_equal( 'foo', Config.cast( 'foo' ) )
+			$mol_assert_equal( 123, Config.cast( 'bar' ) )
+			
+		},
+		
+		"Some variant schema"( $ ) {
+			
+			const Config = $mol_schema_some([ $mol_schema_float, $mol_schema_string ])
+			
+			$mol_assert_equal( true, Config.check( 123 ) )
+			$mol_assert_equal( true, Config.check( 'foo' ) )
+			$mol_assert_equal( false, Config.check( true ) )
+			
+			$mol_assert_equal( 123, Config.cast( 123 ) )
+			$mol_assert_equal( 'foo', Config.cast( 'foo' ) )
+			$mol_assert_equal( Number.NaN, Config.cast( true ) )
+			
+		},
+		
+		"Float range schema"( $ ) {
+			
+			const range = $mol_schema_range( 4, 8 )
+			
+			$mol_assert_equal( true, range.check( 5.5 ) )
+			$mol_assert_equal( true, range.check( 4 ) )
+			$mol_assert_equal( true, range.check( 8 ) )
+			
+			$mol_assert_equal( false, range.check( 3 ) )
+			$mol_assert_equal( false, range.check( 9 ) )
+			
+			$mol_assert_equal( 4, range.cast( 3 ) )
+			$mol_assert_equal( 8, range.cast( 9 ) )
+			$mol_assert_equal( 4, range.cast( Number.NaN ) )
+			
+		},
+		
+		"Range intersection schema"( $ ) {
+			
+			const Narrow = $mol_schema_every([ $mol_schema_integer, $mol_schema_range( 1, 8 ), $mol_schema_range( 4, 10 ) ])
+			
+			$mol_assert_equal( true, Narrow.check( 6 ) )
+			$mol_assert_equal( true, Narrow.check( 4 ) )
+			$mol_assert_equal( true, Narrow.check( 8 ) )
+			
+			$mol_assert_equal( false, Narrow.check( 3 ) )
+			$mol_assert_equal( false, Narrow.check( 9 ) )
+			$mol_assert_equal( false, Narrow.check( 5.5 ) )
+			
+			$mol_assert_equal( 4, Narrow.cast( 3 ) )
+			$mol_assert_equal( 8, Narrow.cast( 9 ) )
+			$mol_assert_equal( 4, Narrow.cast( '6' ) )
+			
+		},
+		
+		"Vary Array schema"( $ ) {
+			
+			$mol_assert_equal( true, $mol_schema_list.check( [] ) )
+			$mol_assert_equal( false, $mol_schema_list.check( '' ) )
+			
+			$mol_assert_equal( [ 123 ], $mol_schema_list.cast( [ 123 ] ) )
+			$mol_assert_equal( [], $mol_schema_list.cast( 'foo' ) )
+			
+		},
+		
+		"Typed Array schema"( $ ) {
+			
+			const Vector = $mol_schema_array( $mol_schema_float )
+			
+			$mol_assert_equal( true, Vector.check( [] ) )
+			$mol_assert_equal( true, Vector.check( [ 123 ] ) )
+			$mol_assert_equal( false, Vector.check( [ 'foo' ] ) )
+			
+			$mol_assert_equal( [ 123 ], Vector.cast( [ 123 ] ) )
+			$mol_assert_equal( [ 123, Number.NaN ], Vector.cast( [ 123, 'foo' ] ) )
+			
+		},
+		
+		"Vary Record schema"( $ ) {
+			
+			$mol_assert_equal( true, $mol_schema_object.check( {} ) )
+			$mol_assert_equal( false, $mol_schema_object.check( [] ) )
+			
+			$mol_assert_equal( { foo: 123 }, $mol_schema_object.cast( { foo: 123 } ) )
+			$mol_assert_equal( {}, $mol_schema_object.cast( [ 'foo', 123 ] ) )
+			
+		},
+		
+		"Typed Record schema"( $ ) {
+			
+			const User = $mol_schema_record({
+				name: $mol_schema_string,
+				age: $mol_schema_integer,
+			})
+			
+			$mol_assert_equal( true, User.check( { name: 'foo', age: 123 } ) )
+			$mol_assert_equal( true, User.check( { name: 'foo', age: 123, height: 777 } ) )
+			
+			$mol_assert_equal( false, User.check( {} ) )
+			$mol_assert_equal( false, User.check( { name: 'foo', age: 1.5 } ) )
+			
+			$mol_assert_equal(
+				{ name: 'foo', age: 123 },
+				User.cast( { name: 'foo', age: 123, height: 777 } )
+			)
+			$mol_assert_equal( { name: 'foo', age: 0 }, User.cast( { name: 'foo' } ) )
+			
+		},
+		
+	})
+}
