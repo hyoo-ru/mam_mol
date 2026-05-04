1c79ecc35c3a8ccb2b642bb1c13e005baf4fc0b3
jin <jin@hyoo.ru>
2026-05-04 18:14:22 +0300

    $mol_type_assert: unify

--- type/assert/assert.ts
+++ type/assert/assert.ts
@@ -2,20 +2,25 @@ namespace $ {
 
 	/**
 	 * Asserts for equality of `Actual` and `Expected` types.
-	 * Don't use `never` as `Expected` - use `mol_type_assert_never` instead.
 	 */
 	export type $mol_type_assert<
-		Actual ,
-		Expected extends
-		| $mol_type_equals< Actual , Expected >
-		| $mol_type_error< 'Assert failed', {
-			actual: Actual ,
-			expected: Expected
-		} >
+		Actual extends $mol_type_equals< Actual , Expected > extends true ? unknown
+			: $mol_type_equals< Actual , any > extends true ? never
+			: $mol_type_error< 'Assert failed', {
+				actual: Actual ,
+				expected: Expected
+			} >,
+		Expected extends $mol_type_equals< Actual , Expected > extends true ? unknown
+			: $mol_type_equals< Expected , any > extends true ? never
+			: $mol_type_error< 'Assert failed', {
+				actual: Actual ,
+				expected: Expected
+			} >
 	> = Actual
 
 	/**
 	 * Asserts for `Actual` type is `never`.
+	 * @deprecated Use `$mol_type_assert< A, B >`
 	 * 
 	 * 	$mol_type_assert_never< $mol_type_equals< 1 , 2 > >
 	 */
