c2dab91020662af5416f8588d008b77bbbc0a5b3
Stefan Zerkalica <zerkalica@gmail.com>
2026-08-14 17:06:46 +0300

    Adds lang/dir to div, not root tag

--- locale/plugin/plugin.view.ts
+++ locale/plugin/plugin.view.ts
@@ -1,16 +1,8 @@
 namespace $.$$ {
 	export class $mol_locale_plugin extends $.$mol_locale_plugin {
 		
-		@ $mol_mem
-		override auto() {
-			const locale = this.$.$mol_static.$mol_locale
-			const doc = this.$.$mol_dom_context.document.documentElement
-
-			doc.lang = locale.lang()
-			doc.dir = locale.direction()
-
-			return null
-		}
-		
+		protected locale() { return this.$.$mol_static.$mol_locale }
+		override lang() { return this.locale().lang() }
+		override direction() { return this.locale().direction() }
 	}
 }
