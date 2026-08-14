84a6b6e0734a2dc75e668da1cdfc7735a15fc908
Stefan Zerkalica <zerkalica@gmail.com>
2026-08-14 20:07:16 +0300

    Removed $mol_static

--- locale/plugin/plugin.view.ts
+++ locale/plugin/plugin.view.ts
@@ -1,7 +1,7 @@
 namespace $.$$ {
 	export class $mol_locale_plugin extends $.$mol_locale_plugin {
 		
-		protected locale() { return this.$.$mol_static.$mol_locale }
+		protected locale() { return this.$.$mol_locale }
 		override lang() { return this.locale().lang() }
 		override direction() { return this.locale().direction() }
 	}
