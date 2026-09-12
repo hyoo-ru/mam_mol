dbde6746222a0bb4106e5f2f4a26e53b6bf9042c
novgorodcev.k <novgorodcev.k@rwb.ru>
2026-09-12 21:32:07 +0300

    $mol_view_tree2_to_dts: test compares the whole generated text

--- view/tree2/to/dts/dts.test.ts
+++ view/tree2/to/dts/dts.test.ts
@@ -9,11 +9,25 @@ namespace $ {
 				'test.view.tree',
 			)
 
-			const dts = $.$mol_tree2_text_to_string( $.$mol_view_tree2_to_dts( tree ) )
-
-			$mol_assert_equal( 
-				`$mol_view_override_mismatch< $mol_view_tree2_to_dts_test_foo, '$mol_view_tree2_to_dts_test_foo' >`
-			 )
+			$mol_assert_equal(
+				$.$mol_tree2_text_to_string( $.$mol_view_tree2_to_dts( tree ) ),
+				[
+					'declare namespace $ {',
+					'',
+					'\texport class $mol_view_tree2_to_dts_test_foo extends $mol_view_tree2_to_dts_test_base {',
+					'\t\tbar( ): string',
+					'\t}',
+					'\t',
+					'\ttype $mol_view_tree2_to_dts_test_foo__override_1 = $mol_type_enforce<',
+					'\t\t$mol_view_override_mismatch< $mol_view_tree2_to_dts_test_foo, \'$mol_view_tree2_to_dts_test_foo\' >',
+					'\t\t,',
+					'\t\t\'Override differs from view.tree prop only by case\'',
+					'\t>',
+					'\t',
+					'}',
+					'',
+				].join( '\n' ),
+			)
 
 		},
 
