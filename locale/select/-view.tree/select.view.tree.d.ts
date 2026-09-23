bb3bb5a4f0151ff702682f02fdd6d2b302ff5262
nin-jin <sairi-na-tenshi@ya.ru>
2026-09-23 19:52:42 +0000

    Deploying to gh-pages from @ hyoo-ru/mam_mol@599c95c96bca0426e89be9354745701269f8a056 🚀

--- locale/select/-view.tree/select.view.tree.d.ts
+++ locale/select/-view.tree/select.view.tree.d.ts
@@ -1,6 +1,12 @@
 declare namespace $ {
 
+	type $mol_locale_select_langs__1 = $mol_type_enforce<
+		`en`
+		,
+		string
+	>
 	export class $mol_locale_select extends $mol_select {
+		langs( ): readonly(string)[]
 	}
 	
 }
--- locale/select/-view.tree/select.view.tree.js
+++ locale/select/-view.tree/select.view.tree.js
@@ -1,3 +1,7 @@
-	($.$mol_locale_select) = class $mol_locale_select extends ($.$mol_select) {};
+	($.$mol_locale_select) = class $mol_locale_select extends ($.$mol_select) {
+		langs(){
+			return ["en"];
+		}
+	};
 
 //# sourceMappingURL=select.view.tree.js.map
